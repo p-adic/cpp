75d4fec62c88bbf67ca4ea200cbeda619a73b6fb
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2020-01-14 10:05:51 +0900

    Update a_Body.hpp

--- Music/OnMei/KanOn/a_Body.hpp
+++ Music/OnMei/KanOn/a_Body.hpp
@@ -8,7 +8,7 @@
 
 inline KanOn::KanOn( const int& num ) noexcept : m_num( num ){}
 
-inline string KanOn::Display() const noexcept { return IntToString( Represent( m_num ) ) ; }
+inline const string& KanOn::Display() const noexcept { return IntToString( Represent( m_num ) ) ; }
 inline const uint& KanOn::GetNum() const noexcept { return m_num; }
 
 inline bool operator==( const KanOn& N1 , const KanOn& N2 ) noexcept { return N1.GetNum() == N2.GetNum(); }
