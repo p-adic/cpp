b669703da81fcfa7829cd2e77a0808e8d28113ac
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2020-03-20 18:27:42 +0900

    Add files via upload

---
+++ Music/SanWaOn/a.hpp
@@ -0,0 +1,25 @@
+// c:/Users/user/Documents/Programming/Music/SanWaOn/a.hpp
+
+#pragma once
+#include "../OnMei/a.hpp"
+
+class SanWaOn
+{
+
+private:
+  OnMei m_N0;
+  OnMei m_N1;
+  OnMei m_N2;
+
+public:
+  inline SanWaOn( const OnMei& N0 , const OnMei& N1 , const OnMei& N2 ) noexcept;
+
+  bool IsValid() noexcept;
+  
+  inline string Display() const noexcept;
+  const OnMei& GetOnMei( const uint& i ) const noexcept;
+
+};
+
+bool operator==( const SanWaOn& H1 , const SanWaOn& H2 ) noexcept;
+inline bool operator!=( const SanWaOn& H1 , const SanWaOn& H2 ) noexcept;
