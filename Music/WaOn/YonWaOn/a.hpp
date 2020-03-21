9628911c2df3432241214a9e57cea44ccd52761e
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2020-03-21 15:49:29 +0900

    Add files via upload

---
+++ Music/WaOn/YonWaOn/a.hpp
@@ -0,0 +1,26 @@
+// c:/Users/user/Documents/Programming/Music/WaOn/YonWaOn/a.hpp
+
+#pragma once
+#include "../../OnMei/a.hpp"
+
+class YonWaOn
+{
+
+private:
+  OnMei m_N0;
+  OnMei m_N1;
+  OnMei m_N2;
+  OnMei m_N3;
+
+public:
+  inline YonWaOn( const OnMei& N0 , const OnMei& N1 , const OnMei& N2 , const OnMei& N3 ) noexcept;
+
+  bool IsValid() const noexcept;
+  
+  inline string Display() const noexcept;
+  const OnMei& GetOnMei( const uint& i ) const noexcept;
+
+};
+
+bool operator==( const YonWaOn& H1 , const YonWaOn& H2 ) noexcept;
+inline bool operator!=( const YonWaOn& H1 , const YonWaOn& H2 ) noexcept;
