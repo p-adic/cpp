1cb19c22cfd83e3948a2ad83cbb70068b74cb529
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2020-03-22 22:06:22 +0900

    Add files via upload

---
+++ Music/Haichi/YonWaOn/Seventh/a.hpp
@@ -0,0 +1,22 @@
+// c:/Users/user/Documents/Programming/Music/Haichi/YonWaOn/Seventh/a.hpp
+
+#pragma once
+#include "../a.hpp"
+#include "../../../Chou/a.hpp"
+
+class Seventh :
+  public YonWaOn
+{
+
+public:
+  inline Seventh( const Chou& N , const KaiMei& n ) noexcept;
+
+  inline const OnMei& GetNeOn() const noexcept;
+  inline const OnMei& GetDaiSanOn() const noexcept;
+  inline const OnMei& GetDaiGoOn() const noexcept;
+  inline const OnMei& GetDaiNanaOn() const noexcept;
+
+};
+
+inline bool operator==( const Seventh& H1 , const Seventh& H2 ) noexcept;
+inline bool operator!=( const Seventh& H1 , const Seventh& H2 ) noexcept;
