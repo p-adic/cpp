d5830947bb089ed17360df785e7053100c075b2b
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2020-03-22 18:20:06 +0900

    Add files via upload

--- Music/Haichi/YonWaOn/a.hpp
+++ Music/Haichi/YonWaOn/a.hpp
@@ -12,14 +12,16 @@ private:
   OnMei m_N2;
   OnMei m_N3;
 
+  bool m_is_valid;
+
 public:
   inline YonWaOn( const OnMei& N0 , const OnMei& N1 , const OnMei& N2 , const OnMei& N3 ) noexcept;
 
-  inline bool IsValid() const noexcept;
-  
   inline string Display() const noexcept;
-  const OnMei& GetOnMei( const uint& i ) const noexcept;
 
+  const OnMei& GetOnMei( const uint& i ) const noexcept;
+  inline const bool& IsValid() const noexcept;
+  
 };
 
 bool operator==( const YonWaOn& H1 , const YonWaOn& H2 ) noexcept;
