1db846bd26d2a2ad66d319d1249391bf1fe7bfaf
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2020-03-22 22:05:29 +0900

    Add files via upload

--- Music/Haichi/SanWaOn/KyouWaOn/a_Body.hpp
+++ Music/Haichi/SanWaOn/KyouWaOn/a_Body.hpp
@@ -10,9 +10,9 @@ inline KyouWaOn::KyouWaOn( const Chou& N , const KaiMei& n ) noexcept : SanWaOn(
 
 inline const bool& KyouWaOn::IsValid() const noexcept { return m_is_valid; }
 
-inline const OnMei& KyouWaOn::GetNeOn() const noexcept { return GetOnMi( 0 ); }
-inline const OnMei& KyouWaOn::GetDaiSanOn() const noexcept { return GetOnMi( 1 ); }
-inline const OnMei& KyouWaOn::GetDaiGoOn() const noexcept { return GetOnMi( 2 ); }
+inline const OnMei& KyouWaOn::GetNeOn() const noexcept { return GetOnMei( 0 ); }
+inline const OnMei& KyouWaOn::GetDaiSanOn() const noexcept { return GetOnMei( 1 ); }
+inline const OnMei& KyouWaOn::GetDaiGoOn() const noexcept { return GetOnMei( 2 ); }
 
 inline bool operator==( const KyouWaOn& H1 , const KyouWaOn& H2 ) noexcept { return H1.GetNeOn() == H2.GetNeOn() && H1.GetDaiSanOn() == H2.GetDaiSanOn() && H1.GetDaiGoOn() == H2.GetDaiGoOn(); }
 inline bool operator!=( const KyouWaOn& H1 , const KyouWaOn& H2 ) noexcept { return !( H1 == H2 ); }
