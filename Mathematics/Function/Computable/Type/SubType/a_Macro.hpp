3be66e8d2b60047fa04f1962e1ff31b4f2029eab
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2020-04-21 22:16:44 +0900

    Add files via upload

--- Mathematics/Function/Computable/Type/SubType/a_Macro.hpp
+++ Mathematics/Function/Computable/Type/SubType/a_Macro.hpp
@@ -13,9 +13,9 @@
 									\
   public:								\
     static inline const string& GetName() { return GetSyntax().GetNodeString( 2 ); }; \
-    static inline const SyntaxOfComputableFunction& GetSyntax() noexcept { static const SyntaxOfComputableFunction t{ TypeString() , SubTypeString() , TO_STRING( TYPE_NAME ) , ( VARIABLE ).Get() , GetCondition().Get() }; return t; }; \
+    static inline const SyntaxOfComputableFunction& GetSyntax() noexcept { static const SyntaxOfComputableFunction t{ TypeString() , SubTypeString() , TO_STRING( TYPE_NAME ) , ( VARIABLE ).Get() , GetRelation().Get() }; return t; }; \
     static inline const VariableSymbol<type>& GetVariable() noexcept { return VARIABLE ; }; \
-    static inline const ConditionOfComputableFunction& GetCondition() noexcept { static const ConditionOfComputableFunction b = REL; return b; }; \
+    static inline const RelationSymbol<type>& GetRelation() noexcept { static const RelationSymbol<type> CONNECT( REL , _copy ) = REL; return r; }; \
 									\
 									\
   };									\
