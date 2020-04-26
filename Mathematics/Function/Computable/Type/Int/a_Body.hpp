7d3c2e32869b7c187ae3597172a50f1db509f134
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2020-04-26 18:05:49 +0900

    Delete a_Body.hpp

--- Mathematics/Function/Computable/Type/Int/a_Body.hpp
+++
@@ -1,9 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Function/Computable/Type/Int/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-#include "../a_Body.hpp"
-
-inline IntTypeOfComputableFunction::IntTypeOfComputableFunction( const int& n ) noexcept : TypeOfComputableFunction() , m_n( n );
-
-const int& IntTypeOfComputableFunction::GetValue() const noexcept { return m_n; }
