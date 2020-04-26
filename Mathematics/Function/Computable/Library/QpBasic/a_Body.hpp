3505a18109fb581d1299a0c92954764cc41b265b
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2020-04-26 17:38:34 +0900

    Add files via upload

---
+++ Mathematics/Function/Computable/Library/QpBasic/a_Body.hpp
@@ -0,0 +1,11 @@
+// c:/Users/user/Documents/Programming/Mathematics/Function/Computable/Basic/a_Body.hpp
+
+#pragma once
+#include "a_Macro.hpp"
+
+namespace QpBasic
+{
+
+#include "../../Function/Basic/QpBasic/a_Body.hpp"
+
+}
