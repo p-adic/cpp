cce3b9eddd3d3f2c6c06ac57bab10b3c78a4b16c
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2020-04-27 11:56:43 +0900

    Delete a_Body.hpp

--- Mathematics/Function/Computable/Type/Valid/a_Body.hpp
+++
@@ -1,7 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Function/Computable/Type/Valid/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-#include "../a_Body.hpp"
-#include "../../Syntax/a_Body.hpp"
