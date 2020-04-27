6c873f46a1bff28dd7b2841b4da534b9779d439b
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2020-04-27 11:29:08 +0900

    Delete a_Body.hpp

--- Mathematics/Function/Computable/Syntax/Basic/a_Body.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Function/Computable/Syntax/Basic/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-DEFINITION_OF_GLOBAL_CONSTANT_STRING_FOR_SYMBOL( Arrow , \\uparrow );
