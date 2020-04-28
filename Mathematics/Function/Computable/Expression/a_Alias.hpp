cbc7ad301dc0fa23d1ca1079931bb7500b0fdbc0
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2020-04-28 10:52:58 +0900

    Delete a_Alias.hpp

--- Mathematics/Function/Computable/Expression/a_Alias.hpp
+++
@@ -1,8 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Function/Computable/Expression/a_Alias.hpp
-
-#pragma once
-
-using ConditionOfComputableFunction = ExpressionOfComputableFunction<bool>;
-
-template <typename Ret>
-using PrettyExpression = ExpressionOfComputableFunction<typename BaseTypeOf<Ret>::type>;
