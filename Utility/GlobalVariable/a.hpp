89ae27776f71956c6724125d4d7dd65a47518228
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2020-11-03 22:18:08 +0900

    Add files via upload

--- Utility/GlobalVariable/a.hpp
+++ Utility/GlobalVariable/a.hpp
@@ -3,13 +3,8 @@
 #pragma once
 #include "a_Macro.hpp"
 
-#include "a_Alias.hpp"
+template <typename T , typename... Args> const T* const to_GlobalConstant( const string& name , const Args&... args );
 
-template <typename T , typename Arg1 , typename... Arg2> auto to_GlobalConstant( const string& name , const Arg1& func , const Arg2&... args ) -> typename OriginalToGlobalConstant<T,Arg1,Arg2...>::type;
-template <typename T , typename Arg1 , typename... Arg2> inline auto to_GlobalConstant( const string& name , const Arg1& arg1 , const Arg2&... arg2 ) -> typename DeligatingToGlobalConstant<T,Arg1,Arg2...>::type;
+template <typename T , typename... Args> T* const to_GlobalVariable( const string& name , const Args&... args );
 
-template <typename T , typename Arg1 , typename... Arg2> auto to_GlobalVariable( const string& name , const Arg1& func , const Arg2&... args ) -> typename OriginalToGlobalVariable<T,Arg1,Arg2...>::type;
-template <typename T , typename Arg1 , typename... Arg2> inline auto to_GlobalVariable( const string& name , const Arg1& arg1 , const Arg2&... arg2 ) -> typename DeligatingToGlobalVariable<T,Arg1,Arg2...>::type;
-
-template <typename T , typename Arg1 , typename... Arg2> auto to_GlobalPointer( const string& name , const Arg1& func , const Arg2&... args ) -> typename OriginalToGlobalPointer<T,Arg1,Arg2...>::type;
-template <typename T , typename Arg1 , typename... Arg2> inline auto to_GlobalPointer( const string& name , const Arg1& arg1 , const Arg2&... arg2 ) -> typename DeligatingToGlobalPointer<T,Arg1,Arg2...>::type;
+template <typename T , typename... Args> T* to_GlobalPointer( const string& name , const Args&... args );
--- Utility/GlobalVariable/a_Body.hpp
+++ Utility/GlobalVariable/a_Body.hpp
@@ -6,15 +6,15 @@
 #include "../List/LabeledList/a_Body.hpp"
 #include "../../SFINAE/MemberFunction/Constructor/a_Body.hpp"
 
-template <typename T , typename Arg1 , typename... Arg2>
-auto to_GlobalConstant( const string& name , const Arg1& func , const Arg2&... args ) -> typename OriginalToGlobalConstant<T,Arg1,Arg2...>::type
+template <typename T , typename... Args>
+const T* const to_GlobalConstant( const string& name , const Args&... args )
 {
 
   static auto g_t = LabeledList<const T*,string>();
   
   if( ! g_t.Contain( name ) ){
     
-    const T* const p_t = new T( func( args... ) );
+    const T* const p_t = new T( args... );
     g_t.Insert( name , p_t );
 
   }
@@ -23,17 +23,15 @@ auto to_GlobalConstant( const string& name , const Arg1& func , const Arg2&... a
 
 }
 
-template <typename T , typename Arg1 , typename... Arg2> inline auto to_GlobalConstant( const string& name , const Arg1& arg1 , const Arg2&... arg2 ) -> typename DeligatingToGlobalConstant<T,Arg1,Arg2...>::type { return to_GlobalConstant<T,FunctionType<T,Arg1,Arg2...>,Arg1,Arg2...>( name , Construct<T,Arg1,Arg2...> , arg1 , arg2... ); }
-
-template <typename T , typename Arg1 , typename... Arg2>
-auto to_GlobalVariable( const string& name , const Arg1& func , const Arg2&... args ) -> typename OriginalToGlobalVariable<T,Arg1,Arg2...>::type
+template <typename T , typename... Args>
+T* const to_GlobalVariable( const string& name , const Args&... args )
 {
 
   static auto g_t = LabeledList<T*,string>();
   
   if( ! g_t.Contain( name ) ){
     
-    T* const p_t = new T( func( args... ) );
+    T* const p_t = new T( args... );
     g_t.Insert( name , p_t );
 
   }
@@ -42,23 +40,19 @@ auto to_GlobalVariable( const string& name , const Arg1& func , const Arg2&... a
 
 }
 
-template <typename T , typename Arg1 , typename... Arg2> inline auto to_GlobalVariable( const string& name , const Arg1& arg1 , const Arg2&... arg2 ) -> typename DeligatingToGlobalVariable<T,Arg1,Arg2...>::type { return to_GlobalVariable<T,FunctionType<T,Arg1,Arg2...>,Arg1,Arg2...>( name , Construct<T,Arg1,Arg2...> , arg1 , arg2... ); }
-
-template <typename T , typename Arg1 , typename... Arg2>
-auto to_GlobalPointer( const string& name , const Arg1& func , const Arg2&... args ) -> typename OriginalToGlobalPointer<T,Arg1,Arg2...>::type
+template <typename T , typename... Args>
+T* to_GlobalPointer( const string& name , const Args&... args )
 {
 
   static auto g_p_t = LabeledList<T*,string>();
 
   if( ! g_p_t.Contain( name ) ){
     
-    T* p_t = new T( func( args... ) );
-    g_p_t.Innsert( name , p_t );
+    T* p_t = new T( args... );
+    g_p_t.Insert( name , p_t );
 
   }
   
   return g_p_t( name );
 
 }
-
-template <typename T , typename Arg1 , typename... Arg2> inline auto to_GlobalPointer( const string& name , const Arg1& arg1 , const Arg2&... arg2 ) -> typename DeligatingToGlobalPointer<T,Arg1,Arg2...>::type { return to_GlobalPointer<T,FunctionType<T,Arg1,Arg2...>,Arg1,Arg2...>( name , Construct<T,Arg1,Arg2...> , arg1 , arg2... ); }
