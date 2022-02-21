a9d1ded1c92b089d5a933d48a6a35af724fea008
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2022-02-21 11:35:30 +0900

    Add files via upload

--- Utility/VLArray/a_Body.hpp
+++ Utility/VLArray/a_Body.hpp
@@ -460,7 +460,7 @@ template <typename T> inline VLMatrix<2,T> to_VLMatrix( const uint& N0 , const u
 template <typename T> inline VLMatrix<3,T> to_VLMatrix( const uint& N0 , const uint& N1 , const uint& N2 , const T& t){ return to_VLArray( N2 , to_VLMatrix( N0 , N1 , t ) ); }
 
 template <typename T> template <typename... Arg>
-void Frown( const Arg&... args )
+VLArray<T> Frown( const Arg&... args )
 {
 
   VLArray<T> a{};
