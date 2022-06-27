5297f1e345d83df3e92f7f31edc8d7b5f0ca682a
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2022-06-27 11:25:49 +0900

    Add files via upload

--- Mathematics/Arithmetic/Mod/VLArray/a.cpp
+++ Mathematics/Arithmetic/Mod/VLArray/a.cpp
@@ -9,12 +9,12 @@
 void LazyEvaluationOfModularInverse( const INT_TYPE_FOR_MOD& M , const INT_TYPE_FOR_MOD& n , INT_TYPE_FOR_MOD& m )
 {
 
-  static VLArray<VLArray<INT_TYPE_FOR_MOD> > a{};
-  VLArray<INT_TYPE_FOR_MOD>* p = nullptr;
+  static VLArray<VLArray<VLArray<INT_TYPE_FOR_MOD> > > a{};
+  VLArray<VLArray<INT_TYPE_FOR_MOD> >* p = nullptr;
 
   for( auto itr = a.begin() , end = a.end() ; itr != end && p != nullptr ; itr++ ){
 
-    if( itr->front() == M ){
+    if( itr->front().back() == M ){
 
       p = &( *itr );
 
@@ -24,34 +24,43 @@ void LazyEvaluationOfModularInverse( const INT_TYPE_FOR_MOD& M , const INT_TYPE_
 
   if( p == nullptr ){
 
-    a.push_back( VLArray<INT_TYPE_FOR_MOD>() );
+    a.push_back( VLArray<VLArray<INT_TYPE_FOR_MOD> >() );
     p = &( a.back() );
-    p->push_back( M );
+    VLArray<INT_TYPE_FOR_MOD> inv_pair{};
+    inv_pair.push_back( 0 );
+    inv_pair.push_back( M );
+    p->push_back( inv_pair );
 
-    for( INT_TYPE_FOR_MOD i = 1 ; i < M ; i++ ){
+  }
 
-      p->push_back( 0 );
+  for( auto itr = p->begin() , end = p->end() ; itr != end ; itr++ ){
+
+    if( itr->front() == n ){
+
+      m = itr->back();
+      return;
 
     }
 
+    if( itr->back() == n ){
+
+      m = itr->front();
+      return;
+
+    }
+    
   }
-
-  INT_TYPE_FOR_MOD& inv = ( *p )[n];
-
-  if( inv != 0 ){
-
-    m = inv;
-    return;
-
-  }
-
+  
   const INT_TYPE_FOR_MOD M_abs = M >= 0 ? M : -M;
   
   for( INT_TYPE_FOR_MOD i = 1 ; i < M ; i++ ){
 
     if( n * i % M_abs == 1 ){
 
-      inv = i;
+      VLArray<INT_TYPE_FOR_MOD> inv_pair{};
+      inv_pair.push_back( n );
+      inv_pair.push_back( i );
+      p->push_back( inv_pair );
       m = i;
       return;
       
@@ -59,7 +68,10 @@ void LazyEvaluationOfModularInverse( const INT_TYPE_FOR_MOD& M , const INT_TYPE_
 
   }
 
-  inv = M;
+  VLArray<INT_TYPE_FOR_MOD> inv_pair{};
+  inv_pair.push_back( n );
+  inv_pair.push_back( M );
+  p->push_back( inv_pair );
   m = M;
   return;
 
