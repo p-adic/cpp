585476a97bac7847e8e4f2def36fbb1ab205bbfd
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2022-07-06 11:42:20 +0900

    Add files via upload

--- Mathematics/Arithmetic/Power/a.hpp
+++ Mathematics/Arithmetic/Power/a.hpp
@@ -4,14 +4,8 @@
 
 // init * ( t ^ num )
 template <typename T , typename UINT>
-T Power( const T& t , const UINT& num , const T& init = 1 , const bool& init_is_unit = true , const bool& for_right_multiplication = true , const string& method = "normal" );
+T Power( const T& t , const UINT& num , const T& init = 1 , const bool& for_right_multiplication = true , const string& method = "normal" );
 
 template <typename T , typename UINT> inline T PowerNormalMethod( const T& t , const UINT& num , const T& init = 1 , const bool& for_right_multiplication = true );
 template <typename T , typename UINT>
-T PowerBinaryMethod( const T& t , const UINT& num , const T& init = 1 , const bool& init_is_unit = true , const bool& for_right_multiplication = true );
-
-// Power‚Ì2ã°‚Ö‚Ì•”•ª“ÁŽê‰»
-template <typename T , typename UINT>
-T Power2Power( const T& t , const UINT& d );
-
-
+T PowerBinaryMethod( const T& t , const UINT& num , const T& init = 1 , const bool& for_right_multiplication = true );
--- Mathematics/Arithmetic/Power/a_Body.hpp
+++ Mathematics/Arithmetic/Power/a_Body.hpp
@@ -3,87 +3,33 @@
 #pragma once
 #include "a.hpp"
 
+#include "../Padic/a_Body.hpp"
 
 template <typename T , typename UINT>
-inline T Power( const T& t , const UINT& num , const T& init , const bool& init_is_unit , const bool& for_right_multiplication , const string& method ) { return method == "binary" ? PowerBinaryMethod<T,UINT>( t , num , init , init_is_unit , for_right_multiplication ) : PowerNormalMethod<T,UINT>( t , num , init , for_right_multiplication ); }
+inline T Power( const T& t , const UINT& num , const T& init , const bool& for_right_multiplication , const string& method ) { return method == "binary" ? PowerBinaryMethod<T,UINT>( t , num , init , for_right_multiplication ) : PowerNormalMethod<T,UINT>( t , num , init , for_right_multiplication ); }
 
 template <typename T , typename UINT> inline T PowerNormalMethod( const T& t , const UINT& num , const T& init , const bool& for_right_multiplication ) { return num == 0 ? init : ( for_right_multiplication ? PowerNormalMethod<T,UINT>( t , num - 1 , init ) * t : t * PowerNormalMethod<T,UINT>( t , num - 1 , init ) ); }
 
 template <typename T , typename UINT>
-T PowerBinaryMethod( const T& t , const UINT& num , const T& init , const bool& init_is_unit , const bool& for_right_multiplication )
+T PowerBinaryMethod( const T& t , const UINT& num , const T& init , const bool& for_right_multiplication )
 {
 
-  if( num == 0 ){
-      
-    return init;
+  const VLArray<UINT> num_binary = AdicInt<2>::Expand( num );
+  T answer = init;
+  T power = t;
+
+  for( auto itr = num_binary.begin() , end = num_binary.end() ; itr != end ; itr++ ){
+
+    if( *itr == 1 ){
+
+      answer = for_right_multiplication ? answer * power : power * answer;
+
+    }
+
+    power = power * power;
 
   }
 
-  // 2ã°‚ÌŒvŽZŒø—¦‰»‚Ì‚½‚ß‚É‘O‰ñ‚ÌŒvŽZŒ‹‰Ê‚ð‹L˜^
-  static T t_prev{};
-  static UINT d = 0;
-  static UINT two_power = 1;
-
-  if( t_prev != t ){
-
-    t_prev = t;
-    d = 0;
-    two_power = 1;
-    
-  }
-
-  while( num % ( two_power + two_power ) == 0 ){
-
-    d++;
-    two_power = two_power + two_power;
-
-  }
-
-  const UINT num_reduced = num - two_power;
-  T power1 = Power2Power<T,UINT>( t , d );
-
-  if( num_reduced == 0 && init_is_unit ){
-
-    return power1;
-
-  }
-  
-  T power2 = PowerBinaryMethod<T,UINT>( t , num_reduced , init , init_is_unit , for_right_multiplication );
-  return for_right_multiplication ? power2 * power1 : power1 * power2;
-
-}
-
-// Power‚Ì2ã°‚Ö‚Ì•”•ª“ÁŽê‰»
-template <typename T , typename UINT>
-T Power2Power( const T& t , const UINT& d )
-{
-
-  if( d == 0 ){
-
-    return t;
-
-  }
-
-  // Power‚ÌŒvŽZŒø—¦‰»‚Ì‚½‚ß‚É‘O‰ñ‚ÌŒvŽZŒ‹‰Ê‚ð‹L˜^
-  static T t_prev{};
-  static UINT d_prev = 0;
-  static T power_prev{};
-
-  if( t_prev != t ){
-
-    t_prev = t;
-    d_prev = 0;
-    power_prev = t;
-    
-  }
-
-  while( d != d_prev ){
-
-    d_prev++;
-    power_prev = power_prev * power_prev;
-
-  }
-  
-  return power_prev;
+  return answer;
 
 }
