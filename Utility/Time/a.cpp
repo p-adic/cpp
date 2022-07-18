756fcfa326d426c163f7546cc88bbbd17a3710fb
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2022-07-18 09:10:58 +0900

    Add files via upload

--- Utility/Time/a.cpp
+++ Utility/Time/a.cpp
@@ -4,44 +4,46 @@
 #include "a_Body.hpp"
 #include <chrono>
 
-void StopWatch( const int& how_many_times , const string& process_name )
+static inline chrono::system_clock::time_point& GetWatch() { static chrono::system_clock::time_point watch; return watch; }
+static inline int& GetWatchCount() { static int watch_count; return watch_count; }
+
+void StartWatch( const string& process_name )
 {
 
-  static bool off = true;
-  static chrono::system_clock::time_point watch;
-  static int count;
+  if( GetWatchCount() == 0 ){
 
-  if( off ){
-
-    off = false;
-    count = 0;
-    
     if( process_name == "nothing" ){
 
-      cout << "Start watching execution time: ";
+      cout << "Counting execution time: ";
 
     } else {
 
-      cout << "Start watching execution time of " << process_name << ": ";
+      cout << "Counting execution time of " << process_name << ": ";
 
     }
     
-    watch = chrono::system_clock::now();
+    GetWatch() = chrono::system_clock::now();
 
-  } else {
-
-    count++;
-
-    if( count == how_many_times ){
-
-      const double time = static_cast<double>( chrono::duration_cast<chrono::microseconds>( chrono::system_clock::now() - watch ).count() / 1000.0 );
-      cout << time << " [ms]" << endl;
-      off = true;
-
-    }
-    
   }
+  
+  return;
 
+}
+
+void StopWatch( const int& how_many_times )
+{
+
+  int& count = GetWatchCount();
+  count++;
+
+  if( count == how_many_times ){
+
+    const double time = static_cast<double>( chrono::duration_cast<chrono::microseconds>( chrono::system_clock::now() - GetWatch() ).count() / 1000.0 );
+    cout << time << " [ms] par " << how_many_times << " executions" << endl;
+    count = 0;
+
+  }
+  
   return;
 
 }
--- Utility/Time/a.hpp
+++ Utility/Time/a.hpp
@@ -2,4 +2,5 @@
 
 #pragma once
 
-void StopWatch( const int& how_many_times = 1 , const string& process_name = "nothing" )
+void StartWatch( const string& process_name = "nothing" );
+void StopWatch( const int& how_many_times = 1 );
