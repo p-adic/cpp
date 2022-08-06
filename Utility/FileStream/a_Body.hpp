024e1ef57f47ff498006517bebebabb321a5cbc6
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2022-08-06 10:22:17 +0900

    Add files via upload

--- Utility/FileStream/a_Body.hpp
+++ Utility/FileStream/a_Body.hpp
@@ -12,4 +12,6 @@ inline bool FileHolder::Contain( const string& filename ) const { return Ofstrea
 
 inline LabeledList<SmartPointer<ofstream>,string>& FileHolder::OfstreamTable() { static auto list = LabeledList<SmartPointer<ofstream>,string>( WrappedType<DirectProduct<string> >( "" ) , WrappedType<SmartPointer<ofstream> >( "dummy file name for OfstreamTable" , ios::in ) ); return list; }
 
+inline void DeleteContents( const string& filename ) { ToOfstream( filename , ios::trunc ); }
+
 template <typename... T> inline void WriteIn( const string& filename , const T&... t ) { ToOfstream( filename ) << to_string_Normalised( t... ) << endl;  }
