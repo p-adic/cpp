9b7d99965d45758f6cbd69e831321424d1256a5c
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2022-08-19 12:40:02 +0900

    Add files via upload

--- Mathematics/SetTheory/DirectProduct/AffineSpace/a_Body.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/a_Body.hpp
@@ -4,7 +4,7 @@
 #include "a.hpp"
 
 template <typename T , uint D> inline AffineSpace<T,D>::AffineSpace() : m_v() {};
-template <typename T , uint D> template<uint E> inline AffineSpace<T,D>::AffineSpace( const T (&v)[E] ) : m_v { Substitute<E>( v ); }
+template <typename T , uint D> template<uint E> inline AffineSpace<T,D>::AffineSpace( const T (&v)[E] ) : m_v() { Substitute<E>( v ); }
 template <typename T , uint D> template<uint E> inline AffineSpace<T,D>& AffineSpace<T,D>::operator=( const AffineSpace<T,E>& x ) { Set<E>( x.m_v ); return *this; }
 
 template <typename T , uint D> inline T& AffineSpace<T,D>::operator[]( const uint& i ) { return m_v[i]; }
