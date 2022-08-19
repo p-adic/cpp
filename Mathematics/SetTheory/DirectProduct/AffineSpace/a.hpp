d9335901638b503454361fb1d6a8f13b4ea3ce5a
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2022-08-19 15:14:27 +0900

    Add files via upload

--- Mathematics/SetTheory/DirectProduct/AffineSpace/a.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/a.hpp
@@ -13,13 +13,17 @@ private:
 public:
   inline AffineSpace();
   inline AffineSpace( const initializer_list<T> init );
-  template<uint E> inline AffineSpace( const T (&v)[E] );
-  template<uint E> inline AffineSpace<T,D>& operator=( const AffineSpace<T,E>& x );
+  template <uint E> inline AffineSpace( const T (&v)[E] );
+  template <uint E> inline AffineSpace( const AffineSpace<T,E>& x );
+
+  template <uint E> inline AffineSpace<T,D>& operator=( const AffineSpace<T,E>& x );
   T& operator[]( const uint& i );
   const T& operator[]( const uint& i ) const;
   
 private:
-  template<uint E> void Set( const T (&v)[E] );
-  template<uint E> void Substitute( const T (&v)[E] );
+  template <uint E> void Set( const T (&v)[E] );
+  void Substitute( const initializer_list<T> init );
+  template <uint E> void Substitute( const T (&v)[E] );
+  template <uint E> void Substitute( const AffineSpace<T,E>& x );
 
 };
--- Mathematics/SetTheory/DirectProduct/AffineSpace/a_Body.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/a_Body.hpp
@@ -4,9 +4,11 @@
 #include "a.hpp"
 
 template <typename T , uint D> inline AffineSpace<T,D>::AffineSpace() : m_v() {};
-template <typename T , uint D> inline AffineSpace<T,D>::AffineSpace( const initializer_list<T> init ) : m_v( init ) {}
-template <typename T , uint D> template<uint E> inline AffineSpace<T,D>::AffineSpace( const T (&v)[E] ) : m_v() { Substitute<E>( v ); }
-template <typename T , uint D> template<uint E> inline AffineSpace<T,D>& AffineSpace<T,D>::operator=( const AffineSpace<T,E>& x ) { Set<E>( x.m_v ); return *this; }
+template <typename T , uint D> inline AffineSpace<T,D>::AffineSpace( const initializer_list<T> init ) : m_v() { Substitute( init ); }
+template <typename T , uint D> template <uint E> inline AffineSpace<T,D>::AffineSpace( const T (&v)[E] ) : m_v() { Substitute<E>( v ); }
+template <typename T , uint D> template <uint E> inline AffineSpace<T,D>::AffineSpace( const AffineSpace<T,E>& x ) : m_v() { Substitute<E>( x ); }
+
+template <typename T , uint D> template <uint E> inline AffineSpace<T,D>& AffineSpace<T,D>::operator=( const AffineSpace<T,E>& x ) { Set<E>( x.m_v ); return *this; }
 
 template <typename T , uint D> inline T& AffineSpace<T,D>::operator[]( const uint& i ) { return m_v[i]; }
 template <typename T , uint D> inline const T& AffineSpace<T,D>::operator[]( const uint& i ) const { return m_v[i]; }
@@ -28,6 +30,26 @@ void AffineSpace<T,D>::Set( const T (&v)[E] )
   
 }
 
+template <typename T , uint D>
+void AffineSpace<T,D>::Substitute( const initializer_list<T> init )
+{
+
+  const uint size = init.size();
+  const uint min = size < D ? size : D;
+  auto itr = init.begin();
+
+  for( uint i = 0 ; i < min ; i++ ){
+
+    m_v[i] = *itr;
+    itr++;
+    
+  }
+
+  return;
+  
+
+}
+
 template <typename T , uint D> template <uint E>
 void AffineSpace<T,D>::Substitute( const T (&v)[E] )
 {
@@ -37,6 +59,22 @@ void AffineSpace<T,D>::Substitute( const T (&v)[E] )
   for( uint i = 0 ; i < min ; i++ ){
 
     m_v[i] = v[i];
+    
+  }
+
+  return;
+  
+}
+
+template <typename T , uint D> template <uint E>
+void AffineSpace<T,D>::Substitute( const AffineSpace<T,E>& x )
+{
+
+  constexpr const uint min = E < D ? E : D;
+
+  for( uint i = 0 ; i < min ; i++ ){
+
+    m_v[i] = x[i];
     
   }
 
