a7d0f567da106c1a7d64eebd2533855d17ea6761
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2022-10-24 11:48:26 +0900

    Add files via upload

--- Mathematics/SetTheory/DirectProduct/AffineSpace/IntervalAddBIT/a_Body.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/IntervalAddBIT/a_Body.hpp
@@ -14,10 +14,10 @@ template <typename T , int N> inline IntervalAddBIT<T,N>& IntervalAddBIT<T,N>::o
 
 template <typename T , int N> inline void IntervalAddBIT<T,N>::Add( const int& i , const T& n ) { IntervalAdd( i , i , n ); }
 
-template <typename T , int N> inline void IntervalAddBIT<T,N>::IntervalAdd( const int& i_start , const int& i_final , const T& n ) { m_bit_0.Add( i_start , - n * ( i_start - 1 ) ); m_bit_0.Add( i_final + 1 , n * i_final ); m_bit_1.Add( i_start , n ); m_bit_0.Add( i_final + 1 , - n ); }
+template <typename T , int N> inline void IntervalAddBIT<T,N>::IntervalAdd( const int& i_start , const int& i_final , const T& n ) { m_bit_0.Add( i_start , - n * ( i_start - 1 ) ); m_bit_0.Add( i_final + 1 , n * i_final ); m_bit_1.Add( i_start , n ); m_bit_1.Add( i_final + 1 , - n ); }
 
 
-template <typename T , int N> inline T IntervalAddBIT<T,N>::InitialSegmentSum( const int& i_final ) { return m_bit_0.InitialSegmentSum( i_final ) + i_final * m_bit_0.InitialSegmentSum( i_final ); }
+template <typename T , int N> inline T IntervalAddBIT<T,N>::InitialSegmentSum( const int& i_final ) { return m_bit_0.InitialSegmentSum( i_final ) + i_final * m_bit_1.InitialSegmentSum( i_final ); }
 
 template <typename T , int N> inline T IntervalAddBIT<T,N>::IntervalSum( const int& i_start , const int& i_final ) { return InitialSegmentSum( i_final ) - InitialSegmentSum( i_start - 1 ); }
 
