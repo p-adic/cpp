ac77fb1f9c5394c1a76720eb6f1c5ae60cf0e40e
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2022-11-08 21:20:59 +0900

    Add files via upload

---
+++ Mathematics/Arithmetic/QuadraticInteger/a_Alias.hpp
@@ -0,0 +1,6 @@
+// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/QuadraticInteger/a_Alias.hpp
+
+#pragma once
+
+using IntTypeForQuadraticInteger = int;
+// using IntTypeForQuadraticInteger = ll;
