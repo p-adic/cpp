a8e5dc3354fb47089193e2578f7eba54236e6a34
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2022-11-09 09:46:51 +0900

    Delete Mathematics/Arithmetic/QuadraticInteger directory

--- Mathematics/Arithmetic/QuadraticInteger/a_Alias.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/QuadraticInteger/a_Alias.hpp
-
-#pragma once
-
-using IntTypeForQuadraticInteger = int;
-// using IntTypeForQuadraticInteger = ll;
