5c7352a3c674db697f8fa7c9888d9c9869f68d80
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2022-12-29 17:42:29 +0900

    Delete Mathematics/Arithmetic/Mod/VLArray directory

--- Mathematics/Arithmetic/Mod/VLArray/a.hpp
+++
@@ -1,7 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/Mod/VLArray/a.hpp
-
-#pragma once
-#include "../a_Alias.hpp"
-
-// Mが素数であるかn <= 1000000の時のみサポート
-void LazyEvaluationOfModularInverse( const INT_TYPE_FOR_MOD& M , const INT_TYPE_FOR_MOD& n , INT_TYPE_FOR_MOD& m );
