05f947240af33699ca10c7aa28952b07163aabc0
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-01-03 20:26:14 +0900

    Delete a_Alias.hpp

--- Mathematics/LinearAlgebra/a_Alias.hpp
+++
@@ -1,9 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/LinearAlgebra/a_Alias.hpp
-
-#pragma once
-
-template <typename T>
-using LineTypeForMatrix = vector<T>;
-
-template <typename T>
-using TableTypeForMatrix = LineTypeForMatrix<LineTypeForMatrix<T> >;
