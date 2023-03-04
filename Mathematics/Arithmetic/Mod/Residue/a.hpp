1a78b79a13b1a33ed6dfd0ebcb4a941936ee0837
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-03-04 21:56:54 +0900

    Add files via upload

--- Mathematics/Arithmetic/Mod/Residue/a.hpp
+++ Mathematics/Arithmetic/Mod/Residue/a.hpp
@@ -2,10 +2,10 @@
 
 #pragma once
 
-template <typename INT , INT M> inline constexpr INT Residue( const INT& n ) noexcept;
+template <uint M , typename INT> inline constexpr INT Residue( const INT& n ) noexcept;
 // nが参照で渡されるのでnが変化することに注意。
-template <typename INT , INT M> inline constexpr INT Residue( INT& n ) noexcept;
-template <typename INT , INT M> inline constexpr INT Residue( INT&& n ) noexcept;
+template <uint M , typename INT> inline constexpr INT Residue( INT& n ) noexcept;
+template <uint M , typename INT> inline constexpr INT Residue( INT&& n ) noexcept;
 
 template <typename INT1 , typename INT2> inline constexpr INT1 Residue( const INT1& n , const INT2& M ) noexcept;
 // nが参照で渡されるのでnが変化することに注意。
