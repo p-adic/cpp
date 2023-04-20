8eac89e69fffffabddd3695c22142dc8952d4a98
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-04-21 07:21:23 +0900

    Delete Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalMaxBIT directory

--- Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalMaxBIT/a.hpp
+++
@@ -1,7 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalMaxBIT/a.hpp
-
-#pragma once
-#include "a_Macro.hpp"
-
-DECRALATION_OF_INTERVAL_MAX_BIT( Max );
-DECRALATION_OF_INTERVAL_MAX_BIT( Min );
--- Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalMaxBIT/a_Body.hpp
+++
@@ -1,7 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalMaxBIT/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-DEFINITION_OF_INTERVAL_MAX_BIT( Max , max );
-DEFINITION_OF_INTERVAL_MAX_BIT( Min , min );
