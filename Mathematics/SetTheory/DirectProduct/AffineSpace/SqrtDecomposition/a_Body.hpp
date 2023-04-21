618f03d8f7570ad7ef520c8d52e2e58cdac8967b
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-04-22 08:51:51 +0900

    Add files via upload

--- Mathematics/SetTheory/DirectProduct/AffineSpace/SqrtDecomposition/a_Body.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/SqrtDecomposition/a_Body.hpp
@@ -14,10 +14,12 @@ template <typename T , int N , int N_sqrt> inline constexpr const T& SqrtDecompo
 template <typename T , int N , int N_sqrt> inline constexpr T SqrtDecomposition<T,N,N_sqrt>::IntervalSum( const int& i_start , const int& i_final )
 {
   
-  int j_0 = ( i_start + N_sqrt - 1 ) / N_sqrt;
-  int j_1 = max( j_0 , ( i_final + 1 ) / N_sqrt );
-  int i_0 = min( j_0 * N_sqrt - 1 , i_final ) ;
-  int i_1 = max( i_start , j_1 * N_sqrt );
+  const int i_min = max( i_start , 0 );
+  const int i_max = min( i_final , N - 1 );
+  const int d_0 = ( i_min + N_sqrt - 1 ) / N_sqrt;
+  const int d_1 = max( d_0 , ( i_max + 1 ) / N_sqrt );
+  const int i_0 = min( d_0 * N_sqrt - 1 , i_max ) ;
+  const int i_1 = max( i_min , d_1 * N_sqrt );
   T answer{ 0 };
   
   for( int i = i_start ; i < i_0 ; i++ ){
@@ -26,9 +28,9 @@ template <typename T , int N , int N_sqrt> inline constexpr T SqrtDecomposition<
 
   }
   
-  for( int j = j_0 ; j < j_1 ; j++ ){
+  for( int d = d_0 ; d < d_1 ; d++ ){
 
-    answer += m_b[j];
+    answer += m_b[d];
 
   }
 
