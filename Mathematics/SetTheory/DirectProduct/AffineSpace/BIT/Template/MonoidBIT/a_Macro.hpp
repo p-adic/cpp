f7943485326ba2f11cf5897ece207005ab4e9a52
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-04-21 14:46:28 +0900

    Add files via upload

---
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/Template/MonoidBIT/a_Macro.hpp
@@ -0,0 +1,4 @@
+// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/BIT/Template/MonoidBIT/a_Macro.hpp
+
+#pragma once
+#define TEMPLETE_ARGUMENTS_FOR_MONOID_BIT typename T , T m_T(const T&,const T&) , const T& e_T() , int N
