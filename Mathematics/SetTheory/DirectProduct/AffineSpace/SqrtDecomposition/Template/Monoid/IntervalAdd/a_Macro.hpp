90c7a3b97011bf73838a18f296f0211b1fbed763
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-04-22 09:13:41 +0900

    Add files via upload

---
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/SqrtDecomposition/Template/Monoid/IntervalAdd/a_Macro.hpp
@@ -0,0 +1,4 @@
+// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/SqrtDecomposition/Template/Monoid/IntervalAdd/a_Macro.hpp
+
+#pragma once
+#define TEMPLATE_ARGUMENTS_FOR_INTERVAL_ADD_SQRT_DECOMPOSITION typename T , T m_T(const T&,const T&) , const T& e_T() , int N , int N_sqrt
