fafd541417b7ac65a71e6b2406cd86761bedc726
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-04-22 10:39:14 +0900

    Update a_Macro.hpp

--- Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/Template/Monoid/a_Macro.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/Template/Monoid/a_Macro.hpp
@@ -1,4 +1,4 @@
-// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/Template/MonoidBIT/a_Macro.hpp
+// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/Template/Monoid/a_Macro.hpp
 
 #pragma once
 #define TEMPLATE_ARGUMENTS_FOR_MONOID_BIT typename T , T m_T(const T&,const T&) , const T& e_T() , int N
