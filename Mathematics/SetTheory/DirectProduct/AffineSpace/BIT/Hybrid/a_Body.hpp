282738a75f937eeb35d5e56dd53ec6490ca16eea
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-04-22 10:14:11 +0900

    Update and rename Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/HybridBIT/a_Body.hpp to Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/Hybrid/a_Body.hpp

--- Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/Hybrid/a_Body.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/Hybrid/a_Body.hpp
@@ -1,4 +1,4 @@
-// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/HybridBIT/a_Body.hpp
+// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/Hybrid/a_Body.hpp
 
 #pragma once
 #include "a.hpp"
