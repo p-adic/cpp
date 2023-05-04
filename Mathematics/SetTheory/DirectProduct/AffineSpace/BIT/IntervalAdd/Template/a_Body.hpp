249064d1391a9034d5b42489bf75e46ac8a3adfd
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-05-05 03:23:58 +0900

    Add files via upload

--- Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalAdd/Template/a_Body.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalAdd/Template/a_Body.hpp
@@ -17,8 +17,8 @@ template <TEMPLATE_ARGUMENTS_FOR_BIT> inline void AbstractIntervalAddBIT<T,m_T,e
 template <TEMPLATE_ARGUMENTS_FOR_BIT> inline void AbstractIntervalAddBIT<T,m_T,e_T,i_T,N>::IntervalAdd( const int& i_start , const int& i_final , const T& n ) { m_bit_0.Add( i_start , prod( i_T( n ) , ( i_start - 1 ) ) ); m_bit_0.Add( i_final + 1 , prod( n , i_final ) ); m_bit_1.Add( i_start , n ); m_bit_1.Add( i_final + 1 , i_T( n ) ); }
 
 
-template <TEMPLATE_ARGUMENTS_FOR_BIT> inline T AbstractIntervalAddBIT<T,m_T,e_T,i_T,N>::InitialSegmentSum( const int& i_final ) { return m_T( m_bit_0.InitialSegmentSum( i_final ) , prod( m_bit_1.InitialSegmentSum( i_final ) , i_final ) ); }
+template <TEMPLATE_ARGUMENTS_FOR_BIT> inline T AbstractIntervalAddBIT<T,m_T,e_T,i_T,N>::InitialSegmentSum( const int& i_final ) const { return m_T( m_bit_0.InitialSegmentSum( i_final ) , prod( m_bit_1.InitialSegmentSum( i_final ) , i_final ) ); }
 
-template <TEMPLATE_ARGUMENTS_FOR_BIT> inline T AbstractIntervalAddBIT<T,m_T,e_T,i_T,N>::IntervalSum( const int& i_start , const int& i_final ) { return m_T( i_T( InitialSegmentSum( i_start - 1 ) ) , InitialSegmentSum( i_final ) ); }
+template <TEMPLATE_ARGUMENTS_FOR_BIT> inline T AbstractIntervalAddBIT<T,m_T,e_T,i_T,N>::IntervalSum( const int& i_start , const int& i_final ) const { return m_T( i_T( InitialSegmentSum( i_start - 1 ) ) , InitialSegmentSum( i_final ) ); }
 
-template <TEMPLATE_ARGUMENTS_FOR_BIT>  inline T AbstractIntervalAddBIT<T,m_T,e_T,i_T,N>::prod( const T& n , int exponent ) { T answer = AbstractBIT<T,m_T,e_T,i_T,N>::g_e; while( exponent-- != 0 ){ answer = m_T( answer , n ); } return answer; }
+template <TEMPLATE_ARGUMENTS_FOR_BIT>  inline T AbstractIntervalAddBIT<T,m_T,e_T,i_T,N>::prod( const T& n , int exponent ) const { T answer = AbstractBIT<T,m_T,e_T,i_T,N>::g_e; while( exponent-- != 0 ){ answer = m_T( answer , n ); } return answer; }
