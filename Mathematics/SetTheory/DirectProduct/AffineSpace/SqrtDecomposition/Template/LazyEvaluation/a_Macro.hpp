69a0ad7ecc39c5d6783c9af5d3beb18128282fe8
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-05-11 17:16:23 +0900

    Add files via upload

---
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/SqrtDecomposition/Template/LazyEvaluation/a_Macro.hpp
@@ -0,0 +1,4 @@
+// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/SqrtDecomposition/Template/LazyEvaluation/a_Macro.hpp
+
+#pragma once
+#define TEMPLATE_ARGUMENTS_FOR_LAZY_SQRT_DECOMPOSITION typename T , T m_T(const T&,const T&) , const T& e_T() , typename U , U m_U(const U&,const U&) , const U& e_U() , U o_U(const T&,const U&) , int N , int N_sqrt
