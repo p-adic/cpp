082d6cf496a9612176881da2224dfdc5a459e826
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-05-31 19:29:00 +0900

    Add files via upload

--- Utility/Search/BreadthFirst/a_Macro.hpp
+++ Utility/Search/BreadthFirst/a_Macro.hpp
@@ -2,8 +2,8 @@
 
 #pragma once
 
-// Reset‚Ím_found‚à‰Šú‰»
-// Shift‚Ím_found‚Í”ñ‰Šú‰»
+// Reset‚Ím_found‚Æm_prev‚à‰Šú‰»
+// Shift‚Ím_found‚Æm_prev‚Í”ñ‰Šú‰»
 #define DECLARATION_OF_FIRST_SEARCH( BREADTH )				\
   template <int V_max,list<int> E(const int&)>				\
   class BREADTH ## FirstSearch						\
@@ -31,11 +31,11 @@
   };									\
 
 #define DEFINITION_OF_FIRST_SEARCH( BREADTH , PUSH )			\
-  template <int V_max,list<int> E(const int&)> inline BREADTH ## FirstSearch<V_max,E>::BREADTH ## FirstSearch( const int& V ) : m_V( V ) , m_init() , m_next() , m_found() , m_prev() { for( int i = 0 ; i < V ; i++ ){ m_prev[i] = -1; } } \
+  template <int V_max,list<int> E(const int&)> inline BREADTH ## FirstSearch<V_max,E>::BREADTH ## FirstSearch( const int& V ) : m_V( V ) , m_init() , m_next() , m_found() , m_prev() { for( int i = 0 ; i < m_V ; i++ ){ m_prev[i] = -1; } } \
   template <int V_max,list<int> E(const int&)> inline BREADTH ## FirstSearch<V_max,E>::BREADTH ## FirstSearch( const int& V , const int& init ) : BREADTH ## FirstSearch( V ) { m_init = init; m_next.push_back( m_init ); m_found[m_init] = true; } \
 									\
-  template <int V_max,list<int> E(const int&)> inline void BREADTH ## FirstSearch<V_max,E>::Reset( const int& init ) { m_init = init; assert( m_init < m_V ); m_next.clear(); m_next.push_back( m_init ); for( int i = 0 ; i < m_V ; i++ ){ m_found[i] = i == m_init; } } \
-  template <int V_max,list<int> E(const int&)> inline void BREADTH ## FirstSearch<V_max,E>::Shift( const int& init ) { m_init = init; assert( m_init < m_V ); m_next.clear(); if( ! m_found[m_init] ){ m_next.push_back( m_init ); m_found[m_init] = true;} } \
+  template <int V_max,list<int> E(const int&)> inline void BREADTH ## FirstSearch<V_max,E>::Reset( const int& init ) { m_init = init; assert( m_init < m_V ); m_next.clear(); m_next.push_back( m_init ); for( int i = 0 ; i < m_V ; i++ ){ m_found[i] = i == m_init; m_prev[i] = -1; } } \
+  template <int V_max,list<int> E(const int&)> inline void BREADTH ## FirstSearch<V_max,E>::Shift( const int& init ) { m_init = init; assert( m_init < m_V ); m_next.clear(); if( ! m_found[m_init] ){ m_next.push_back( m_init ); m_found[m_init] = true; m_prev[i] = -1; } } \
 									\
   template <int V_max,list<int> E(const int&)> inline bool& BREADTH ## FirstSearch<V_max,E>::Found( const int& i ) { assert( i < m_V ); return m_found[i]; } \
   template <int V_max,list<int> E(const int&)> inline const int& BREADTH ## FirstSearch<V_max,E>::Prev( const int& i ) { assert( i < m_V ); return m_prev[i]; } \
