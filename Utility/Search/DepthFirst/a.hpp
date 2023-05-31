58a3eee2be5b982a86bb79baa34c631f65076c4d
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-05-31 11:14:11 +0900

    Add files via upload

---
+++ Utility/Search/DepthFirst/a.hpp
@@ -0,0 +1,6 @@
+// c:/Users/user/Documents/Programming/Utility/Search/DepthFirst/a.hpp
+
+#pragma once
+#include "../BreadthFirst/a_Macro.hpp"
+
+DECLARATION_OF_FIRST_SEARCH( Depth );
---
+++ Utility/Search/DepthFirst/a_Body.hpp
@@ -0,0 +1,6 @@
+// c:/Users/user/Documents/Programming/Utility/Search/DepthFirst/a_Body.hpp
+
+#pragma once
+#include "a.hpp"
+
+DEFINITION_OF_FIRST_SEARCH( Depth , push_front );
