b5690bf88db8494f1aae5ccae2889b6c9c7d5a32
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-06-09 09:37:19 +0900

    Add files via upload

--- Mathematics/Arithmetic/Mod/Euler/a.hpp
+++ Mathematics/Arithmetic/Mod/Euler/a.hpp
@@ -1,17 +1,25 @@
 // c:/Users/user/Documents/Programming/Mathematics/Arithmetic/Mod/Euler/a.hpp
 
 #pragma once
+#include "../../Prime/Constexpr/a.hpp"
 
 template <typename INT> inline INT EulerFunction( const INT& n );
 
 // ”ñ•‰®”n‚Ì‘fˆö”‚ğP‚ÉŠi”[‚µA‘fˆö”‚Ìw”‚ğexponent‚ÉŠi”[‚·‚éB
 template <typename INT>
 INT EulerFunction( const INT& n , vector<INT>& P , vector<INT>& exponent );
+// n < val_limit‚Ì2æ ‚Ì‚Ì‚İƒTƒ|[ƒgB
+template <typename INT , INT val_limit , int length_max>
+INT EulerFunction( const PrimeEnumeration<INT,val_limit,length_max>& prime , const INT& n , vector<INT>& P , vector<INT>& exponent );
 
 template <typename INT> inline INT CarmichaelFunction( const INT& n );
 
 // ”ñ•‰®”n‚Ì‘fˆö”‚ğP‚ÉŠi”[‚µA‘fˆö”‚Ìw”‚ğexponent‚ÉŠi”[‚µA‘fˆö”‚Ìw”æ‚ğP_power‚ÉŠi”[‚·‚éB
 template <typename INT>
 INT CarmichaelFunction( const INT& n , vector<INT>& P , vector<INT>& exponent , vector<INT>& P_power );
+// n < val_limit‚Ì2æ ‚Ì‚Ì‚İƒTƒ|[ƒgB
+template <typename INT , INT val_limit , int length_max>
+INT CarmichaelFunction( const PrimeEnumeration<INT,val_limit,length_max>& prime , const INT& n , vector<INT>& P , vector<INT>& exponent , vector<INT>& P_power );
 
+// ‘fˆö”•ª‰ğ‚Ìw”‚ªexponent‚Å‚ ‚é”ñ•‰®”‚Æ‚»‚ÌCarmichaelŠÖ”‚Ì’l‚ğÅ¬Œö”{”‚Ì‘fˆö”‚Ìw”‚ğexponent‚ÉŠi”[‚µ’¼‚·B
 void CarmichaelTransformation( vector<uint>& exponent );
--- Mathematics/Arithmetic/Mod/Euler/a_Body.hpp
+++ Mathematics/Arithmetic/Mod/Euler/a_Body.hpp
@@ -3,6 +3,8 @@
 #pragma once
 #include "a.hpp"
 
+#include "../../Prime/Constexpr/a_Body.hpp"
+
 #include "../CRT/a_Body.hpp"
 #include "../../Prime/a_Body.hpp"
 
@@ -15,7 +17,7 @@ INT EulerFunction( const INT& n , vector<INT>& P , vector<INT>& exponent )
 
   SetPrimeFactorisation( n , P , exponent );
   INT answer = n;
-  INT size = P.size();
+  const INT size = P.size();
 
   for( INT i = 0 ; i < size ; i++ ){
 
@@ -28,6 +30,24 @@ INT EulerFunction( const INT& n , vector<INT>& P , vector<INT>& exponent )
 
 }
 
+template <typename INT , INT val_limit , int length_max>
+INT EulerFunction( const PrimeEnumeration<INT,val_limit,length_max>& prime , const INT& n , vector<INT>& P , vector<INT>& exponent )
+{
+
+  SetPrimeFactorisation( prime , n , P , exponent );
+  INT answer = n;
+  const INT size = P.size();
+
+  for( INT i = 0 ; i < size ; i++ ){
+
+    const INT& P_i = P[i];
+    answer -= answer / P_i;
+    
+  }
+
+  return answer;
+
+}
 
 template <typename INT> inline INT CarmichaelFunction( const INT& n ) { vector<INT> P{}; vector<INT> exponent{}; vector<INT> P_power{}; return CarmichaelFunction( n , P , exponent , P_power ); }
   
@@ -39,6 +59,29 @@ INT CarmichaelFunction( const INT& n , vector<INT>& P , vector<INT>& exponent , 
   vector<INT> exponent{};
   vector<INT> P_power{};
   SetPrimeFactorisation( n , P , exponent , P_power );
+  INT answer = 1;
+  INT size = P.size();
+
+  for( INT i = 0 ; i < size ; i++ ){
+
+    const INT& P_i = P[i];
+    const INT& P_power_i = P_power[i];
+    answer = LCM( answer , ( P_power_i - P_power_i / P_i ) );
+    
+  }
+
+  return answer;
+
+}
+
+template <typename INT , INT val_limit , int length_max>
+INT CarmichaelFunction( const PrimeEnumeration<INT,val_limit,length_max>& prime , const INT& n , vector<INT>& P , vector<INT>& exponent , vector<INT>& P_power )
+{
+
+  vector<INT> P{};
+  vector<INT> exponent{};
+  vector<INT> P_power{};
+  SetPrimeFactorisation( prime , n , P , exponent , P_power );
   INT answer = 1;
   INT size = P.size();
 
