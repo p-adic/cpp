653755eaabf4c866e4c8d6145f17e767bdeb298c
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-06-11 12:31:16 +0900

    Add files via upload

---
+++ Mathematics/Combinatorial/ZetaTransform/a.hpp
@@ -0,0 +1,246 @@
+// c:/Users/user/Documents/Programming/Mathematics/Combinatorial/ZetaTransform/a.hpp
+
+#pragma once
+#include "a_Macro.hpp"
+
+template <typename T , typename U , int size_max>
+class ZetaTransformBody
+{
+
+private:
+  int m_length;
+  map<T,int> m_memory;
+  vector<T> m_memory_inv;
+
+protected:
+  int m_size;
+  U m_val[size_max];
+
+public:
+  // 仮想継承用にダミーのデフォルトコンストラクタを設定。
+  inline ZetaTransformBody( const int& size = 0 );
+
+  inline void Add( const T& t , const U& u );
+  inline void AddAll( const U& u );
+  inline ZetaTransformBody<T,U,size_max>& operator+=( const ZetaTransformBody<T,U,size_max>& a );
+  inline void MultiplyAll( const U& u );
+  inline ZetaTransformBody<T,U,size_max>& operator*=( const ZetaTransformBody<T,U,size_max>& a );
+
+  // 子クラスの半順序のメビウス関数のデフォルトの再帰式を使うため、
+  // 再帰深度が浅い場合にしか使えない。
+  inline U Get( const T& t );
+  // muは子クラスの半順序のメビウス関数。
+  template <int mu(const T&,const T&)> inline U Get( const T& t );
+  inline const U& InitialSegmentSum( const T& t );
+
+  // f_inv_maxは定義域にr(s)を含む部分写像T->Sであり、要件
+  // (1) Sは半順序集合である。
+  // (2) r(s)は重複を持たない。（よって集合とみなす）
+  // (3) sはr(s)の最大元である。
+  // (4) 像f_inv_max(s)の要素を上界に持つTの要素全体Rへのfの制限f_Rは順序保存写像R->r(s)である。
+  // (5) r(s)の任意の要素tに対しf_inv_max(t)が逆像f_R^{-1}(t)の最大元である。
+  // を満たすfが存在する場合にのみ以下の２つをサポート。
+
+  // f( t ) = sを満たすRの要素t全体を渡る総和取得。
+  template <typename S , T f_inv_max(const S&) , list<S> r(const S&)> inline U InverseImageSum( const S& s );
+  template <typename S , T f_inv_max(const S&) , list<S> r(const S&) , int mu(const T&,const T&)> inline U InverseImageSum( const S& s );
+  // f( t ) <= sを満たすRの要素t全体を渡る総和取得。（結果的にrは使わないが要件上はrの存在が必要） 
+  template <typename S , T f_inv_max(const S&)> inline const U& IntervalInverseImageSum( const S& s );
+
+  virtual T e( const int& i );
+  virtual int e_inv( const T& t );
+  
+private:
+  virtual const U& Zero() const = 0;
+  virtual U Sum( const U& u0 , const U& u1 ) const = 0;
+  virtual U Prod( const U& u0 , const U& u1 ) const = 0;
+  virtual list<T> Sub( const T& t ) const = 0;
+  virtual list<T> Sup( const T& t ) const = 0;
+  virtual int Moevius( const T& t0 , const T& t1 ) = 0;
+
+};
+
+template <typename T , typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max>
+class SemiRingForZetaTransform :
+  virtual public ZetaTransformBody<T,U,size_max>
+{
+public:
+  inline SemiRingForZetaTransform( const int& dummy );
+  
+private:
+  inline const U& Zero() const;
+  inline U Sum( const U& u0 , const U& u1 ) const;
+  inline U Prod( const U& u0 , const U& u1 ) const;
+
+};
+
+template <typename T , list<T> E(const T&) , list<T> E_inv(const T&) , typename U , int size_max>
+class PartiallyOrderedSetForZetaTransform :
+  virtual public ZetaTransformBody<T,U,size_max>
+{
+
+private:
+  map<int,map<int,int> > m_moevius;
+
+  inline list<T> Sub( const T& t ) const;
+  inline list<T> Sup( const T& t ) const;
+  // デフォルトの再帰式であるため、再帰深度が浅い場合にしか使えない。
+  inline int Moevius( const T& t0 , const T& t1 );
+  
+};
+
+template <typename U , int size_max>
+class EnumerationForZetaTransform :
+  virtual public ZetaTransformBody<int,U,size_max>
+{
+
+private:
+  inline int e( const int& i );
+  inline int e_inv( const int& t );
+  
+};
+
+// 入力の範囲内で要件
+// (1) Eがint上の半順序<のグラフでE_invが(int,>)のグラフである。
+// を満たす場合にのみ以下をサポート。
+
+// 0による初期化O(size_max)
+// ゼータ変換前の配列による初期化O(始切片のサイズの総和)
+
+// 一点加算O(終切片[t,∞)のサイズ)
+// 全体加算O(size)
+// 各点加法O(size)
+// 全体乗算O(size)
+// (int,<)がjoin半束である場合の畳み込み乗法O(size)
+
+// 一点取得O(始切片(-∞,t]のサイズ×メビウス関数の計算量)
+// 区間和取得O(1)
+
+// 逆像和取得O(始切片(-∞,f_inv_max(r_max)]のサイズ×メビウス関数の計算量)
+// 区間逆像和取得O(1)
+template <list<int> E(const int&) , list<int> E_inv(const int&) , int size_max>
+class ZetaTransform :
+  public PartiallyOrderedSetForZetaTransform<int,E,E_inv,ll,size_max> ,
+  public EnumerationForZetaTransform<ll,size_max>
+{
+
+public:
+  inline ZetaTransform( const int& size );
+  inline ZetaTransform( const int& size , const ll ( &a )[size_max] );
+
+private:
+  inline const ll& Zero() const;
+  inline ll Sum( const ll& u0 , const ll& u1 ) const;
+  inline ll Prod( const ll& u0 , const ll& u1 ) const;
+
+};
+
+// 入力の範囲内で要件
+// (1) 2^digit <= size_max
+// (2) (U,a_U:U^2->U,z_U:1->U,m_U:U^2->U)が単位的環である。
+// を満たす場合にのみ以下をサポート。
+
+// z_U()による初期化O(size_max)
+// ゼータ変換前の配列による初期化O(digit 2^digit)（可換加法モノイド性を使う）
+
+// 一点加算O(2^digit)（可換加法モノイド性を使う）
+// 全体加算O(2^digit)（可換加法モノイド性だけでも実装できるが単位的半環性を使う）
+// 各点加法O(2^digit)（加法モノイド性を使う）
+// 全体乗算O(2^digit)（半環性を使う）
+// 畳み込み乗法O(2^digit)（半環性を使う）
+
+// 一点取得O(2^digit)（単位的環性を使う。愚直と同じオーダー）
+// 多点取得O(digit 2^digit)（単位的環性を使う）
+// 区間和取得O(1)（可換加法モノイド性を使う）
+// 逆像和取得O(始切片(-∞,f_inv_max(r_max)]のサイズ)（単位的環性を使う）
+// 区間逆像和取得O(1)（可換加法モノイド性を使う）
+template <typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max>
+class FastZetaTransform :
+  public SemiRingForZetaTransform<int,U,a_U,z_U,m_U,size_max> ,
+  public EnumerationForZetaTransform<U,size_max>
+{
+
+private:
+  int m_digit;
+  
+public:
+  inline FastZetaTransform( const int& digit );
+  inline FastZetaTransform( const int& digit , const U ( &a )[size_max] );
+
+  inline FastZetaTransform<U,a_U,z_U,m_U,size_max>& operator+=( const FastZetaTransform<U,a_U,z_U,m_U,size_max>& a );
+  inline FastZetaTransform<U,a_U,z_U,m_U,size_max>& operator*=( const FastZetaTransform<U,a_U,z_U,m_U,size_max>& a );
+
+  inline void FastMoeviusTransform( U ( &a )[size_max] );
+
+private:
+  inline list<int> Sub( const int& t ) const;
+  inline list<int> Sup( const int& t ) const;
+  inline int Moevius( const int& t0 , const int& t1 );
+
+};
+
+// 入力の範囲内で要件
+// (1) EがT上の半順序<のグラフでE_invが(T,>)のグラフである。
+// (2) (U,a_U:U^2->U,z_U:1->U,m_U:U^2->U)が単位的環である。
+// を満たす場合にのみ以下をサポート。
+
+// z_U()による初期化O(size_max)
+
+// 一点加算O(終切片[t,∞)のサイズ×log_2(size))（可換加法モノイド性を使う）
+// 全体加算O(size)（可換加法モノイド性だけでも実装できるが単位的半環性を使う）
+// 各点加法O(size)（加法モノイド性を使う）
+// 全体乗算O(size)（半環性を使う）
+// (T,<)がjoin半束である場合の畳み込み乗法O(size)（半環性を使う）
+
+// 一点取得O(始切片(-∞,t]のサイズ×メビウス関数の計算量×log_2(size))（単位的環性を使う）
+// 区間和取得O(log_2(size))（可換加法モノイド性を使う）
+
+// 逆像和取得O(始切片(-∞,f_inv_max(r_max)]のサイズ×メビウス関数の計算量×log_2(size))（単位的環性を使う）
+// 区間逆像和取得O(log_2(size))（可換加法モノイド性を使う）
+template <typename T , list<T> E(const T&) , list<T> E_inv(const T&) , typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max>
+class MemorisationZetaTransform :
+  public SemiRingForZetaTransform<T,U,a_U,z_U,m_U,size_max> ,
+  public PartiallyOrderedSetForZetaTransform<T,E,E_inv,U,size_max>
+{
+
+public:
+  inline MemorisationZetaTransform( const int& size );
+
+};
+
+// 入力の範囲内で要件
+// (1) EがT上の半順序<のグラフでE_invが(T,>)のグラフである。
+// (2) (U,a_U:U^2->U,z_U:1->U,m_U:U^2->U)が単位的環である。
+// (3) enum_T:int->Tとenum_T_inv:int->Tが互いに逆写像である。
+// を満たす場合にのみ以下をサポート。
+
+// z_U()による初期化O(size_max)
+
+// 一点加算O(終切片[t,∞)のサイズ)（可換加法モノイド性を使う）
+// 全体加算O(size)（可換加法モノイド性だけでも実装できるが単位的半環性を使う）
+// 各点加法O(size)（加法モノイド性を使う）
+// 全体乗算O(size)（半環性を使う）
+// (T,<)がjoin半束である場合の畳み込み乗法O(size)（半環性を使う）
+
+// 一点取得O(始切片(-∞,t]のサイズ×メビウス関数の計算量)（単位的環性を使う）
+// 区間和取得O(1)（可換加法モノイド性を使う）
+
+// 逆像和取得O(始切片(-∞,f_inv_max(r_max)]のサイズ×メビウス関数の計算量)（単位的環性を使う）
+// 区間逆像和取得O(1)（可換加法モノイド性を使う）
+template <typename T , list<T> E(const T&) , list<T> E_inv(const T&) , typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max , T enum_T(const int&) , int enum_T_inv(const T&)>
+class EnumerationZetaTransform :
+  public SemiRingForZetaTransform<T,U,a_U,z_U,m_U,size_max> ,
+  public PartiallyOrderedSetForZetaTransform<T,E,E_inv,U,size_max>
+{
+
+public:
+  inline EnumerationZetaTransform( const int& size );
+
+private:
+  inline T e( const int& i );
+  inline int e_inv( const T& t );
+
+};
+
+// 古典的なメビウス関数は../../Arithmetic/Prime/Divisor/a_Body.hppで定義
+
---
+++ Mathematics/Combinatorial/ZetaTransform/a_Body.hpp
@@ -0,0 +1,381 @@
+// c:/Users/user/Documents/Programming/Mathematics/Combinatorial/ZetaTransform/a_Body.hpp
+
+#pragma once
+#include "a.hpp"
+
+template <typename T , typename U , int size_max> inline ZetaTransformBody<T,U,size_max>::ZetaTransformBody( const int& size ) : m_length() , m_memory() , m_memory_inv() , m_size( size ) , m_val() {}
+template <typename T , typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max> inline SemiRingForZetaTransform<T,U,a_U,z_U,m_U,size_max>::SemiRingForZetaTransform( const int& dummy ) :
+  ZetaTransformBody<T,U,size_max>( dummy )
+{
+
+  using base = ZetaTransformBody<T,U,size_max>;
+  const U& zero = z_U();
+
+  if( base::m_val[0] != zero ){
+
+    for( int i = 0 ; i < base::m_size ; i++ ){
+
+      base::m_val[i] = zero;
+
+    }
+
+  }
+
+}
+
+template <list<int> E(const int&) , list<int> E_inv(const int&) , int size_max> inline ZetaTransform<E,E_inv,size_max>::ZetaTransform( const int& size ) :
+  ZetaTransformBody<int,ll,size_max>( size ) ,
+  PartiallyOrderedSetForZetaTransform<int,E,E_inv,ll,size_max>()
+{}
+
+template <list<int> E(const int&) , list<int> E_inv(const int&) , int size_max> inline ZetaTransform<E,E_inv,size_max>::ZetaTransform( const int& size , const ll ( &a )[size_max] ) :
+  ZetaTransformBody<int,ll,size_max>( size ) ,
+  PartiallyOrderedSetForZetaTransform<int,E,E_inv,ll,size_max>() ,
+  EnumerationForZetaTransform<ll,size_max>()
+{
+
+  using base = ZetaTransformBody<int,ll,size_max>;
+  
+  for( int i = 0 ; i < base::m_size ; i++ ){
+
+    ll& m_val_i = base::m_val[i];
+    list<int> sub_i = E( i );
+
+    while( ! sub_i.empty() ){
+
+      m_val_i += a[sub_i.front()];
+      sub_i.pop_front();
+
+    }
+
+  }
+
+}
+
+template <typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max> inline FastZetaTransform<U,a_U,z_U,m_U,size_max>::FastZetaTransform( const int& digit ) :
+  ZetaTransformBody<int,U,size_max>( size ) ,
+  SemiRingForZetaTransform<int,U,a_U,z_U,m_U,size_max>( size ) ,
+  EnumerationForZetaTransform<U,size_max>() ,
+  m_digit( digit )
+{}
+
+template <typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max> inline FastZetaTransform<U,a_U,z_U,m_U,size_max>::FastZetaTransform( const int& digit , const U ( &a )[size_max] ) :
+  ZetaTransformBody<int,U,size_max>( 1 << digit ) ,
+  SemiRingForZetaTransform<int,U,a_U,z_U,m_U,size_max>( size )
+{
+
+  using base = ZetaTransformBody<int,U,size_max>;
+  
+  for( int i = 0 ; i < base::m_size ; i++ ){
+
+    base::m_val[i] = a[i];
+
+  }
+
+  int power = 1;
+
+  for( int d = 0 ; d < m_digit ; d++ , power <<= 1 ){
+
+    for( int i = 0 ; i < base::m_size ; i++ ){
+
+      if( ( i & power ) != 0 ){
+
+	U& m_val_i = base::m_val[i];
+	m_val_i = a_U( m_val_i , base::m_val[i ^ power] );
+
+      }
+      
+    }
+
+  }
+  
+}
+
+template <typename T , list<T> E(const T&) , list<T> E_inv(const T&) , typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max> inline MemorisationZetaTransform<T,E,E_inv,U,a_U,z_U,m_U,size_max>::MemorisationZetaTransform( const int& size ) :
+  ZetaTransformBody<T,U,size_max>( size ) ,
+  SemiRingForZetaTransform<T,U,a_U,z_U,m_U,size_max>( size ) ,
+  PartiallyOrderedSetForZetaTransform<T,E,E_inv,U,size_max>()
+{}
+
+template <typename T , list<T> E(const T&) , list<T> E_inv(const T&) , typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max , T enum_T(const int&) , int enum_T_inv(const T&)> inline EnumerationZetaTransform<T,E,E_inv,U,a_U,z_U,m_U,size_max,enum_T,enum_T_inv>::EnumerationZetaTransform( const int& size ) :
+  ZetaTransformBody<T,U,size_max>( size ) ,
+  SemiRingForZetaTransform<T,U,a_U,z_U,m_U,size_max>( size ) ,
+  PartiallyOrderedSetForZetaTransform<T,E,E_inv,U,size_max>()
+{}
+
+template <typename T , typename U , int size_max> inline void ZetaTransformBody<T,U,size_max>::Add( const T& t , const U& u )
+{
+
+  list<T> sup = Sup( t );
+  sup.push_front( t );
+
+  while( ! sup.empty() ){
+
+    U& m_val_i = m_val[e_inv( sup.front() )];
+    m_val_i = Sum( m_val_i , u );
+    sup.pop_front();
+
+  }
+
+  return;
+
+}
+
+template <typename T , typename U , int size_max> inline void ZetaTransformBody<T,U,size_max>::AddAll( const U& u )
+{
+
+  for( int i = 0 ; i < m_size ; i++ ){
+
+    U& m_val_i = m_val[i];
+    m_val_i = Sum( m_val_i , Prod( Sub( e( i ) ).size() , u ) );
+
+  }
+  
+  return;
+
+}
+
+template <typename T , typename U , int size_max> inline ZetaTransformBody<T,U,size_max>& ZetaTransformBody<T,U,size_max>::operator+=( const ZetaTransformBody<T,U,size_max>& a )
+{
+
+  for( int i = 0 ; i < m_size ; i++ ){
+
+    U& m_val_i = m_val[i];
+    m_val_i = Sum( m_val_i , a.m_val[i] );
+
+  }
+  
+  return *this;
+
+}
+
+template <typename T , typename U , int size_max> inline void ZetaTransformBody<T,U,size_max>::MultiplyAll( const U& u )
+{
+
+  for( int i = 0 ; i < m_size ; i++ ){
+
+    U& m_val_i = m_val[i];
+    m_val_i = Prod( m_val_i , u );
+    
+  }
+  
+  return;
+
+}
+
+template <typename T , typename U , int size_max> inline ZetaTransformBody<T,U,size_max>& ZetaTransformBody<T,U,size_max>::operator*=( const ZetaTransformBody<T,U,size_max>& a )
+{
+
+  for( int i = 0 ; i < m_size ; i++ ){
+
+    U& m_val_i = m_val[i];
+    m_val_i = Prod( m_val_i , a.m_val[i] );
+
+  }
+  
+  return *this;
+
+}
+
+template <typename T , typename U , int size_max> inline U ZetaTransformBody<T,U,size_max>::Get( const T& t ) { DEFINITION_OF_GET_FOR_ZETA_TRANSFORM( Moevius ); }
+template <typename T , typename U , int size_max> template <int mu(const T&,const T&)> inline U ZetaTransformBody<T,U,size_max>::Get( const T& t ) { DEFINITION_OF_GET_FOR_ZETA_TRANSFORM( mu ); }
+
+template <typename T , typename U , int size_max> inline const U& ZetaTransformBody<T,U,size_max>::InitialSegmentSum( const T& t ) { return m_val[e_inv( t )]; }
+
+template <typename T , typename U , int size_max> template <typename S , T f_inv_max(const S&) , list<S> r(const S&)> inline U ZetaTransformBody<T,U,size_max>::InverseImageSum( const S& s ) { DEFINITION_OF_INVERSE_IMAGE_SUM_FOR_ZETA_TRANSFORM( Moevius ); }
+template <typename T , typename U , int size_max> template <typename S , T f_inv_max(const S&) , list<S> r(const S&) , int mu(const T&,const T&)> inline U ZetaTransformBody<T,U,size_max>::InverseImageSum( const S& s ) { DEFINITION_OF_INVERSE_IMAGE_SUM_FOR_ZETA_TRANSFORM( mu ); }
+
+template <typename T , typename U , int size_max> template <typename S , T f_inv_max(const S&)> inline const U& ZetaTransformBody<T,U,size_max>::IntervalInverseImageSum( const S& s ) { return m_val[e_inv( f_inv_max( s ) )]; }
+
+
+template <typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max> inline void FastZetaTransform<U,a_U,z_U,m_U,size_max>::FastMoeviusTransform( U ( &a )[size_max] )
+{
+
+  using base = ZetaTransformBody<int,U,size_max>;
+  
+  for( int i = 0 ; i < base::m_size ; i++ ){
+
+    a[i] = base::m_val[i];
+
+  }
+
+  int power = 1;
+
+  for( int d = 0 ; d < m_digit ; d++ , power <<= 1 ){
+
+    for( int i = 0 ; i < base::m_size ; i++ ){
+
+      if( ( i & power ) != 0 ){
+
+	U& a_i = a[i];
+	a_i = a_U( a_i , m_U( -1 , a[i ^ power] ) );
+
+      }
+      
+    }
+
+  }
+
+}
+
+
+template <typename T , typename U , int size_max>
+T ZetaTransformBody<T,U,size_max>::e( const int& i )
+{
+  
+  assert( i < m_length );
+  return m_memory_inv[i];
+
+}
+
+template <typename U , int size_max> inline int EnumerationForZetaTransform<U,size_max>::e( const int& i ) { return i; }
+template <typename T , list<T> E(const T&) , list<T> E_inv(const T&) , typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max , T enum_T(const int&) , int enum_T_inv(const T&)> inline T EnumerationZetaTransform<T,E,E_inv,U,a_U,z_U,m_U,size_max,enum_T,enum_T_inv>::e( const int& i ) { return enum_T( i ); }
+
+template <typename T , typename U , int size_max>
+int ZetaTransformBody<T,U,size_max>::e_inv( const T& t )
+{
+
+  if( m_memory.count( t ) == 0 ){
+
+    assert( m_length < m_size );
+    m_memory_inv.push_back( t );
+    return m_memory[t] = m_length++;
+
+  }
+  
+  return m_memory[t];
+
+}
+
+template <typename U , int size_max> inline int EnumerationForZetaTransform<U,size_max>::e_inv( const int& t ) { return t; }
+template <typename T , list<T> E(const T&) , list<T> E_inv(const T&) , typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max , T enum_T(const int&) , int enum_T_inv(const T&)> inline int EnumerationZetaTransform<T,E,E_inv,U,a_U,z_U,m_U,size_max,enum_T,enum_T_inv>::e_inv( const T& t ) { return enum_T_inv( t ); }
+
+
+template <typename T , typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max> inline const U& SemiRingForZetaTransform<T,U,a_U,z_U,m_U,size_max>::Zero() const { return z_U(); }
+template <list<int> E(const int&) , list<int> E_inv(const int&) , int size_max> inline const ll& ZetaTransform<E,E_inv,size_max>::Zero() const { static const ll zero = 0; return zero; }
+
+template <typename T , typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max> inline U SemiRingForZetaTransform<T,U,a_U,z_U,m_U,size_max>::Sum( const U& u0 , const U& u1 ) const { return a_U( u0 , u1 ); }
+template <list<int> E(const int&) , list<int> E_inv(const int&) , int size_max> inline ll ZetaTransform<E,E_inv,size_max>::Sum( const ll& u0 , const ll& u1 ) const { return u0 + u1; }
+
+template <typename T , typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max> inline U SemiRingForZetaTransform<T,U,a_U,z_U,m_U,size_max>::Prod( const U& u0 , const U& u1 ) const { return m_U( u0 , u1 ); }
+template <list<int> E(const int&) , list<int> E_inv(const int&) , int size_max> inline ll ZetaTransform<E,E_inv,size_max>::Prod( const ll& u0 , const ll& u1 ) const { return u0 * u1; }
+
+
+template <typename T , list<T> E(const T&) , list<T> E_inv(const T&) , typename U , int size_max> inline list<T> PartiallyOrderedSetForZetaTransform<T,E,E_inv,U,size_max>::Sub( const T& t ) const { return E( t ); }
+template <typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max> inline list<int> FastZetaTransform<U,a_U,z_U,m_U,size_max>::Sub( const int& t ) const
+{
+
+  list<int> sub{};
+  sub.push_back( t );
+  int sub_size = 1;
+  int power = 1;
+  
+  for( int d = 0 ; d < m_digit ; d++ , power <<= 1 ){
+
+    if( ( t & power ) != 0 ){
+
+      auto itr = sub.begin();
+
+      for( int i = 0 ; i < sub_size ; i++ , itr++ ){
+
+	sub.push_back( *itr ^ power );
+
+      }
+
+      sub_size <<= 1;
+
+    }
+
+  }
+  
+  return sub;
+
+}
+
+
+template <typename T , list<T> E(const T&) , list<T> E_inv(const T&) , typename U , int size_max> inline list<T> PartiallyOrderedSetForZetaTransform<T,E,E_inv,U,size_max>::Sup( const T& t ) const { return E_inv( t ); }
+
+template <typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max> inline list<int> FastZetaTransform<U,a_U,z_U,m_U,size_max>::Sup( const int& t ) const
+{
+
+  list<int> sup{};
+  sup.push_back( t );
+  int sup_size = 1;
+  int power = 1;
+  
+  for( int d = 0 ; d < m_digit ; d++ , power <<= 1 ){
+
+    if( ( t & power ) == 0 ){
+
+      auto itr = sup.begin();
+
+      for( int i = 0 ; i < sup_size ; i++ , itr++ ){
+
+	sup.push_back( *itr | power );
+
+      }
+
+      sup_size <<= 1;
+
+    }
+
+  }
+  
+  return sup;
+
+}
+
+
+template <typename T , list<T> E(const T&) , list<T> E_inv(const T&) , typename U , int size_max> inline int PartiallyOrderedSetForZetaTransform<T,E,E_inv,U,size_max>::Moevius( const T& t0 , const T& t1 )
+{
+
+  using base = ZetaTransformBody<T,U,size_max>;
+  const int i = base::e_inv( t0 );
+  const int j = base::e_inv( t1 );
+  map<int,int>& moevius_t0 = m_moevius[i];
+  bool found = moevius_t0.count( j ) == 1;
+  int& answer = moevius_t0[j];
+
+  if( ! found ){
+
+    if( i == j ){
+
+      answer = 1;
+
+    } else {
+
+      list<T> sub = E( t1 );
+
+      while( ! sub.empty() ){
+
+	cerr << "メビウス関数のデフォルトの再帰式が呼ばれています。" << endl;
+	cerr << "このエラー出力回数が多い場合、再帰深度が深過ぎる可能性があります。" << endl;
+	answer -= Moevius( t0 , sub.front() );
+	sub.pop_front();
+
+      }
+
+    }
+
+  }
+
+  return answer;
+
+}
+
+template <typename U , U a_U(const U&,const U&) , const U& z_U() , U m_U(const U&,const U&) , int size_max> int FastZetaTransform<U,a_U,z_U,m_U,size_max>::Moevius( const int& t0 , const int& t1 )
+{
+
+  int t = t1 ^ t0;
+  int count = 0;
+  
+  while( t != 0 ){
+
+    t -= t & -t;
+    count++;
+
+  }
+  
+  return ( count & 1 ) == 0 ? 1 : -1;
+
+}
