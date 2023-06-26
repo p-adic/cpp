cd66b1aa622b5d4be6beb8123282d42510c1bc3c
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-06-26 10:16:11 +0900

    Add files via upload

--- Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalMax/a.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalMax/a.hpp
@@ -3,5 +3,20 @@
 #pragma once
 #include "a_Macro.hpp"
 
+// 最大（最小）元による初期化O(N)
+// 配列による初期化O(N)
+
+// 一点取得O(1)
+// 区間max（min）取得O(log_2 N)
+
+// 一点更新O((log_2 N)^2)
+// max（min）による一点更新O(log_2 N)
+// max（min）による区間更新O(i_final-i_start+log_2 N)
+
+// t以上（以下）となる要素の添字の最小値の二分探索O(log_2 N)
+
+// そのうちの区間min取得と一点更新は
+// M. Dima, R. Ceterchi, Efficient Range Minimum Queries using Binary Indexed Trees, Olympiads in Informatics, 2015, Vol. 9, 39--44
+// の手法をもとに実装
 DECRALATION_OF_INTERVAL_MAX_BIT( Max );
 DECRALATION_OF_INTERVAL_MAX_BIT( Min );
--- Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalMax/a_Macro.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalMax/a_Macro.hpp
@@ -2,21 +2,6 @@
 
 #pragma once
 
-// 最大（最小）元による初期化O(N)
-// 配列による初期化O(N)
-
-// 一点取得O(1)
-// 区間max（min）取得O(log_2 N)
-
-// 一点更新O((log_2 N)^2)
-// max（min）による一点更新O(log_2 N)
-// max（min）による区間更新O(i_final-i_start+log_2 N)
-
-// t以上（以下）となる要素の添字の最小値の二分探索O(log_2 N)
-
-// そのうちの区間min取得と一点更新は
-// M. Dima, R. Ceterchi, Efficient Range Minimum Queries using Binary Indexed Trees, Olympiads in Informatics, 2015, Vol. 9, 39--44
-// の手法をもとに実装
 #define DECRALATION_OF_INTERVAL_MAX_BIT( MAX )				\
   template <typename T , int N>						\
   class Interval ## MAX ## BIT						\
@@ -264,7 +249,9 @@
     return;								\
 									\
   }									\
-									\
+
+
+#define DEFINITION_OF_BINARY_SEARCH_FOR_INTERVAL_MAX_BIT( MAX , INEQUALITY ) \
   template <typename T , int N>						\
   void Interval ## MAX ## BIT<T,N>::IntervalSet ## MAX( const int& i_start , const int& i_final , const T& n ) \
   {									\
