3ab6252d65c83c48e75cb8012832ab77ce27f1b7
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-07-09 07:35:23 +0900

    Delete Utility/Search directory

--- Utility/Search/BreadthFirst/a.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Utility/Search/BreadthFirst/a.hpp
-
-#pragma once
-#include "a_Macro.hpp"
-
-DECLARATION_OF_FIRST_SEARCH( Breadth );
--- Utility/Search/BreadthFirst/a_Body.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Utility/Search/BreadthFirst/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-DEFINITION_OF_FIRST_SEARCH( Breadth , push_back );
