b2e0027e3e7042415fa055eadf036df1483cc1f1
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-07-09 18:15:09 +0900

    Delete Mathematics/Arithmetic/Dimension directory

--- Mathematics/Arithmetic/Dimension/VLArray/a_Body.hpp
+++
@@ -1,8 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/Dimension/VLArray/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-#include "../a_Body.hpp"
-
-#include "../../../../Utility/VLArray/a_Body.hpp"
--- Mathematics/Arithmetic/Dimension/a_Alias.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/Dimension/a_Alias.hpp
-
-#pragma once
-
-using INT_TYPE_FOR_DIM = uint;
-// using INT_TYPE_FOR_DIM = ll;
