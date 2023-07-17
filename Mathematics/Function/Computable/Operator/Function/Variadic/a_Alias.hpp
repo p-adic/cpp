3aeef2578b7bde8551c8bd447a953534f626d9e7
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-07-17 10:32:27 +0900

    Delete Mathematics/Function/Computable/Operator directory

--- Mathematics/Function/Computable/Operator/Function/Variadic/a_Alias.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Function/Computable/Operator/Function/Variadic/a_Alias.hpp
-
-#pragma once
-
-template <typename Ret, typename VArg, typename... Args>
-using PrettyVariadicFunction = VariadicFunctionSymbol<typename BaseTypeOf<Ret>::type,typename BaseTypeOf<VArg>::type,typename BaseTypeOf<Args>::type...>;
