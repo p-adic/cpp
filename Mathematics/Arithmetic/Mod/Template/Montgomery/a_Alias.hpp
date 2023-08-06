6d5ac97739d27374b4d9abf07132539c5674135f
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-08-07 08:13:11 +0900

    Delete Mathematics/Arithmetic/Mod directory

--- Mathematics/Arithmetic/Mod/Template/Montgomery/a_Alias.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/Mod/Template/Montgomery/a_Alias.hpp
-
-#pragma once
-
-template <INT_TYPE_FOR_MOD M>
-using Montgomery = Mod<M,enable_if_t< ( M & 1 ) == 1 , OperatorsForMontgomery<M> > >;
