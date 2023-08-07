a3e7f41dadd187932b1c595d2f8607fdea9b3529
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-08-07 11:09:54 +0900

    Delete Mathematics/Arithmetic/Mod/Function/GCD directory

--- Mathematics/Arithmetic/Mod/Function/GCD/a.hpp
+++
@@ -1,8 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/Mod/Function/GCD/a.hpp
-
-#pragma once
-
-template <typename INT>
-INT GCD( const INT& b_0 , const INT& b_1 );
-
-template <typename INT> inline INT LCM( const INT& b_0 , const INT& b_1 );
