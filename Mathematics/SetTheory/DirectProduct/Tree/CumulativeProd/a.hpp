45b2bb42a498688dfba87f7787aa95306040e2fa
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-08-07 16:28:04 +0900

    Add files via upload

--- Mathematics/SetTheory/DirectProduct/Tree/CumulativeProd/a.hpp
+++ Mathematics/SetTheory/DirectProduct/Tree/CumulativeProd/a.hpp
@@ -5,8 +5,8 @@
 
 // 入力の範囲内で要件
 // (1) (T,m_T:T^2->T,i_T:T->T)が群である。
-// が成り立つ場合のみサポート。（単位元は引数に渡さなくてよい）
-template <typename T , T m_T(const T&,const T&), T i_T(const T&) , int size_max>
+// が成り立つ場合のみサポート。（単位元はテンプレート引数に渡さなくてよい）
+template <typename T , T m_T(const T&,const T&) , T i_T(const T&) , int size_max>
 class CumulativeProd_Body
 {
 
@@ -17,6 +17,8 @@ protected:
 
 public:
   inline CumulativeProd_Body( const int& size );
+
+  // 0 <= i,j < m_sizeの場合のみサポート。
   // iからへのpathがi=v_0->...->v_k=jの時m_a[v_0]...m_a[v_k]を
   // Prodや逆順のProdに関して計算する。
   inline T PathProd( const int& i , const int& j );
@@ -29,13 +31,20 @@ private:
 
 
 // 通常の配列上の累積積。
-template <typename T , T m_T(const T&,const T&), T i_T(const T&) , int size_max>
-class CumulativeProd
+// テンプレート引数に単位元e_T:1->Tも渡す。
+template <typename T , T m_T(const T&,const T&) , const T& e_T() , T i_T(const T&) , int size_max>
+class CumulativeProd ：
   public CumulativeProd_Body<T,m_T,i_T,size_max>
 {
 
 public:
   inline CumulativeProd( const T ( &a )[size_max] , const int& size );
+
+  // 0 <= iかつi-1 <= j < m_sizeの場合のみサポート。
+  // m_a[i]...m_a[j]をm_Tに関して計算する。
+  inline T RightProd( const int& i , const int& j );
+  // m_a[j]...m_a[i]をm_Tに関して計算する。
+  inline T LeftProd( const int& i , const int& j );
 
 private:
   inline int Parent( const int& i );
@@ -53,8 +62,8 @@ private:
 // 2^16 = 65536
 // 2^17 = 131072
 // 2^18 = 262144
-template <typename T , T m_T(const T&,const T&), T i_T(const T&) , list<int> E(const int&) , int size_max , int digit>
-class CumulativeProdOnTree
+template <typename T , T m_T(const T&,const T&) , T i_T(const T&) , list<int> E(const int&) , int size_max , int digit>
+class CumulativeProdOnTree ：
   public CumulativeProd_Body<T,m_T,i_T,size_max>
 {
 
--- Mathematics/SetTheory/DirectProduct/Tree/CumulativeProd/a_Body.hpp
+++ Mathematics/SetTheory/DirectProduct/Tree/CumulativeProd/a_Body.hpp
@@ -5,9 +5,9 @@
 
 #include "../../../../Geometry/Graph/DepthFirstSearch/Tree/a_Body.hpp"
 
-template <typename T , T m_T(const T&,const T&), T i_T(const T&) , int size_max> inline CumulativeProd_Body<T,m_T,i_T,size_max>::CumulativeProd_Body( const int& size ) : m_size( size ) , m_a() , m_a_reverse() { assert( size <= size_max ); }
+template <typename T , T m_T(const T&,const T&) , T i_T(const T&) , int size_max> inline CumulativeProd_Body<T,m_T,i_T,size_max>::CumulativeProd_Body( const int& size ) : m_size( size ) , m_a() , m_a_reverse() { assert( size <= size_max ); }
 
-template <typename T , T m_T(const T&,const T&), T i_T(const T&) , int size_max> inline CumulativeProd<T,m_T,i_T,size_max>::CumulativeProd( const T ( &a )[size_max] , const int& size ) : CumulativeProd_Body<T,m_T,i_T,size_max>( size )
+template <typename T , T m_T(const T&,const T&) , const T& e_T() , T i_T(const T&) , int size_max> inline CumulativeProd<T,m_T,e_T,i_T,size_max>::CumulativeProd( const T ( &a )[size_max] , const int& size ) : CumulativeProd_Body<T,m_T,i_T,size_max>( size )
 {
 
   using base = CumulativeProd_Body<T,m_T,i_T,size_max>;
@@ -23,7 +23,7 @@ template <typename T , T m_T(const T&,const T&), T i_T(const T&) , int size_max>
 
 }
 
-template <typename T , T m_T(const T&,const T&), T i_T(const T&) , list<int> E(const int&) , int size_max , int digit> inline CumulativeProdOnTree<T,m_T,i_T,E,size_max,digit>::CumulativeProdOnTree( const T ( &a )[size_max] , const int& V ) : CumulativeProd_Body<T,m_T,i_T,size_max>( V ) , m_dfst( V , 0 )
+template <typename T , T m_T(const T&,const T&) , T i_T(const T&) , list<int> E(const int&) , int size_max , int digit> inline CumulativeProdOnTree<T,m_T,i_T,E,size_max,digit>::CumulativeProdOnTree( const T ( &a )[size_max] , const int& V ) : CumulativeProd_Body<T,m_T,i_T,size_max>( V ) , m_dfst( V , 0 )
 {
 
   using base = CumulativeProd_Body<T,m_T,i_T,size_max>;
@@ -41,12 +41,29 @@ template <typename T , T m_T(const T&,const T&), T i_T(const T&) , list<int> E(c
 }
 
 
-template <typename T , T m_T(const T&,const T&), T i_T(const T&) , int size_max> inline  T CumulativeProd_Body<T,m_T,i_T,size_max>::PathProd( const int& i , const int& j ) { const int k = LCA( i , j ); return m_T( m_T( m_a_reverse[i] , i_T( m_a_reverse[k] ) ) , k == 0 ? m_a[j] : m_T( i_T( m_a[Parent( k ) ] ) , m_a[j] )); }
+template <typename T , T m_T(const T&,const T&) , T i_T(const T&) , int size_max> inline  T CumulativeProd_Body<T,m_T,i_T,size_max>::PathProd( const int& i , const int& j ) { assert( 0 <= i && i < m_size && 0 <= j && j < m_size ); const int k = LCA( i , j ); return m_T( m_T( m_a_reverse[i] , i_T( m_a_reverse[k] ) ) , k == 0 ? m_a[j] : m_T( i_T( m_a[Parent( k ) ] ) , m_a[j] )); }
+
+template <typename T , T m_T(const T&,const T&) , const T& e_T() , T i_T(const T&) , int size_max> inline T CumulativeProd<T,m_T,e_T,i_T,size_max>::RightProd( const int& i , const int& j )
+{
+  
+  assert( i - 1 <= j );
+  using base = CumulativeProd_Body<T,m_T,i_T,size_max>;
+  return i <= j ? i == 0 ? base::m_a[j] : m_T( i_T( base::m_a[i-1] ) , base::m_a[j] ) : e_T();
+
+}
+
+template <typename T , T m_T(const T&,const T&) , const T& e_T() , T i_T(const T&) , int size_max> inline T CumulativeProd<T,m_T,e_T,i_T,size_max>::LeftProd( const int& i , const int& j )
+{
+  
+  assert( i - 1 <= j );
+  using base = CumulativeProd_Body<T,m_T,i_T,size_max>;
+  return i <= j ? i == 0 ? base::m_a_reverse[j] : m_T( base::m_a_reverse[j] , i_T( base::m_a_reverse[i - 1] ) ) : e_T();
+
+}
+
+template <typename T , T m_T(const T&,const T&) , T i_T(const T&) , int size_max> inline int CumulativeProd<T,m_T,e_T,i_T,size_max>::Parent( const int& i ) { return i - 1; }
+template <typename T , T m_T(const T&,const T&) , T i_T(const T&) , list<int> E(const int&) , int size_max , int digit> inline int CumulativeProdOnTree<T,m_T,i_T,E,size_max,digit>::Parent( const int& i ) { return m_dfst.Parent( i ); }
 
 
-template <typename T , T m_T(const T&,const T&), T i_T(const T&) , int size_max> inline int CumulativeProd<T,m_T,i_T,size_max>::Parent( const int& i ) { return i - 1; }
-template <typename T , T m_T(const T&,const T&), T i_T(const T&) , list<int> E(const int&) , int size_max , int digit> inline int CumulativeProdOnTree<T,m_T,i_T,E,size_max,digit>::Parent( const int& i ) { return m_dfst.Parent( i ); }
-
-
-template <typename T , T m_T(const T&,const T&), T i_T(const T&) , int size_max> inline int CumulativeProd<T,m_T,i_T,size_max>::LCA( const int& i , const int& j ) { return min( i , j ); }
-template <typename T , T m_T(const T&,const T&), T i_T(const T&) , list<int> E(const int&) , int size_max , int digit> inline int CumulativeProdOnTree<T,m_T,i_T,E,size_max,digit>::LCA( const int& i , const int& j ) { return m_dfst.LCA( i , j ); }
+template <typename T , T m_T(const T&,const T&) , const T& e_T() , T i_T(const T&) , int size_max> inline int CumulativeProd<T,m_T,e_T,i_T,size_max>::LCA( const int& i , const int& j ) { return min( i , j ); }
+template <typename T , T m_T(const T&,const T&) , T i_T(const T&) , list<int> E(const int&) , int size_max , int digit> inline int CumulativeProdOnTree<T,m_T,i_T,E,size_max,digit>::LCA( const int& i , const int& j ) { return m_dfst.LCA( i , j ); }
