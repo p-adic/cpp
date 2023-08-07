64ca74ccd1863bcd03126c819836ee7faef5d6a0
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-08-07 11:10:57 +0900

    Add files via upload

---
+++ Mathematics/Arithmetic/Prime/Divisor/GCD/a.hpp
@@ -0,0 +1,8 @@
+// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/Prime/Divisor/GCD/a.hpp
+
+#pragma once
+
+template <typename INT>
+INT GCD( const INT& b_0 , const INT& b_1 );
+
+template <typename INT> inline INT LCM( const INT& b_0 , const INT& b_1 );
