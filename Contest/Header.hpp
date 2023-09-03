dadcc49880fc509be1e85dc34a510af236e85ff7
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-09-03 21:09:24 +0900

    Create Header.hpp

---
+++ Contest/Header.hpp
@@ -0,0 +1,6 @@
+// c:/Users/user/Documents/Programming/Contest/Header.hpp
+
+#pragma once
+#include "../Utility/Header.hpp"
+#include "../Utility/String/a_Body.hpp"
+#include "../Utility/VLArray/a_Body.hpp"
