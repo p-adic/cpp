7315659144a33e6b209782b6ae88e52aaa373a76
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-09-09 09:23:37 +0900

    Delete Mathematics/SetTheory/DirectProduct/AffineSpace/SegmentTree/Power directory

--- Mathematics/SetTheory/DirectProduct/AffineSpace/SegmentTree/Power/a_Body.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/SegmentTree/Power/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-template <int N> inline constexpr PowerCalculation<N>::PwerCalculation() : m_val( 1 ) { while( m_val < N ){ m_val <<= 1; } }
