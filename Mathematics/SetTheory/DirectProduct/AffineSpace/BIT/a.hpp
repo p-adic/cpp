b206532cc2a236e667725dbb8f41e518bc49b0c0
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-09-20 07:54:34 +0900

    Add files via upload

--- Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/a.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/a.hpp
@@ -1,39 +1,23 @@
-// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/a.hpp
+// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalMax/a.hpp
 
 #pragma once
-// 配列の各要素がint型の範疇でも総和がそうでない場合はTをint型にすると正しく動作しないことに注意。
-// InitialSegmentSumで負の入力を扱うためにuintではなくintをテンプレート引数にする。
+#include "a_Macro.hpp"
 
-// 使用演算：
-// T& T::operator=( const T& )
-// T& T::operator+=( const T& )
-// T operator-( const T& , const T& )（ただしIntervalSumを用いない場合は不要）
-// T operator<( const T& , const T& )（ただしBinarySearchを用いない場合は不要）
-template <typename T , int N>
-class BIT
-{
-private:
-  T m_fenwick[N + 1];
+// 最大（最小）元による初期化O(N)
+// 配列による初期化O(N)
 
-public:
-  inline BIT();
-  BIT( const T ( & a )[N] );
+// 一点取得O(1)
+// 区間max（min）取得O(log_2 N)
 
-  // const参照でないことに注意。
-  inline T Get( const int& i ) const;
-  inline void Set( const int& i , const T& n );
-  inline void Set( const T ( & a )[N] );
+// 一点更新O((log_2 N)^2)
+// max（min）による一点更新O(log_2 N)
+// max（min）による区間更新O(i_final-i_start+log_2 N)
 
-  inline BIT<T,N>& operator+=( const T ( & a )[N] );
-  void Add( const int& i , const T& n );
+// n以上（以下）となる要素の添字の最小値の二分探索O(log_2 N)
+// （存在しない場合はN以上の最小の2羃×2-1を返すので、N以上であることで判定可能）
 
-  T InitialSegmentSum( const int& i_final ) const;
-  inline T IntervalSum( const int& i_start , const int& i_final ) const;
-  
-  // operator+=の単位元T()より小さくない要素のみを成分に持つ場合のみサポート。
-  // InitialSegmentSum( i )がn以上となるiが存在する場合にその最小値を2進法で探索。
-  int BinarySearch( const T& n ) const;
-  // IntervalSum( i_start , i )がt以上となるi_start以上のiが存在する場合にその最小値を2進法で探索。
-  inline int BinarySearch( const int& i_start , const T& n ) const;
-  
-};
+// そのうちの区間min取得と一点更新は
+// M. Dima, R. Ceterchi, Efficient Range Minimum Queries using Binary Indexed Trees, Olympiads in Informatics, 2015, Vol. 9, 39--44
+// の手法をもとに実装
+DECLARATION_OF_INTERVAL_MAX_BIT( Max );
+DECLARATION_OF_INTERVAL_MAX_BIT( Min );
