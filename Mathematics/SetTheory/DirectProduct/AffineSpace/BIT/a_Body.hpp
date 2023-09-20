f43c9a1464460048f1ecd6ae27c06e941644daa4
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-09-20 10:17:13 +0900

    Add files via upload

--- Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/a_Body.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/a_Body.hpp
@@ -34,7 +34,7 @@ BIT<T,N>::BIT( const T ( &a )[N] ) : m_fenwick()
 template <typename T , int N> inline T BIT<T,N>::Get( const int& i ) const { return IntervalSum( i , i ); }
 template <typename T , int N> inline void BIT<T,N>::Set( const int& i , const T& n ) { Add( i , n - IntervalSum( i , i ) ); }
 template <typename T , int N> inline void BIT<T,N>::Set( const T ( &a )[N] ) { BIT<T,N> a_copy{ a }; swap( m_fenwick , a_copy.m_fenwick ); }
-template <typename T , int N> inline void BIT<T,N>::Initialise() { for( int j = 1 ; j <= N ; j++ ){ m_fenwick[i] = 0; } }
+template <typename T , int N> inline void BIT<T,N>::Initialise() { for( int j = 1 ; j <= N ; j++ ){ m_fenwick[j] = 0; } }
 
 template <typename T , int N> inline BIT<T,N>& BIT<T,N>::operator+=( const T ( &a )[N] ) { for( int i = 0 ; i < N ; i++ ){ Add( i , a[i] ); } return *this; }
 
