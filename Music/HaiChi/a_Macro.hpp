04d8717afed1f633612582040667e1ce96307ba6
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-10-10 09:52:26 +0900

    Add files via upload

--- Music/HaiChi/a_Macro.hpp
+++ Music/HaiChi/a_Macro.hpp
@@ -1,7 +1,7 @@
 // c:/Users/user/Documents/Programming/Music/HaiChi/a_Macro.hpp
 
 #pragma once
-#include "../../Utility/Macro_Macro.hpp"
+#include "../../Utility/String/a_Macro.hpp"
 
 #define DEFINITION_OF_NOTE_NUMBER( ONMEI , OCTAVE )			\
 									\
