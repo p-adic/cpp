ca4e4de422120f0d7ff3efdd18be093a21158402
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-10-10 10:33:16 +0900

    Add files via upload

--- Contest/Header.hpp
+++ Contest/Header.hpp
@@ -1,12 +1,6 @@
 // c:/Users/user/Documents/Programming/Contest/Header.hpp
 
 #pragma once
-#include <list>
-#include <string>
-
-using namespace std;
-using uint = unsigned int;
-
-#include "../Utility/StdStream/a_Body.hpp"
+#include "../Utility/Header.hpp"
 #include "../Utility/String/a_Body.hpp"
 #include "../Utility/VLArray/a_Body.hpp"
