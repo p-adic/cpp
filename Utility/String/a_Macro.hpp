9162559afba795ed2192ff4efa4924cb21f03558
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-10-10 10:02:12 +0900

    Add files via upload

--- Utility/String/a_Macro.hpp
+++ Utility/String/a_Macro.hpp
@@ -3,5 +3,11 @@
 #pragma once
 #include "../../SFINAE/MemberFunction/a_Macro.hpp"
 
+#define CONNECT( S1 , S2 ) SUBSTITUTE_CONNECT( S1 , S2 ) 
+#define SUBSTITUTE_CONNECT( S1 , S2 ) S1 ## S2 
+
+#define TO_STRING( ... ) SUBSTITUTE_TO_STRING( __VA_ARGS__ ) 
+#define SUBSTITUTE_TO_STRING( ... ) to_string( #__VA_ARGS__ ) 
+
 #define ARGUMENTS( ... ) TO_STRING( __VA_ARGS__ ) , __VA_ARGS__ 
 #define ECHO( ... ) IndicateArguments( ARGUMENTS( __VA_ARGS__ ) ) 
