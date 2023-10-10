125a367672e4ce2d45c123d4fde31f10a593629a
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-10-10 10:01:48 +0900

    Delete Utility/Macro_Macro.hpp

--- Utility/Macro_Macro.hpp
+++
@@ -1,9 +0,0 @@
-// Macro_Macro.hpp
-
-#pragma once
-
-#define CONNECT( S1 , S2 ) SUBSTITUTE_CONNECT( S1 , S2 ) 
-#define SUBSTITUTE_CONNECT( S1 , S2 ) S1 ## S2 
-
-#define TO_STRING( ... ) SUBSTITUTE_TO_STRING( __VA_ARGS__ ) 
-#define SUBSTITUTE_TO_STRING( ... ) to_string( #__VA_ARGS__ ) 
