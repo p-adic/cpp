420af69256b4b3c61f047861e9b8f15e272de6f5
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-10-10 09:51:55 +0900

    Add files via upload

--- Mathematics/Function/a_Macro.hpp
+++ Mathematics/Function/a_Macro.hpp
@@ -1,7 +1,7 @@
 // c:/Users/user/Documents/Programming/Mathematics/Function/a_Macro.hpp
 
 #pragma once
-#include "../../Utility/Macro_Macro.hpp"
+#include "../../Utility/String/a_Macro.hpp"
 
 #define DECLARATION_OF_ITERATED_COMPOSITION_FOR_NON_MEMBER_FUNCTION( FUNCTION ) \
   template <typename Arg , typename U> inline Arg Iteration( const CONNECT( FUNCTION , PointerType )<Arg,Arg>& f , const U& num , const Arg& arg ) 
