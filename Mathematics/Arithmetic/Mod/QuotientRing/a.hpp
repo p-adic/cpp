9e507674609664c538d22b54f8b29615ec563f6a
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-10-13 14:32:34 +0900

    Add files via upload

--- Mathematics/Arithmetic/Mod/QuotientRing/a.hpp
+++ Mathematics/Arithmetic/Mod/QuotientRing/a.hpp
@@ -12,7 +12,7 @@ protected:
 
 public:
   inline QuotientRing() noexcept;
-  inline QuotientRing( const INT& n , const INT* const & p_M = nullptr ) noexcept;
+  inline QuotientRing( const INT& n , const INT* const& p_M = nullptr ) noexcept;
   inline QuotientRing( const QuotientRing<INT>& n ) noexcept;
 
   inline QuotientRing<INT>& operator+=( const QuotientRing<INT>& n ) noexcept;
@@ -22,22 +22,31 @@ public:
   inline QuotientRing<INT>& operator-=( const INT& n ) noexcept;
   inline QuotientRing<INT>& operator*=( const QuotientRing<INT>& n ) noexcept;
   inline QuotientRing<INT>& operator*=( const INT& n ) noexcept;
+  // *m_p_Mが素数でかつnの逆元が存在する場合のみサポート。
+  inline QuotientRing<INT>& operator/=( const QuotientRing<INT>& n );
+  inline QuotientRing<INT>& operator/=( const INT& n );
+
+  // m_nの正負やm_p_Mの一致込みの等号。
+  inline bool operator==( const QuotientRing<INT>& n ) const noexcept;
+  inline bool operator!=( const QuotientRing<INT>& n ) const noexcept;
+
+  template <typename T> inline QuotientRing<INT> operator+( const T& n1 ) const noexcept;
+  inline QuotientRing<INT> operator-() const noexcept;
+  template <typename T> inline QuotientRing<INT> operator-( const T& n1 ) const noexcept;
+  template <typename T> inline QuotientRing<INT> operator*( const T& n1 ) const noexcept;
+  // *m_p_Mが素数でかつn1の逆元が存在する場合のみサポート。
+  template <typename T> inline QuotientRing<INT> operator/( const T& n1 ) const;
 
   inline const INT& Represent() const noexcept;
   inline const INT& GetModulo() const noexcept;
-
-  // m_nの正負やm_p_Mの一致込みの等号。
-  static inline bool Equal( const QuotientRing<INT>& n0 , const QuotientRing<INT>& n1 ) noexcept;
+  inline void SetModulo( const INT* const& p_M = nullptr ) noexcept;
 
   template <typename T> static QuotientRing<INT> Power( const QuotientRing<INT>& n , T exponent );
+  // *m_p_Mが素数でかつnの逆元が存在する場合のみサポート。
+  static QuotientRing<INT> Inverse( const QuotientRing<INT>& n );
   
 };
 
-template <typename INT> inline bool operator==( const QuotientRing<INT>& n0 , const QuotientRing<INT>& n1 ) noexcept;
-template <typename INT> inline bool operator!=( const QuotientRing<INT>& n0 , const QuotientRing<INT>& n1 ) noexcept;
-
-template <typename INT , typename T> inline QuotientRing<INT> operator+( const QuotientRing<INT>& n0 , const T& n1 ) noexcept;
-template <typename INT , typename T> inline QuotientRing<INT> operator-( const QuotientRing<INT>& n0 , const T& n1 ) noexcept;
-template <typename INT , typename T> inline QuotientRing<INT> operator*( const QuotientRing<INT>& n0 , const T& n1 ) noexcept;
-
 template <typename INT , typename T> inline QuotientRing<INT> Power( const QuotientRing<INT>& n , T exponent );
+// *(n.m_p_M)が素数でかつnの逆元が存在する場合のみサポート。
+template <typename INT> inline QuotientRing<INT> Inverse( const QuotientRing<INT>& n );
--- Mathematics/Arithmetic/Mod/QuotientRing/a_Body.hpp
+++ Mathematics/Arithmetic/Mod/QuotientRing/a_Body.hpp
@@ -4,7 +4,7 @@
 #include "a.hpp"
 
 template <typename INT> inline QuotientRing<INT>::QuotientRing() noexcept : m_n() , m_p_M( nullptr ) {}
-template <typename INT> inline QuotientRing<INT>::QuotientRing( const INT& n , const INT* const & p_M ) noexcept : m_n( p_M == nullptr ? n : n % *p_M ) , m_p_M( p_M ) {}
+template <typename INT> inline QuotientRing<INT>::QuotientRing( const INT& n , const INT* const& p_M ) noexcept : m_n( p_M == nullptr ? n : n % *p_M ) , m_p_M( p_M ) {}
 template <typename INT> inline QuotientRing<INT>::QuotientRing( const QuotientRing<INT>& n ) noexcept : m_n( n.m_n ) , m_p_M( n.m_p_M ) {}
 
 template <typename INT> inline QuotientRing<INT>& QuotientRing<INT>::operator+=( const QuotientRing<INT>& n ) noexcept { if( m_p_M == nullptr ){ m_p_M = n.m_p_M; } m_n += n.m_n; if( m_p_M != nullptr ){ m_n %= *m_p_M; } return *this; }
@@ -13,11 +13,21 @@ template <typename INT> inline QuotientRing<INT>& QuotientRing<INT>::operator-=(
 template <typename INT> inline QuotientRing<INT>& QuotientRing<INT>::operator-=( const INT& n ) noexcept { m_n -= n; if( m_p_M != nullptr ){ m_n %= *m_p_M; } return *this; }
 template <typename INT> inline QuotientRing<INT>& QuotientRing<INT>::operator*=( const QuotientRing<INT>& n ) noexcept { if( m_p_M == nullptr ){ m_p_M = n.m_p_M; } m_n *= n.m_n; if( m_p_M != nullptr ){ m_n %= *m_p_M; } return *this; }
 template <typename INT> inline QuotientRing<INT>& QuotientRing<INT>::operator*=( const INT& n ) noexcept { m_n *= n; if( m_p_M != nullptr ){ m_n %= *m_p_M; } return *this; }
+template <typename INT> inline QuotientRing<INT>& QuotientRing<INT>::operator/=( const QuotientRing<INT>& n ) { if( m_p_M == nullptr ){ if( n.m_p_M == nullptr ){ assert( n.m_n != 0 ); m_n /= n.m_n; return *this; } else { m_p_M = n.m_p_M; } } return operator*=( Inverse( QuotientRing<INT>( n.m_n , m_p_M ) ) ); }
+template <typename INT> inline QuotientRing<INT>& QuotientRing<INT>::operator/=( const INT& n ) { if( m_p_M == nullptr ){ assert( n.m_n != 0 ); m_n /= n.m_n; return *this; } return operator*=( Inverse( Q( n.m_n , m_p_M ) ) ); }
+
+template <typename INT> inline bool QuotientRing<INT>::operator==( const QuotientRing<INT>& n ) const noexcept { return m_p_M == n.m_p_M && m_n == n.m_n; }
+template <typename INT> inline bool QuotientRing<INT>::operator!=( const QuotientRing<INT>& n ) const noexcept { return !operator==( n ); }
+
+template <typename INT> template<typename T> inline QuotientRing<INT> QuotientRing<INT>::operator+( const T& n ) const noexcept { return QuotientRing<INT>( *this ).operator+=( n ); }
+template <typename INT> inline QuotientRing<INT> QuotientRing<INT>::operator-() const noexcept { return QuotientRing<INT>( -m_n , m_p_M ); }
+template <typename INT> template<typename T> inline QuotientRing<INT> QuotientRing<INT>::operator-( const T& n ) const noexcept { return QuotientRing<INT>( *this ).operator-=( n ); }
+template <typename INT> template<typename T> inline QuotientRing<INT> QuotientRing<INT>::operator*( const T& n ) const noexcept { return QuotientRing<INT>( *this ).operator*=( n ); }
+template <typename INT> template<typename T> inline QuotientRing<INT> QuotientRing<INT>::operator/( const T& n ) const { return QuotientRing<INT>( *this ).operator/=( n ); }
   
 template <typename INT> inline const INT& QuotientRing<INT>::Represent() const noexcept { return m_n; }
 template <typename INT> inline const INT& QuotientRing<INT>::GetModulo() const noexcept { static const INT zero{ 0 }; return m_p_M == nullptr ? zero : *m_p_M; }
-
-template <typename INT> inline bool QuotientRing<INT>::Equal( const QuotientRing<INT>& n0 , const QuotientRing<INT>& n1 ) noexcept { return n0.m_n == n1.m_n && n0.m_p_M == n1.m_p_M; }
+template <typename INT> inline void QuotientRing<INT>::SetModulo( const INT* const& p_M ) noexcept { m_p_M = p_M; if( m_p_M != nullptr ){ m_n %= *m_p_M; } }
 
 template <typename INT> template <typename T>
 QuotientRing<INT> QuotientRing<INT>::Power( const QuotientRing<INT>& n , T exponent )
@@ -43,11 +53,7 @@ QuotientRing<INT> QuotientRing<INT>::Power( const QuotientRing<INT>& n , T expon
 
 }
 
-template <typename INT> inline bool operator==( const QuotientRing<INT>& n0 , const QuotientRing<INT>& n1 ) noexcept { return QuotientRing<INT>::Equal( n0 , n1 ); }
-template <typename INT> inline bool operator!=( const QuotientRing<INT>& n0 , const QuotientRing<INT>& n1 ) noexcept { return ! QuotientRing<INT>::Equal( n0 , n1 ); }
-
-template <typename INT , typename T> inline QuotientRing<INT> operator+( const QuotientRing<INT>& n0 , const T& n1 ) noexcept { return QuotientRing<INT>( n0 ).operator+=( n1 ); }
-template <typename INT , typename T> inline QuotientRing<INT> operator-( const QuotientRing<INT>& n0 , const T& n1 ) noexcept { return QuotientRing<INT>( n0 ).operator-=( n1 ); }
-template <typename INT , typename T> inline QuotientRing<INT> operator*( const QuotientRing<INT>& n0 , const T& n1 ) noexcept { return QuotientRing<INT>( n0 ).operator*=( n1 ); }
+template <typename INT> inline QuotientRing<INT> QuotientRing<INT>::Inverse( const QuotientRing<INT>& n ) { assert( n.m_p_M != nullptr ); return Power( n , *( n.m_p_M ) - 2 ); }
 
 template <typename INT , typename T> inline QuotientRing<INT> Power( const QuotientRing<INT>& n , T exponent ) { return QuotientRing<INT>::template Power<T>( n , exponent ); }
+template <typename INT> inline QuotientRing<INT> Inverse( const QuotientRing<INT>& n ) { return QuotientRing<INT>::Inverse( n ); }
