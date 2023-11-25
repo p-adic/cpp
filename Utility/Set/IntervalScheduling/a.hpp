51d51950114af03c767326b76e2465b1a790ee41
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-11-25 14:28:30 +0900

    Add files via upload

---
+++ Utility/Set/IntervalScheduling/a.hpp
@@ -0,0 +1,36 @@
+// c:/Users/user/Documents/Programming/Utility/Set/IntervalScheduling/a.hpp
+
+#pragma once
+
+// verify: https://yukicoder.me/submissions/931520
+
+template <typename T>
+class IntervalScheduling
+{
+  
+private:
+  int m_count;
+  vector<T> m_l;
+  vector<T> m_r;
+  vector<bool> m_valid;
+  set<tuple<T,T,int>> m_rli;
+  int m_size;
+  T m_l_max;
+
+public:
+  inline void insert( const T& l , const T& r );
+  inline void erase( const tuple<T,T,int>& v );
+
+  inline const int& count() const noexcept;
+  inline const T& GetL( const int& i ) const;
+  inline const T& GetR( const int& i ) const;
+  // constéQè∆Ç≈Ç»Ç¢Ç±Ç∆Ç…íçà”
+  inline bool GetValid( const int& i ) const;
+  inline const int& size() const noexcept;
+
+  inline const int& Init() const;
+  int Next( const int& i ) const;
+  void SetNext( vector<int>& a ) const noexcept;
+  void SetMaximal( list<int>& a , const int& i = -1 ) const;
+  
+};
