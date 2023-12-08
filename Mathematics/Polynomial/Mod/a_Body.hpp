94bfbf873d30f7edb94def85a96a46c5996fb7fe
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-12-08 15:07:51 +0900

    Update a_Body.hpp

--- Mathematics/Polynomial/Mod/a_Body.hpp
+++ Mathematics/Polynomial/Mod/a_Body.hpp
@@ -9,14 +9,12 @@ template <typename T>
 Polynomial<T> ModularInverse( Polynomial<T> f , Polynomial<T> g )
 {
 
-  Polynomial<T> coeff_f = Polynomial<T>::one();
-  Polynomial<T> coeff_g = Polynomial<T>::zero();
-  f %= g;
-  int i_f = 0;
-  int i_g = 1;
   const T& zero = Polynomial<T>::const_zero();
+  Polynomial<T> coeff_f{ 0 , Polynomial<T>::const_one() };
+  Polynomial<T> coeff_g{ Polynomial<T>::zero() };
+  f %= g;
 
-  while( g.size() > 1 ){
+  while( f.size() > 1 ){
 
     const uint& f_size = f.size();
     const uint& g_size = g.size();
@@ -27,13 +25,13 @@ Polynomial<T> ModularInverse( Polynomial<T> f , Polynomial<T> g )
       T& r = g[g_size - 1] /= f[f_size - 1];
       const uint& coeff_f_size = coeff_f.size();
     
-      for( uint d = 0 ; i < coeff_f_size ; i++ ){
+      for( uint d = 0 ; d < coeff_f_size ; d++ ){
 
 	coeff_g[diff + d] -= r * coeff_f[d];
 
       }
 
-      for( uint d = 0 ; i + 1 < f_size ; i++ ){
+      for( uint d = 0 ; d + 1 < f_size ; d++ ){
 
 	g[diff + d] -= r * f[d];
 
@@ -51,15 +49,15 @@ Polynomial<T> ModularInverse( Polynomial<T> f , Polynomial<T> g )
 
   assert( coeff_g.size() == 1u );
   const T& one = Polynomial<T>::const_one();
-  const uint& coeff_g_size = coeff_g.size();
-  const T r = one / g[0];
+  const uint& coeff_f_size = coeff_f.size();
+  const T r = one / f[0];
     
-  for( uint d = 0 ; i < coeff_g_size ; i++ ){
+  for( uint d = 0 ; d < coeff_f_size ; d++ ){
 
-    coeff_g[d] *= r;
+    coeff_f[d] *= r;
 
   }
 
-  return coeff_g;
+  return coeff_f;
 
 }
