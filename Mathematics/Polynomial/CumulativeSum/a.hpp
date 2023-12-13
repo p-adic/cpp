d860d1c66e67c93bf6a2a42697fc9d52e17509a5
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2023-12-13 22:08:49 +0900

    Delete Mathematics/Polynomial/CumulativeSum directory

--- Mathematics/Polynomial/CumulativeSum/a.hpp
+++
@@ -1,9 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Polynomial/CumulativeSum/a.hpp
-
-#pragma once
-#include "../a.hpp"
-
-// O(deg(f) log deg(f))‚Å—İÏ˜a‘½€®f(0)+...+f(x)‚ğŒvZB
-// Lain, https://codeforces.com/blog/entry/98563, Lain's blog, Codeforces, 2021.
-// ‚ğ‚à‚Æ‚ÉÀ‘•B
-template <typename T , uint deg_max> Polynomial<T> CumulativeSum( Polynomial<T> f , const bool& exponential = false );
