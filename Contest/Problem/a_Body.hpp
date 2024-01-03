dcdab42080b8115ede6a384b3a0e7f9a4489175a
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-01-03 13:48:47 +0900

    Add files via upload

--- Contest/Problem/a_Body.hpp
+++ Contest/Problem/a_Body.hpp
@@ -8,9 +8,14 @@
 #include "RearrangeSamples/a_Body.hpp"
 #include "WriteInSamples/a_Body.hpp"
 
+// 入出力用関数は../../Utility/StdStream/で定義。
+// GetgEたちの定義はグラフ用関数の位置に移動。
 
-// 算術用関数
-template <typename T> inline T Residue( const T& a , const T& p ){ return a >= 0 ? a % p : p - 1 - ( ( - ( a + 1 ) ) % p ); }
+// 算術用
+template <typename T> constexpr T PositiveBaseResidue( const T& a , const T& p ){ return a >= 0 ? a % p : p - 1 - ( ( - ( a + 1 ) ) % p ); }
+template <typename T> constexpr T Residue( const T& a , const T& p ){ return PositiveBaseResidue( a , p < 0 ? -p : p ); }
+template <typename T> constexpr T PositiveBaseQuotient( const T& a , const T& p ){ return ( a - PositiveBaseResidue( a , p ) ) / p; }
+template <typename T> constexpr T Quotient( const T& a , const T& p ){ return p < 0 ? PositiveBaseQuotient( -a , -p ) : PositiveBaseQuotient( a , p ); }
 
 #define POWER( ANSWER , ARGUMENT , EXPONENT )				\
   static_assert( ! is_same<TYPE_OF( ARGUMENT ),int>::value && ! is_same<TYPE_OF( ARGUMENT ),uint>::value ); \
@@ -30,7 +35,8 @@ template <typename T> inline T Residue( const T& a , const T& p ){ return a >= 0
 #define POWER_MOD( ANSWER , ARGUMENT , EXPONENT , MODULO )		\
   ll ANSWER{ 1 };							\
   {									\
-    ll ARGUMENT_FOR_SQUARE_FOR_POWER = ( ( MODULO ) + ( ( ARGUMENT ) % ( MODULO ) ) ) % ( MODULO ); \
+    ll ARGUMENT_FOR_SQUARE_FOR_POWER = ( ( ARGUMENT ) % ( MODULO ) ) % ( MODULO ); \
+    ARGUMENT_FOR_SQUARE_FOR_POWER < 0 ? ARGUMENT_FOR_SQUARE_FOR_POWER += ( MODULO ) : ARGUMENT_FOR_SQUARE_FOR_POWER; \
     TYPE_OF( EXPONENT ) EXPONENT_FOR_SQUARE_FOR_POWER = ( EXPONENT );	\
     while( EXPONENT_FOR_SQUARE_FOR_POWER != 0 ){			\
       if( EXPONENT_FOR_SQUARE_FOR_POWER % 2 == 1 ){			\
@@ -57,53 +63,63 @@ template <typename T> inline T Residue( const T& a , const T& p ){ return a >= 0
     }									\
   }									\
 
-// 二分探索テンプレート
-// EXPRESSIONがANSWERの広義単調関数の時、EXPRESSION >= TARGETの整数解を格納。
-#define BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , DESIRED_INEQUALITY , TARGET , INEQUALITY_FOR_CHECK , UPDATE_U , UPDATE_L , UPDATE_ANSWER ) \
-  static_assert( ! is_same<TYPE_OF( TARGET ),uint>::value && ! is_same<TYPE_OF( TARGET ),ull>::value ); \
+// 二分探索用
+// EXPRESSIONがANSWERの広義単調関数の時、EXPRESSION >= CONST_TARGETの整数解を格納。
+#define BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , DESIRED_INEQUALITY , CONST_TARGET , INEQUALITY_FOR_CHECK , UPDATE_U , UPDATE_L , UPDATE_ANSWER ) \
+  static_assert( ! is_same<TYPE_OF( CONST_TARGET ),uint>::value && ! is_same<TYPE_OF( CONST_TARGET ),ull>::value ); \
   ll ANSWER = MINIMUM;							\
-  if( MINIMUM <= MAXIMUM ){						\
-    ll VARIABLE_FOR_BINARY_SEARCH_L = MINIMUM;				\
-    ll VARIABLE_FOR_BINARY_SEARCH_U = MAXIMUM;				\
-    ANSWER = ( VARIABLE_FOR_BINARY_SEARCH_L + VARIABLE_FOR_BINARY_SEARCH_U ) / 2; \
-    ll VARIABLE_FOR_DIFFERENCE_FOR_BINARY_SEARCH;			\
-    while( VARIABLE_FOR_BINARY_SEARCH_L != VARIABLE_FOR_BINARY_SEARCH_U ){ \
-      VARIABLE_FOR_DIFFERENCE_FOR_BINARY_SEARCH = ( EXPRESSION ) - ( TARGET ); \
-      CERR( "二分探索中： " << VARIABLE_FOR_BINARY_SEARCH_L << "<=" << ANSWER << "<=" << VARIABLE_FOR_BINARY_SEARCH_U << ":" << EXPRESSION << "-" << TARGET << "=" << VARIABLE_FOR_DIFFERENCE_FOR_BINARY_SEARCH ); \
-      if( VARIABLE_FOR_DIFFERENCE_FOR_BINARY_SEARCH INEQUALITY_FOR_CHECK 0 ){	\
-	VARIABLE_FOR_BINARY_SEARCH_U = UPDATE_U;			\
-      } else {								\
-	VARIABLE_FOR_BINARY_SEARCH_L = UPDATE_L;			\
-      }									\
-      ANSWER = UPDATE_ANSWER;						\
-    }									\
-    CERR( "二分探索終了： " << VARIABLE_FOR_BINARY_SEARCH_L << "<=" << ANSWER << "<=" << VARIABLE_FOR_BINARY_SEARCH_U << ":" << EXPRESSION << ( EXPRESSION > TARGET ? ">" : EXPRESSION < TARGET ? "<" : "=" ) << TARGET ); \
-    if( EXPRESSION DESIRED_INEQUALITY TARGET ){				\
-      CERR( "二分探索成功" );						\
+  {									\
+  ll L_BS = MINIMUM;							\
+  ll U_BS = MAXIMUM;							\
+  ANSWER = UPDATE_ANSWER;						\
+  ll EXPRESSION_BS;							\
+  const ll CONST_TARGET_BS = ( CONST_TARGET );				\
+  ll DIFFERENCE_BS;							\
+  while( L_BS < U_BS ){							\
+    DIFFERENCE_BS = ( EXPRESSION_BS = ( EXPRESSION ) ) - CONST_TARGET_BS; \
+    CERR( "二分探索中：" , "L_BS =" , L_BS , "<=" , ANSWER , "<=" , U_BS , "= U_BS :" , #EXPRESSION , "-" , #CONST_TARGET , "=" , EXPRESSION_BS , "-" , CONST_TARGET_BS , "=" , DIFFERENCE_BS ); \
+    if( DIFFERENCE_BS INEQUALITY_FOR_CHECK 0 ){				\
+      U_BS = UPDATE_U;							\
     } else {								\
-      CERR( "二分探索失敗" );						\
+      L_BS = UPDATE_L;							\
+    }									\
+    ANSWER = UPDATE_ANSWER;						\
+  }									\
+  if( L_BS > U_BS ){							\
+    CERR( "二分探索失敗：" , "L_BS =" , L_BS , ">" , U_BS , "= U_BS :" , #ANSWER , ":=" , #MAXIMUM , "+ 1 =" , MAXIMUM + 1  ); \
+    CERR( "二分探索マクロにミスがある可能性があります。変更前の版に戻してください。" );	\
+    ANSWER = MAXIMUM + 1;						\
+  } else {								\
+    CERR( "二分探索終了：" , "L_BS =" , L_BS , "<=" , ANSWER , "<=" , U_BS , "= U_BS" ); \
+    CERR( "二分探索が成功したかを確認するために" , #EXPRESSION , "を計算します。" ); \
+    CERR( "成功判定が不要な場合はこの計算を削除しても構いません。" );	\
+    EXPRESSION_BS = ( EXPRESSION );					\
+    CERR( "二分探索結果：" , #EXPRESSION , "=" , EXPRESSION_BS , ( EXPRESSION_BS > CONST_TARGET_BS ? ">" : EXPRESSION_BS < CONST_TARGET_BS ? "<" : "=" ) , CONST_TARGET_BS ); \
+    if( EXPRESSION_BS DESIRED_INEQUALITY CONST_TARGET_BS ){		\
+      CERR( "二分探索成功：" , #ANSWER , ":=" , ANSWER );		\
+    } else {								\
+      CERR( "二分探索失敗：" , #ANSWER , ":=" , #MAXIMUM , "+ 1 =" , MAXIMUM + 1 ); \
+      CERR( "単調でないか、単調増加性と単調減少性を逆にしてしまったか、探索範囲内に解が存在しません。" ); \
       ANSWER = MAXIMUM + 1;						\
     }									\
-  } else {								\
-    CERR( "二分探索失敗： " << MINIMUM << ">" << MAXIMUM );		\
-    ANSWER = MAXIMUM + 1;						\
+  }									\
   }									\
 
-// 単調増加の時にEXPRESSION >= TARGETの最小解を格納。
-#define BS1( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , TARGET )		\
-  BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , >= , TARGET , >= , ANSWER , ANSWER + 1 , ( VARIABLE_FOR_BINARY_SEARCH_L + VARIABLE_FOR_BINARY_SEARCH_U ) / 2 ) \
+// 単調増加の時にEXPRESSION >= CONST_TARGETの最小解を格納。
+#define BS1( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET )	\
+  BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , >= , CONST_TARGET , >= , ANSWER , ANSWER + 1 , ( L_BS + U_BS ) / 2 ) \
 
-// 単調増加の時にEXPRESSION <= TARGETの最大解を格納。
-#define BS2( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , TARGET )		\
-  BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , <= , TARGET , > , ANSWER - 1 , ANSWER , ( VARIABLE_FOR_BINARY_SEARCH_L + 1 + VARIABLE_FOR_BINARY_SEARCH_U ) / 2 ) \
+// 単調増加の時にEXPRESSION <= CONST_TARGETの最大解を格納。
+#define BS2( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET )	\
+  BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , <= , CONST_TARGET , > , ANSWER - 1 , ANSWER , ( L_BS + 1 + U_BS ) / 2 ) \
 
-// 単調減少の時にEXPRESSION >= TARGETの最大解を格納。
-#define BS3( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , TARGET )		\
-  BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , >= , TARGET , < , ANSWER - 1 , ANSWER , ( VARIABLE_FOR_BINARY_SEARCH_L + 1 + VARIABLE_FOR_BINARY_SEARCH_U ) / 2 ) \
+// 単調減少の時にEXPRESSION >= CONST_TARGETの最大解を格納。
+#define BS3( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET )	\
+  BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , >= , CONST_TARGET , < , ANSWER - 1 , ANSWER , ( L_BS + 1 + U_BS ) / 2 ) \
 
-// 単調減少の時にEXPRESSION <= TARGETの最小解を格納。
-#define BS4( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , TARGET )		\
-  BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , <= , TARGET , <= , ANSWER , ANSWER + 1 , ( VARIABLE_FOR_BINARY_SEARCH_L + VARIABLE_FOR_BINARY_SEARCH_U ) / 2 ) \
+// 単調減少の時にEXPRESSION <= CONST_TARGETの最小解を格納。
+#define BS4( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET )	\
+  BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , <= , CONST_TARGET , <= , ANSWER , ANSWER + 1 , ( L_BS + U_BS ) / 2 ) \
 
 // t以下の値が存在すればその最大値のiterator、存在しなければend()を返す。
 template <typename T> inline typename set<T>::iterator MaximumLeq( set<T>& S , const T& t ) { const auto end = S.end(); if( S.empty() ){ return end; } auto itr = S.upper_bound( t ); return itr == end ? S.find( *( S.rbegin() ) ) : itr == S.begin() ? end : --itr; }
@@ -114,7 +130,11 @@ template <typename T> inline typename set<T>::iterator MinimumGeq( set<T>& S , c
 // tより大きい値が存在すればその最小値のiterator、存在しなければend()を返す。
 template <typename T> inline typename set<T>::iterator MinimumGt( set<T>& S , const T& t ) { return S.upper_bound( t ); }
 
-// データ構造用関数
+// データ構造用
+template <typename T , template <typename...> typename V> inline T operator+( const T& t0 , const T& t1 ) { if( t0.empty() ){ return t1; } if( t1.empty() ){ return t0; } assert( t0.size() == t1.size() ); V<T> answer{}; for( auto itr0 = t0.begin() , itr1 = t1.begin() , end0 = t0.end(); itr0 != end0 ; itr0++ , itr1++ ){ answer.push_back( *itr0 + *itr1 ); } return answer; }
+template <typename T> inline T operator+( const T2<T>& t0 , const T2<T>& t1 ) { return { t0.first + t1.first , t0.second + t1.second }; }
+template <typename T> inline T operator+( const T3<T>& t0 , const T3<T>& t1 ) { return { get<0>( t0 ) + get<0>( t1 ) , get<1>( t0 ) + get<1>( t1 ) , get<2>( t0 ) + get<2>( t1 ) }; }
+template <typename T> inline T operator+( const T4<T>& t0 , const T4<T>& t1 ) { return { get<0>( t0 ) + get<0>( t1 ) , get<1>( t0 ) + get<1>( t1 ) , get<2>( t0 ) + get<2>( t1 ) , get<3>( t0 ) + get<3>( t1 ) }; }
 template <typename T> inline T Add( const T& t0 , const T& t1 ) { return t0 + t1; }
 template <typename T> inline T XorAdd( const T& t0 , const T& t1 ){ return t0 ^ t1; }
 template <typename T> inline T Multiply( const T& t0 , const T& t1 ) { return t0 * t1; }
@@ -125,9 +145,10 @@ template <typename T> inline T Id( const T& v ) { return v; }
 template <typename T> inline T Min( const T& a , const T& b ){ return a < b ? a : b; }
 template <typename T> inline T Max( const T& a , const T& b ){ return a < b ? b : a; }
 
-// グリッド問題用関数
+// グリッド問題用
 int H , W , H_minus , W_minus , HW;
-inline pair<int,int> EnumHW( const int& v ) { return { v / W , v % W }; }
+vector<vector<bool> > non_wall;
+inline T2<int> EnumHW( const int& v ) { return { v / W , v % W }; }
 inline int EnumHW_inv( const int& h , const int& w ) { return h * W + w; }
 const string direction[4] = {"U","R","D","L"};
 // (i,j)->(k,h)の方向番号を取得
@@ -136,9 +157,49 @@ inline int DirectionNumberOnGrid( const int& i , const int& j , const int& k , c
 inline int DirectionNumberOnGrid( const int& v , const int& w ){auto [i,j]=EnumHW(v);auto [k,h]=EnumHW(w);return DirectionNumberOnGrid(i,j,k,h);}
 // 方向番号の反転U<->D、R<->L
 inline int ReverseDirectionNumberOnGrid( const int& n ){assert(0<=n&&n<4);return(n+2)%4;}
+inline void SetEdgeOnGrid( const string& Si , const int& i , list<int> ( &e )[] , const char& walkable = '.' ){FOR(j,0,W){if(Si[j]==walkable){int v = EnumHW_inv(i,j);if(i>0){e[EnumHW_inv(i-1,j)].push_back(v);}if(i+1<H){e[EnumHW_inv(i+1,j)].push_back(v);}if(j>0){e[EnumHW_inv(i,j-1)].push_back(v);}if(j+1<W){e[EnumHW_inv(i,j+1)].push_back(v);}}}}
+inline void SetEdgeOnGrid( const string& Si , const int& i , list<path> ( &e )[] , const char& walkable = '.' ){FOR(j,0,W){if(Si[j]==walkable){const int v=EnumHW_inv(i,j);if(i>0){e[EnumHW_inv(i-1,j)].push_back({v,1});}if(i+1<H){e[EnumHW_inv(i+1,j)].push_back({v,1});}if(j>0){e[EnumHW_inv(i,j-1)].push_back({v,1});}if(j+1<W){e[EnumHW_inv(i,j+1)].push_back({v,1});}}}}
+inline void SetWallOnGrid( const string& Si , const int& i , vector<vector<bool> >& non_wall , const char& walkable = '.'  , const char& unwalkable = '#' ){non_wall.push_back(vector<bool>(W));auto& non_wall_i=non_wall[i];FOR(j,0,W){non_wall_i[j]=Si[j]==walkable?true:(assert(Si[j]==unwalkable),false);}}
 
-// デバッグ用関数
+// グラフ用
+template <typename PATH> vector<list<PATH> > gE;
+template <typename T> map<T,T> gF;
+template <typename T> vector<T> gA;
+
+template <typename PATH> list<PATH> GetgE( const int& i )
+{
+  list<PATH> answer = gE<PATH>[i];
+  // VVV 入力によらない処理は以下に挿入する。
+
+  // AAA 入力によらない処理は以上に挿入する。
+  return answer;
+}
+
+template <typename T> inline T GetgF( const T& t ){ return gF<T>[t]; }
+template <typename T> inline T GetgA( const int& i ){ return gA<T>[i]; }
+
+// デバッグ用
 #ifdef DEBUG
   inline void AlertAbort( int n ) { CERR( "abort関数が呼ばれました。assertマクロのメッセージが出力されていない場合はオーバーフローの有無を確認をしてください。" ); }
-  void AutoCheck( bool& auto_checked );
+  void AutoCheck( int& exec_mode , const bool& use_getline );
+  inline void Solve();
+  inline void Experiment();
+  inline void SmallTest();
+  inline void RandomTest();
+  ll GetRand( const ll& Rand_min , const ll& Rand_max );
+  int exec_mode;
+  CEXPR( int , solve_mode , 0 );
+  CEXPR( int , sample_debug_mode , 1 );
+  CEXPR( int , submission_debug_mode , 2 );
+  CEXPR( int , library_search_mode , 3 );
+  CEXPR( int , experiment_mode , 4 );
+  CEXPR( int , small_test_mode , 5 );
+  CEXPR( int , random_test_mode , 6 );
+  #ifdef USE_GETLINE
+    CEXPR( bool , use_getline , true );
+  #else
+    CEXPR( bool , use_getline , false );
+  #endif
+#else
+  ll GetRand( const ll& Rand_min , const ll& Rand_max ) { ll answer = time( NULL ); return answer * rand() % ( Rand_max + 1 - Rand_min ) + Rand_min; }
 #endif
--- Contest/Problem/a_Macro.hpp
+++ Contest/Problem/a_Macro.hpp
@@ -1,43 +1,55 @@
 // c:/Users/user/Documents/Programming/Contest/Problem/a_Macro.hpp
 
+// 想定解用マクロのうち
+// ifdef DEBUG, ifdef REACTIVE, ifndef USE_GETLINE
+// を残し
+// REPEAT_MAIN
+// を変更する。
 #pragma once
 #define _GLIBCXX_DEBUG
-#include<bits/stdc++.h>
+#define SIGNAL signal( SIGABRT , &AlertAbort );
+#define DEXPR( LL , BOUND , VALUE , DEBUG_VALUE ) CEXPR( LL , BOUND , DEBUG_VALUE )
+#define ASSERT( A , MIN , MAX ) CERR( "ASSERTチェック： " , ( MIN ) , ( ( MIN ) <= A ? "<=" : ">" ) , A , ( A <= ( MAX ) ? "<=" : ">" ) , ( MAX ) ); assert( ( MIN ) <= A && A <= ( MAX ) )
+#define CERR( ... ) VariadicCout( cerr , __VA_ARGS__ ) << endl
+#define COUT( ... ) VariadicCout( cout << "出力： " , __VA_ARGS__ ) << endl
+#define CERR_A( A , N ) OUTPUT_ARRAY( cerr , A , N ) << endl
+#define COUT_A( A , N ) cout << "出力： "; OUTPUT_ARRAY( cout , A , N ) << endl
+#define CERR_ITR( A ) OUTPUT_ITR( cerr , A ) << endl
+#define COUT_ITR( A ) cout << "出力： "; OUTPUT_ITR( cout , A ) << endl
+#define ENDL endl
+#define SET_LL( A ) cin >> A
+#define CIN( LL , ... ) LL __VA_ARGS__; VariadicCin( cin , __VA_ARGS__ )
+#define SET_A( A , N ) FOR( VARIABLE_FOR_CIN_A , 0 , N ){ cin >> A[VARIABLE_FOR_CIN_A]; }
+#define CIN_A( LL , A , N ) vector<LL> A( N ); SET_A( A , N );
+#include <bits/stdc++.h>
 using namespace std;
 using uint = unsigned int;
 using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
 using lld = __float128;
+using lld = __float128;
+template <typename INT> using T2 = pair<INT,INT>;
+template <typename INT> using T3 = tuple<INT,INT,INT>;
+template <typename INT> using T4 = tuple<INT,INT,INT,INT>;
+using path = pair<int,ll>;
 #define REPEAT_MAIN( BOUND ) void ExecuteSolution_Body(){ signal( SIGABRT , &AlertAbort ); CEXPR( int , bound_test_case_num , BOUND ); int test_case_num = 1; if constexpr( bound_test_case_num > 1 ){ SET_ASSERT( test_case_num , 1 , bound_test_case_num ); } REPEAT( test_case_num ){ if constexpr( bound_test_case_num > 1 ){ CERR( "testcase " , VARIABLE_FOR_REPEAT_test_case_num , ":" ); } Solve(); } }
 #define START_WATCH chrono::system_clock::time_point watch = chrono::system_clock::now()
 #define CURRENT_TIME static_cast<double>( chrono::duration_cast<chrono::microseconds>( chrono::system_clock::now() - watch ).count() / 1000.0 )
 #define CHECK_WATCH( TL_MS ) ( CURRENT_TIME < TL_MS - 100.0 )
 #define TYPE_OF( VAR ) decay_t<decltype( VAR )>
 #define CEXPR( LL , BOUND , VALUE ) constexpr LL BOUND = VALUE
-#define DEXPR( LL , BOUND , VALUE , DEBUG_VALUE ) CEXPR( LL , BOUND , VALUE )
-#define ASSERT( A , MIN , MAX ) assert( ( MIN ) <= A && A <= ( MAX ) )
-#define CIN( LL , ... ) LL __VA_ARGS__; VariadicCin( cin , __VA_ARGS__ )
-#define SET_ASSERT( A , MIN , MAX ) cin >> A; ASSERT( A , MIN , MAX )
+#define SET_ASSERT( A , MIN , MAX ) SET_LL( A ); ASSERT( A , MIN , MAX )
 #define CIN_ASSERT( A , MIN , MAX ) TYPE_OF( MAX ) A; SET_ASSERT( A , MIN , MAX )
-#define CIN_A( LL , A , N ) LL A[N]; FOR( VARIABLE_FOR_CIN_A , 0 , N ){ cin >> A[VARIABLE_FOR_CIN_A]; }
-#define GETLINE_SEPARATE( SEPARATOR , ... ) string __VA_ARGS__; VariadicGetline( cin , SEPARATOR , __VA_ARGS__ )
-#define GETLINE( ... ) GETLINE_SEPARATE( " " , ... )
 #define FOR( VAR , INITIAL , FINAL_PLUS_ONE ) for( TYPE_OF( FINAL_PLUS_ONE ) VAR = INITIAL ; VAR < FINAL_PLUS_ONE ; VAR ++ )
 #define FOREQ( VAR , INITIAL , FINAL ) for( TYPE_OF( FINAL ) VAR = INITIAL ; VAR <= FINAL ; VAR ++ )
-#define FOREQINV( VAR , INITIAL , FINAL ) for( TYPE_OF( INITIAL ) VAR = INITIAL ; VAR >= FINAL ; VAR -- )
+#define FOREQINV( VAR , INITIAL , FINAL ) for( TYPE_OF( INITIAL ) VAR = INITIAL ; VAR + 1 > FINAL ; VAR -- )
 #define AUTO_ITR( ARRAY ) auto itr_ ## ARRAY = ARRAY .begin() , end_ ## ARRAY = ARRAY .end()
 #define FOR_ITR( ARRAY ) for( AUTO_ITR( ARRAY ) , itr = itr_ ## ARRAY ; itr_ ## ARRAY != end_ ## ARRAY ; itr_ ## ARRAY ++ , itr++ )
 #define REPEAT( HOW_MANY_TIMES ) FOR( VARIABLE_FOR_REPEAT_ ## HOW_MANY_TIMES , 0 , HOW_MANY_TIMES )
-#define SET_PRECISION( PRECISION ) cout << fixed << setprecision( PRECISION )
-#define CERR( ... ) VariadicCout( cerr , __VA_ARGS__ ) << endl
-#define COUT( ... ) VariadicCout( cout , __VA_ARGS__ ) << endl
+#define SET_PRECISION( DECIMAL_DIGITS ) cout << fixed << setprecision( DECIMAL_DIGITS )
 #define OUTPUT_ARRAY( OS , A , N ) FOR( VARIABLE_FOR_OUTPUT_ARRAY , 0 , N ){ OS << A[VARIABLE_FOR_OUTPUT_ARRAY] << (VARIABLE_FOR_OUTPUT_ARRAY==N-1?"":" "); } OS
-#define CERR_A( A , N ) OUTPUT_ARRAY( cerr , A , N ) << endl
-#define COUT_A( A , N ) OUTPUT_ARRAY( cout , A , N ) << endl
 #define OUTPUT_ITR( OS , A ) { auto ITERATOR_FOR_OUTPUT_ITR = A.begin() , END_FOR_OUTPUT_ITR = A.end(); bool VARIABLE_FOR_OUTPUT_ITR = ITERATOR_FOR_COUT_ITR != END_FOR_COUT_ITR; while( VARIABLE_FOR_OUTPUT_ITR ){ OS << *ITERATOR_FOR_COUT_ITR; ( VARIABLE_FOR_OUTPUT_ITR = ++ITERATOR_FOR_COUT_ITR != END_FOR_COUT_ITR ) ? OS : OS << " "; } } OS
-#define CERR_ITR( A ) OUTPUT_ITR( cerr , A ) << endl
-#define COUT_ITR( A ) OUTPUT_ITR( cout , A ) << endl
 #define RETURN( ... ) COUT( __VA_ARGS__ ); return
 
 // 圧縮用
