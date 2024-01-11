83db8cdcdaeca3bc2177b920b1dc1b516f040b16
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-01-11 12:17:58 +0900

    Delete Mathematics/Function/Iteration/Doubling/a_Macro.hpp

--- Mathematics/Function/Iteration/Doubling/a_Macro.hpp
+++
@@ -1,4 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Function/Iteration/Doubling/a_Macro.hpp
-
-#pragma once
-#define SFINAE_FOR_DOUBLING_BODY( DEFAULT ) enable_if_t<is_convertible_v<U,T> >* DEFAULT
