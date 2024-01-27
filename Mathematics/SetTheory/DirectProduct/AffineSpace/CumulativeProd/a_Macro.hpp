a2ad5fad12c33bf7338bcd50d83216b7f48c6143
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-01-27 21:41:00 +0900

    Delete Mathematics/SetTheory/DirectProduct/AffineSpace/CumulativeProd/a_Macro.hpp

--- Mathematics/SetTheory/DirectProduct/AffineSpace/CumulativeProd/a_Macro.hpp
+++
@@ -1,4 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/CumulativeProd/a_Macro.hpp
-
-#pragma once
-#define SFINAE_FOR_CUMULATIVE_PROD( DEFAULT ) enable_if_t<is_convertible_v<U,T> >* DEFAULT
