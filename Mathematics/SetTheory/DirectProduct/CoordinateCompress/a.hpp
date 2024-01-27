ceaf730bfe558b7a3504bc1446ea3ab9b4d480b0
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-01-27 21:41:37 +0900

    Add files via upload

--- Mathematics/SetTheory/DirectProduct/CoordinateCompress/a.hpp
+++ Mathematics/SetTheory/DirectProduct/CoordinateCompress/a.hpp
@@ -1,39 +1,37 @@
 // c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/CoordinateCompress/a.hpp
 
 #pragma once
-#include <map>
+#include "../../../Function/Map/a.hpp"
 
 // verify:
-// https://yukicoder.me/submissions/904160
-// https://yukicoder.me/submissions/924803
+// https://yukicoder.me/submissions/947058（map、GetOrder）
 
-template <typename T>
+template <typename T , template <typename...> typename MAP = Map>
 class CoordinateCompress
 {
 
 private:
   vector<T> m_a;
-  map<T,int> m_enum;
+  MAP<T,int> m_enum;
   bool m_compressed;
   int m_size;
 
 public:
   inline CoordinateCompress();
-  template <typename U , int length_max> inline CoordinateCompress( const U ( &a )[length_max] , const int& length );
   template <typename U> inline CoordinateCompress( const vector<U>& a );
 
   inline void insert( const T& t );
-  template <typename U , int length_max> inline void insert( const U ( &a )[length_max] , const int& length );
   template <typename U> inline void insert( const vector<U>& a );
 
   inline const T& operator[]( const int& i );
   inline const T& GetSmallest( const int& i = 0 );
   inline const T& GetLargest( const int& i = 0 );
+  // MAP = mapの時は大小順、MAP = unordered_mapの時は格納順
   inline int GetOrder( const T& t );
   inline const int& size();
 
-  inline typename map<T,int>::iterator begin();
-  inline typename map<T,int>::iterator end();
+  inline typename MAP<T,int>::iterator begin();
+  inline typename MAP<T,int>::iterator end();
 
 private:
   inline void Compress();
--- Mathematics/SetTheory/DirectProduct/CoordinateCompress/a_Body.hpp
+++ Mathematics/SetTheory/DirectProduct/CoordinateCompress/a_Body.hpp
@@ -3,21 +3,21 @@
 #pragma once
 #include "a.hpp"
 
-template <typename T> inline CoordinateCompress<T>::CoordinateCompress() : m_a() , m_enum() , m_compressed() , m_size() {}
-template <typename T> template <typename U , int length_max> inline CoordinateCompress<T>::CoordinateCompress( const U ( &a )[length_max] , const int& length ) : CoordinateCompress() { insert( a , length ); }
-template <typename T> template <typename U> inline CoordinateCompress<T>::CoordinateCompress( const vector<U>& a ) : CoordinateCompress() { insert( a ); }
+#include "../../../Function/Map/a_Body.hpp"
 
-template <typename T> inline void CoordinateCompress<T>::insert( const T& t ) { m_enum[t]; m_size = m_enum.size(); m_compressed = false; }
-template <typename T> template <typename U , int length_max> inline void CoordinateCompress<T>::insert( const U ( &a )[length_max] , const int& length ) { assert( length <= length_max ); if( length > 0 ){ for( int i = 0 ; i < length ; i++ ){ m_enum[ a[i] ]; } m_size = m_enum.size(); m_compressed = false; } }
-template <typename T> template <typename U> inline void CoordinateCompress<T>::insert( const vector<U>& a ) { const int length = a.size(); if( length > 0 ){ for( int i = 0 ; i < length ; i++ ){ m_enum[ a[i] ]; } m_size = m_enum.size(); m_compressed = false; } }
+template <typename T , template <typename...> typename MAP> inline CoordinateCompress<T,MAP>::CoordinateCompress() : m_a() , m_enum() , m_compressed() , m_size() {}
+template <typename T , template <typename...> typename MAP> template <typename U> inline CoordinateCompress<T,MAP>::CoordinateCompress( const vector<U>& a ) : CoordinateCompress() { insert( a ); }
 
-template <typename T> inline const T& CoordinateCompress<T>::operator[]( const int& i ) { return GetSmallest( i ); }
-template <typename T> inline const T& CoordinateCompress<T>::GetSmallest( const int& i ) { if( ! m_compressed ){ Compress(); } assert( i < m_size ); return m_a[i]; }
-template <typename T> inline const T& CoordinateCompress<T>::GetLargest( const int& i ) { if( ! m_compressed ){ Compress(); } assert( i < m_size ); return m_a[m_size - i - 1]; }
-template <typename T> inline int CoordinateCompress<T>::GetOrder( const T& t ) { if( ! m_compressed ){ Compress(); } return m_enum.count( t ) == 1 ? m_enum[t] : -1; }
-template <typename T> inline const int& CoordinateCompress<T>::size() { return m_size; }
+template <typename T , template <typename...> typename MAP> inline void CoordinateCompress<T,MAP>::insert( const T& t ) { m_enum[t]; m_size = m_enum.size(); m_compressed = false; }
+template <typename T , template <typename...> typename MAP> template <typename U> inline void CoordinateCompress<T,MAP>::insert( const vector<U>& a ) { const int length = a.size(); if( length > 0 ){ for( int i = 0 ; i < length ; i++ ){ m_enum[ a[i] ]; } m_size = m_enum.size(); m_compressed = false; } }
 
-template <typename T> inline typename map<T,int>::iterator CoordinateCompress<T>::begin() { return m_enum.begin(); }
-template <typename T> inline typename map<T,int>::iterator CoordinateCompress<T>::end() { return m_enum.end(); }
+template <typename T , template <typename...> typename MAP> inline const T& CoordinateCompress<T,MAP>::operator[]( const int& i ) { return GetSmallest( i ); }
+template <typename T , template <typename...> typename MAP> inline const T& CoordinateCompress<T,MAP>::GetSmallest( const int& i ) { if( ! m_compressed ){ Compress(); } assert( i < m_size ); return m_a[i]; }
+template <typename T , template <typename...> typename MAP> inline const T& CoordinateCompress<T,MAP>::GetLargest( const int& i ) { if( ! m_compressed ){ Compress(); } assert( i < m_size ); return m_a[m_size - i - 1]; }
+template <typename T , template <typename...> typename MAP> inline int CoordinateCompress<T,MAP>::GetOrder( const T& t ) { if( ! m_compressed ){ Compress(); } return m_enum.count( t ) == 1 ? m_enum[t] : -1; }
+template <typename T , template <typename...> typename MAP> inline const int& CoordinateCompress<T,MAP>::size() { return m_size; }
 
-template <typename T> inline void CoordinateCompress<T>::Compress() { m_a.resize( m_size ); m_size = 0; for( auto itr = m_enum.begin() , end = m_enum.end() ; itr != end ; itr++ ){ m_a[itr->second = m_size++] = itr->first; } m_compressed = true; }
+template <typename T , template <typename...> typename MAP> inline typename MAP<T,int>::iterator CoordinateCompress<T,MAP>::begin() { return m_enum.begin(); }
+template <typename T , template <typename...> typename MAP> inline typename MAP<T,int>::iterator CoordinateCompress<T,MAP>::end() { return m_enum.end(); }
+
+template <typename T , template <typename...> typename MAP> inline void CoordinateCompress<T,MAP>::Compress() { m_a.resize( m_size ); m_size = 0; for( auto itr = m_enum.begin() , end = m_enum.end() ; itr != end ; itr++ ){ m_a[itr->second = m_size++] = itr->first; } m_compressed = true; }
