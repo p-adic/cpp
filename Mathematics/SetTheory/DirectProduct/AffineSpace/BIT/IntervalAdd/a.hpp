36c04e670c70ef275301986606fc32bf30de08e1
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-02-07 00:07:45 +0900

    Add files via upload

--- Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalAdd/a.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalAdd/a.hpp
@@ -3,16 +3,17 @@
 #pragma once
 #include "../a.hpp"
 
-// verify: https://yukicoder.me/submissions/942888（零初期化、一点加算、区間加算、一点取得）
+// verify:
+// https://yukicoder.me/submissions/948000（零初期化、一点加算、区間加算、一点取得）
 
 // 使用演算：
-// T& T::operator=( const T& )（BITそのものに使用）
-// T& T::operator+=( const T& )
-// T& operator+( const T& , const T& )
-// T operator-( const T& )
-// T operator-( const T& , const T& )
+// U& U::operator=( const U& )（BITそのものに使用）
+// U& U::operator+=( const U& )
+// U& operator+( const U& , const U& )
+// U operator-( const U& )
+// U operator-( const U& , const U& )
 
-// T()による初期化O(size)
+// U()による初期化O(size)
 // 配列による初期化O(size)
 
 // 一点取得O(log_2 size)
@@ -23,35 +24,35 @@
 // +による一点更新O(log_2 size)
 // +による区間更新O(log_2 size)
 // 配列の加算による全体更新O(size)
-template <typename T>
+template <typename U>
 class IntervalAddBIT
 {
 private:
   // 母関数の微分の負の階差数列（(i-1)a_{i-1} - ia_i）の管理
-  BIT<T> m_bit_0;
+  BIT<U> m_bit_0;
   // 階差数列（a_i - a_{i-1}）の管理
-  BIT<T> m_bit_1;
+  BIT<U> m_bit_1;
 
 public:
   inline IntervalAddBIT( const int& size = 0 );
-  inline IntervalAddBIT( const vector<T>& a );
+  inline IntervalAddBIT( const vector<U>& a );
 
-  inline IntervalAddBIT<T>& operator=( IntervalAddBIT<T>&& a );
+  inline IntervalAddBIT<U>& operator=( IntervalAddBIT<U>&& a );
 
-  inline void Set( const int& i , const T& n );
-  inline void Set( const vector<T>& a );
+  inline void Set( const int& i , const U& u );
+  inline void Set( const vector<U>& a );
   inline void Initialise( const int& size = 0 );
 
-  inline IntervalAddBIT<T>& operator+=( const vector<T>& a );
-  // 0<=i<Nの場合は第i成分にnを足し、そうでない場合は何もしない。
-  inline void Add( const int& i , const T& n );
-  // max(0,i_start)<=i<=min(N-1,i_final)を満たすiに対し各第i成分にnを足す。
-  inline void IntervalAdd( const int& i_start , const int& i_final , const T& n );
+  inline IntervalAddBIT<U>& operator+=( const vector<U>& a );
+  inline void Add( const int& i , const U& u );
+  // max(0,i_start)<=i<=min(N-1,i_final)を満たすiに対し各第i成分にuを足す。
+  inline void IntervalAdd( const int& i_start , const int& i_final , const U& u );
 
+  inline const int& size() const noexcept;
   // const参照でないことに注意。
-  inline T operator[]( const int& i ) const;
-  inline T Get( const int& i ) const;
-  inline T InitialSegmentSum( const int& i_final ) const;
-  inline T IntervalSum( const int& i_start , const int& i_final ) const;
+  inline U operator[]( const int& i ) const;
+  inline U Get( const int& i ) const;
+  inline U InitialSegmentSum( const int& i_final ) const;
+  inline U IntervalSum( const int& i_start , const int& i_final ) const;
   
 };
--- Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalAdd/a_Body.hpp
+++ Mathematics/SetTheory/DirectProduct/AffineSpace/BIT/IntervalAdd/a_Body.hpp
@@ -5,24 +5,25 @@
 
 #include "../a_Body.hpp"
 
-template <typename T> inline IntervalAddBIT<T>::IntervalAddBIT( const int& size ) : m_bit_0( size ) , m_bit_1( size ) {}
-template <typename T> inline IntervalAddBIT<T>::IntervalAddBIT( const vector<T>& a ) : m_bit_0() , m_bit_1() { const int size = a.size(); vector<T> diff( size ); diff[0] = a[0]; for( int i = 1 ; i < size ; i++ ){ diff[i] = a[i] - a[i-1]; } m_bit_0.Set( diff ); for( int i = 1 ; i < size ; i++ ){ ( diff[i] *= 1 - i ) -= a[i]; } m_bit_1.Set( diff ); }
+template <typename U> inline IntervalAddBIT<U>::IntervalAddBIT( const int& size ) : m_bit_0( size ) , m_bit_1( size ) {}
+template <typename U> inline IntervalAddBIT<U>::IntervalAddBIT( const vector<U>& a ) : m_bit_0() , m_bit_1() { const int size = a.size(); vector<U> diff( size ); diff[0] = a[0]; for( int i = 1 ; i < size ; i++ ){ diff[i] = a[i] - a[i-1]; } m_bit_0.Set( diff ); for( int i = 1 ; i < size ; i++ ){ ( diff[i] *= 1 - i ) -= a[i]; } m_bit_1.Set( diff ); }
 
-template <typename T> inline IntervalAddBIT<T>& IntervalAddBIT<T>::operator=( IntervalAddBIT<T>&& a ) { m_bit_0 = move( a.m_bit_0 ); m_bit_1 = move( a.m_bit_1 ); }
+template <typename U> inline IntervalAddBIT<U>& IntervalAddBIT<U>::operator=( IntervalAddBIT<U>&& a ) { m_bit_0 = move( a.m_bit_0 ); m_bit_1 = move( a.m_bit_1 ); }
 
-template <typename T> inline void IntervalAddBIT<T>::Set( const int& i , const T& n ) { Add( i , n - IntervalSum( i , i ) ); }
-template <typename T> inline void IntervalAddBIT<T>::Set( const vector<T>& a ) { *this = IntervalAddBIT<T>( a ); }
-template <typename T> inline void IntervalAddBIT<T>::Initialise( const int& size ) { m_bit_0.Initialise( size ); m_bit_1.Initialise( size ); }
+template <typename U> inline void IntervalAddBIT<U>::Set( const int& i , const U& u ) { Add( i , u - IntervalSum( i , i ) ); }
+template <typename U> inline void IntervalAddBIT<U>::Set( const vector<U>& a ) { *this = IntervalAddBIT<U>( a ); }
+template <typename U> inline void IntervalAddBIT<U>::Initialise( const int& size ) { m_bit_0.Initialise( size ); m_bit_1.Initialise( size ); }
 
-template <typename T> inline IntervalAddBIT<T>& IntervalAddBIT<T>::operator+=( const vector<T>& a ) { IntervalAddBIT<T> a_copy{ a }; const int size = a.size(); for( int i = 1 ; i < size ; i++ ){ m_bit_0[i] += a_copy.m_bit_0[i]; m_bit_1[i] += a_copy.m_bit_1[i]; } return *this; }
+template <typename U> inline IntervalAddBIT<U>& IntervalAddBIT<U>::operator+=( const vector<U>& a ) { IntervalAddBIT<U> a_copy{ a }; m_bit_0 += a_copy.m_bit_0; m_bit_1 += a_copy.m_bit_1; return *this; }
 
-template <typename T> inline void IntervalAddBIT<T>::Add( const int& i , const T& n ) { IntervalAdd( i , i , n ); }
+template <typename U> inline void IntervalAddBIT<U>::Add( const int& i , const U& u ) { IntervalAdd( i , i , u ); }
 
-template <typename T> inline void IntervalAddBIT<T>::IntervalAdd( const int& i_start , const int& i_final , const T& n ) { m_bit_0.Add( i_start , - ( i_start - 1 ) * n ); m_bit_0.Add( i_final + 1 , i_final * n ); m_bit_1.Add( i_start , n ); m_bit_1.Add( i_final + 1 , - n ); }
+template <typename U> inline void IntervalAddBIT<U>::IntervalAdd( const int& i_start , const int& i_final , const U& u ) { m_bit_0.Add( i_start , - ( i_start - 1 ) * u ); m_bit_0.Add( i_final + 1 , i_final * u ); m_bit_1.Add( i_start , u ); m_bit_1.Add( i_final + 1 , - u ); }
 
-template <typename T> inline T IntervalAddBIT<T>::operator[]( const int& i ) const { assert( 0 <= i && i < m_bit_0.size() ); return IntervalSum( i , i ); }
-template <typename T> inline T IntervalAddBIT<T>::Get( const int& i ) const { return operator[]( i ); }
-template <typename T> inline T IntervalAddBIT<T>::InitialSegmentSum( const int& i_final ) const { return m_bit_0.InitialSegmentSum( i_final ) + i_final * m_bit_1.InitialSegmentSum( i_final ); }
+template <typename U> inline const int& IntervalAddBIT<U>::size() const noexcept { return m_bit_0.size(); }
+template <typename U> inline U IntervalAddBIT<U>::operator[]( const int& i ) const { assert( 0 <= i && i < size() ); return IntervalSum( i , i ); }
+template <typename U> inline U IntervalAddBIT<U>::Get( const int& i ) const { return operator[]( i ); }
+template <typename U> inline U IntervalAddBIT<U>::InitialSegmentSum( const int& i_final ) const { return m_bit_0.InitialSegmentSum( i_final ) + i_final * m_bit_1.InitialSegmentSum( i_final ); }
 
-template <typename T> inline T IntervalAddBIT<T>::IntervalSum( const int& i_start , const int& i_final ) const { return InitialSegmentSum( i_final ) - InitialSegmentSum( i_start - 1 ); }
+template <typename U> inline U IntervalAddBIT<U>::IntervalSum( const int& i_start , const int& i_final ) const { return InitialSegmentSum( i_final ) - InitialSegmentSum( i_start - 1 ); }
 
