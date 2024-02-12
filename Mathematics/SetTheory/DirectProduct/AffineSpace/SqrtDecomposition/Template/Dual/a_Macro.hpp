46cbda789a147676c82525dd7864eec81a983c4f
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-02-12 16:19:10 +0900

    Delete Mathematics/SetTheory/DirectProduct/AffineSpace/SqrtDecomposition directory

--- Mathematics/SetTheory/DirectProduct/AffineSpace/SqrtDecomposition/Template/Dual/a_Macro.hpp
+++
@@ -1,4 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/SqrtDecomposition/Template/Dual/a_Macro.hpp
-
-#pragma once
-#define TEMPLATE_ARGUMENTS_FOR_DUAL_SQRT_DECOMPOSITION typename T , T m_T(const T&,const T&) , const T& e_T() , typename U , U o_U(const T&,const U&) , int N , int N_sqrt
--- Mathematics/SetTheory/DirectProduct/AffineSpace/SqrtDecomposition/Template/Monoid/a_Macro.hpp
+++
@@ -1,4 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/SetTheory/DirectProduct/AffineSpace/SqrtDecomposition/Template/Monoid/a_Macro.hpp
-
-#pragma once
-#define TEMPLATE_ARGUMENTS_FOR_MONOID_SQRT_DECOMPOSITION typename T , T m_T(const T&,const T&) , const T& e_T() , int N , int N_sqrt
