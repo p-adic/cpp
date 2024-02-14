4b3bdfaed588522e2639b813a47cc910c4d2de16
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-02-14 15:30:04 +0900

    Add files via upload

--- Mathematics/Geometry/Graph/BreadthFirstSearch/BitExhausiveSearch/a.hpp
+++ Mathematics/Geometry/Graph/BreadthFirstSearch/BitExhausiveSearch/a.hpp
@@ -6,30 +6,31 @@
 #include "../a.hpp"
 
 // verify:
-// https://yukicoder.me/submissions/945926（NextSubset）
+// https://yukicoder.me/submissions/950728（NextSubset）
 
 class BitExhausiveEdge
 {
 
 public:
-  int* m_p_V;
-  inline BitExhausiveEdge( const int& V );
+  const int* m_p_V;
   inline list<int> operator()( const int& t );
 
 };
 
+// bitを0から1に変更する方向に有向辺を貼ったグラフに対するBFS
 class BitExhausiveSearch :
   public BreadthFirstSearch<Graph<BitExhausiveEdge>>
 {
 
 private:
   Graph<BitExhausiveEdge> m_G;
+  const int* m_p_V;
   int m_digit;
   
 public:
   // Vは最大の集合に対応する整数+1（digitの羃集合全体なら1<<digit）
-  inline BitExhausiveSearch( const int& digit , const int& V );
-  inline BitExhausiveSearch( const int& digit );
+  inline BitExhausiveSearch( const int& V , const int& digit );
+  inline BitExhausiveSearch( const int& V );
 
   // tに対応する部分集合に1つ要素を追加した部分集合で未到達なものを格納。
   inline list<int> UnreachedAdjacentSupsetOf( const int& t ) noexcept;
@@ -54,7 +55,9 @@ public:
   // 対応する部分集合の濃度を変えずにtを昇順で後続に遷移させる。
   inline bool NextSubset( int& t ) noexcept;
 
-  // tの2進加法付値を返す。
+  // tの2進表記における桁数を返す。
+  static inline int DigitOf( const int& t ) noexcept;
+  // tの2進加法付値をメモ化して返す。（合計O(max t)）
   static inline const int& ValuationOf( const int& t ) noexcept;
   
 };
--- Mathematics/Geometry/Graph/BreadthFirstSearch/BitExhausiveSearch/a_Body.hpp
+++ Mathematics/Geometry/Graph/BreadthFirstSearch/BitExhausiveSearch/a_Body.hpp
@@ -7,20 +7,22 @@
 
 inline list<int> BitExhausiveEdge::operator()( const int& t ) { DEFINITION_OF_ADJACENT_SET( ~t , | , *m_p_V , answer.push_back( t_next ); ); }
 
-inline BitExhausiveSearch::BitExhausiveSearch( const int& digit , const int& V ) : BreadthFirstSearch<Graph<BitExhausiveEdge>>( m_G ) , m_G( V , BitExhausiveEdge() ) , m_V( m_G.size() ) , m_digit( digit ) { assert( 0 <= m_digit && m_digit <= 30 && ( m_V >> m_digit ) <= 1 ); m_G.edge().m_p_V = &( m_G.size() ); }
-inline BitExhausiveSearch::BitExhausiveSearch( const int& digit ) : BitExhausiveSearch( digit , 1 << digit ) {}
+inline BitExhausiveSearch::BitExhausiveSearch( const int& V , const int& digit ) : BreadthFirstSearch<Graph<BitExhausiveEdge>>( m_G ) , m_G( V , BitExhausiveEdge() ) , m_p_V( &( m_G.size() ) ) , m_digit( digit ) { assert( 0 <= m_digit && m_digit <= 30 && ( *m_p_V >> m_digit ) <= 1 ); m_G.edge().m_p_V = m_p_V; }
+inline BitExhausiveSearch::BitExhausiveSearch( const int& V ) : BitExhausiveSearch( V , DigitOf( V ) ) {}
 
-inline list<int> BitExhausiveSearch::UnreachedAdjacentSupsetOf( const int& t ) noexcept { DEFINITION_OF_ADJACENT_SET( ~t , | , m_V , if( ! this->found( t_next ) ){ answer.push_back( t_next ); } ); }
-inline list<int> BitExhausiveSearch::UnreachedProperSupsetOf( const int& t ) noexcept { DEFINITION_OF_PROPER_SET( ++ , |= , m_V , if( ! this->found( t_next ) ){ answer.push_back( t_next ); } ); }
-inline list<int> BitExhausiveSearch::AdjacentSupsetOf( const int& t ) noexcept { DEFINITION_OF_ADJACENT_SET( ~t , | , m_V , answer.push_back( t_next ) ); }
-inline list<int> BitExhausiveSearch::ProperSupsetOf( const int& t ) noexcept { DEFINITION_OF_PROPER_SET( ++ , |= , m_V , answer.push_back( t_next ); ); }
+inline list<int> BitExhausiveSearch::UnreachedAdjacentSupsetOf( const int& t ) noexcept { DEFINITION_OF_ADJACENT_SET( ~t , | , *m_p_V , if( ! this->found( t_next ) ){ answer.push_back( t_next ); } ); }
+inline list<int> BitExhausiveSearch::UnreachedProperSupsetOf( const int& t ) noexcept { DEFINITION_OF_PROPER_SET( ++ , |= , *m_p_V , if( ! this->found( t_next ) ){ answer.push_back( t_next ); } ); }
+inline list<int> BitExhausiveSearch::AdjacentSupsetOf( const int& t ) noexcept { DEFINITION_OF_ADJACENT_SET( ~t , | , *m_p_V , answer.push_back( t_next ) ); }
+inline list<int> BitExhausiveSearch::ProperSupsetOf( const int& t ) noexcept { DEFINITION_OF_PROPER_SET( ++ , |= , *m_p_V , answer.push_back( t_next ); ); }
 
 inline list<int> BitExhausiveSearch::UnreachedAdjacentSubsetOf( const int& t ) noexcept { DEFINITION_OF_ADJACENT_SET( t , ^ , t , if( ! this->found( t_next ) ){ answer.push_back( t_next ); } ); }
 inline list<int> BitExhausiveSearch::UnreachedProperSubsetOf( const int& t ) noexcept { DEFINITION_OF_PROPER_SET( -- , ^= , t , if( ! this->found( t_next ) ){ answer.push_back( t_next ); } ); }
 inline list<int> BitExhausiveSearch::AdjacentSubsetOf( const int& t ) noexcept { DEFINITION_OF_ADJACENT_SET( t , ^ , t , answer.push_back( t_next ) ); }
 inline list<int> BitExhausiveSearch::ProperSubsetOf( const int& t ) noexcept { DEFINITION_OF_PROPER_SET( -- , ^= , t , answer.push_back( t_next ); ); }
 
-inline bool BitExhausiveSearch::NextSubset( int& t ) noexcept { vector<int> S( m_digit ); for( int d = 0 ; d < m_digit ; d++ ){ S[d] = ( ( t >> ( m_digit - d - 1 ) ) & 1 ); } bool b = next_permutation( S.begin() , S.end() ); t = 0; for( int d = 0 ; d < m_digit ; d++ ){ t |= S[d] << ( m_digit - d - 1 ); b = b && t < m_V; } return b; }
+inline bool BitExhausiveSearch::NextSubset( int& t ) noexcept { vector<int> S( m_digit ); for( int d = 0 ; d < m_digit ; d++ ){ S[d] = ( ( t >> ( m_digit - d - 1 ) ) & 1 ); } bool b = next_permutation( S.begin() , S.end() ); t = 0; for( int d = 0 ; d < m_digit ; d++ ){ t |= S[d] << ( m_digit - d - 1 ); b = b && t < *m_p_V; } return b; }
+
+inline int BitExhausiveSearch::DigitOf( const int& t ) noexcept { int digit = 0; int power = 1; while( t > power ){ power <<= 1; digit++; } return digit; }
 
 inline const int& BitExhausiveSearch::ValuationOf( const int& t ) noexcept
 {
