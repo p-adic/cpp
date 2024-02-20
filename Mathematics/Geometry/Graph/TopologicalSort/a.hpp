37d4444417b05b86a0f1fc188e0801c14942c133
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-02-20 13:44:00 +0900

    Add files via upload

--- Mathematics/Geometry/Graph/TopologicalSort/a.hpp
+++ Mathematics/Geometry/Graph/TopologicalSort/a.hpp
@@ -7,13 +7,14 @@
 // https://yukicoder.me/submissions/953259（GetLongestWalk）
 
 // 入力の範囲内で要件
-// (1) Gが辺Edge:T->(T \times U)^{< \omega}を持ち閉路を持たない有向グラフである。
-// (2) Mがoperator<(const U&,const U&)に関してUの順序モノイド構造である。
+// (1) Gが辺Edge:T->(T \times ...)^{< \omega}を持ち閉路を持たない有向グラフである。
 // を満たす場合にのみサポート。
+// GのトポロジカルソートをO(|V_G|+|E_G|)で計算する。
 template <typename GRAPH> list<inner_t<GRAPH>> TopologicalSort( GRAPH& G );
 
 // 入力の範囲内で要件
 // (1) Gが辺Edge:T->(T \times U)^{< \omega}を持ち閉路を持たない有向グラフである。
 // (2) Mがoperator<(const U&,const U&)に関してUの順序モノイド構造である。
 // を満たす場合にのみサポート。
+// Gの最長歩道をO(|V_G|+|E_G|)で計算する。
 template <typename GRAPH , typename MONOID> pair<inner_t<MONOID>,list<inner_t<GRAPH>>> GetLongestWalk( GRAPH& G , MONOID M );
