7df0c4102b5237cae9dd0a59f6557d6e034ada96
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-03-04 09:07:55 +0900

    Add files via upload

--- Mathematics/Geometry/Graph/UnionFindForest/a.hpp
+++ Mathematics/Geometry/Graph/UnionFindForest/a.hpp
@@ -1,14 +1,17 @@
-// c:/Users/user/Documents/Programming/Utility/VLTree/UnionFindForest/a.hpp
+// c:/Users/user/Documents/Programming/Mathematics/Geometry/Graph/UnionFindForest/a.hpp
 
 #pragma once
+#include "../../../Algebra/Monoid/Group/a.hpp"
 
 // verify:
 // https://yukicoder.me/submissions/941558
 // https://yukicoder.me/submissions/941553
 // https://yukicoder.me/submissions/941556
-// https://atcoder.jp/contests/abc087/submissions/48977983（符号付き重み）
+// https://atcoder.jp/contests/abc087/submissions/50885973（ポテンシャル）
 
-// Tが加法について可換群をなす場合にのみサポート。
+// 入力の範囲内で要件
+// (1) MがUの可換群構造である。
+// を満たす場合にのみサポート。
 
 // - 構築 O(size)
 
@@ -16,36 +19,51 @@
 // - 全根取得 O(size)
 // - 根数取得 O(1)
 
-// - 二点符号付き重み取得 O(α(size))
+// - 二点間ポテンシャル取得 O(α(size))
 // - 二点接合 O(α(size))
-template <typename T = int>
-class UnionFindForest
+template <typename U , typename GROUP>
+class AbstractUnionFindForest
 {
 
 private:
-  uint m_node_size;
-  uint m_root_size;
-  vector<uint> m_pred;
-  vector<uint> m_height;
-  // m_w[num]はnum番目のnodeがrootならば0、rootでないならば親nodeへ向かうパスの符号付き重み
-  vector<T> m_w;
+  GROUP m_M;
+  int m_node_size;
+  int m_root_size;
+  vector<int> m_pred;
+  vector<int> m_height;
+  // m_w[num]はnum番目のnodeがrootならば0、rootでないならば親nodeからのパスの符号付き重みを返す。
+  // Graftと整合的な解xが存在する場合にはx[num]-x[親node]を返すことと等価。
+  vector<U> m_w;
   
 public:
-  inline UnionFindForest( const uint& size );
+  inline AbstractUnionFindForest( const int& size , GROUP M );
 
   // num番目のnodeのrootを計算して返す。
-  const uint& RootOfNode( const uint& num );
+  const int& RootOfNode( const int& num );
   // rootを全て格納する。
-  template <template <typename...> typename V> void SetRoot( V<uint>& a ) const;
+  template <template <typename...> typename V> void SetRoot( V<int>& a ) const;
 
   // num1番目のnodeからnum0番目のnodeへ向かうパスの符号付き重みを返す。
-  inline T Weight( const uint& num0 , const uint& num1 );
-  
-  inline const uint& SizeOfNode() const noexcept;
-  inline const uint& SizeOfRoot() const noexcept;
+  // Graftと整合的な解xが存在する時のx[num0]-x[num1]の取得と等価。
+  inline U Potential( const int& num0 , const int& num1 );
+
+  // 
+  inline const int& size( const bool& node = true ) const noexcept;
 
   // num1番目のnodeからnum0番目のnodeへ符号付き重みwの有向辺を結ぶ操作と整合的に
   // なるようにrootを接合。符号付き重みの整合性が取れない場合はfalseを返す。
-  bool Graft( const uint& num0 , const uint& num1 , const T& w = 0 );
+  // 返り値はx[num0]-x[num1]=wの解の有無と等価。
+  bool Graft( const int& num0 , const int& num1 , const U& w = 0 );
   
+};
+template <typename GROUP> AbstractUnionFindForest( const int& size , GROUP M ) -> AbstractUnionFindForest<inner_t<GROUP>,GROUP>;
+
+template <typename U = int>
+class UnionFindForest :
+  public AbstractUnionFindForest<U,AdditiveGroup<U>>
+{
+
+public:
+  inline UnionFindForest( const int& size );
+
 };
--- Mathematics/Geometry/Graph/UnionFindForest/a_Body.hpp
+++ Mathematics/Geometry/Graph/UnionFindForest/a_Body.hpp
@@ -1,19 +1,22 @@
-// c:/Users/user/Documents/Programming/Utility/VLTree/UnionFindForest/a_Body.hpp
+// c:/Users/user/Documents/Programming/Mathematics/Geometry/Graph/UnionFindForest/a_Body.hpp
 
 #pragma once
 #include "a.hpp"
 
-template <typename T> inline UnionFindForest<T>::UnionFindForest( const uint& size ) : m_node_size( size ) , m_root_size( m_node_size ) , m_pred( m_node_size ) , m_height( m_node_size , 1 ) , m_w( m_node_size ) { for( uint i = 0 ; i < m_node_size ; i++ ){ m_pred[i] = i; } }
+#include "../../../Algebra/Monoid/Group/a_Body.hpp"
 
-template <typename T>
-const uint& UnionFindForest<T>::RootOfNode( const uint& num )
+template <typename U , typename GROUP> inline AbstractUnionFindForest<U,GROUP>::AbstractUnionFindForest( const int& size , GROUP M ) : m_M( move( M ) ) , m_node_size( size ) , m_root_size( m_node_size ) , m_pred( m_node_size ) , m_height( m_node_size , 1 ) , m_w( m_node_size , m_M.Zero() ) { for( int i = 0 ; i < m_node_size ; i++ ){ m_pred[i] = i; } }
+template <typename U> inline UnionFindForest<U>::UnionFindForest( const int& size ) : AbstractUnionFindForest<U,AdditiveGroup<U>>( size , AdditiveGroup<U>() ) {}
+
+template <typename U , typename GROUP>
+const int& AbstractUnionFindForest<U,GROUP>::RootOfNode( const int& num )
 {
 
-  uint& pred1 = m_pred[num];
+  int& pred1 = m_pred[num];
 
   while( true ){
     
-    uint& pred2 = m_pred[pred1];
+    int& pred2 = m_pred[pred1];
 
     if( pred1 == pred2 ){
 
@@ -21,7 +24,7 @@ const uint& UnionFindForest<T>::RootOfNode( const uint& num )
 
     }
 
-    m_w[num] += m_w[pred1] += m_w[pred2];
+    m_w[num] = m_M.Sum( m_w[num] , m_w[pred1] = m_M.Sum( m_w[pred1] , m_w[pred2] ) );
     pred1 = pred2 = m_pred[pred2];
 
   }
@@ -30,13 +33,13 @@ const uint& UnionFindForest<T>::RootOfNode( const uint& num )
 
 }
 
-template <typename T> template <template <typename...> typename V>
-void UnionFindForest<T>::SetRoot( V<uint>& a ) const
+template <typename U , typename GROUP> template <template <typename...> typename V>
+void AbstractUnionFindForest<U,GROUP>::SetRoot( V<int>& a ) const
 {
 
   a.clear();
 
-  for( uint i = 0 ; i < m_node_size ; i++ ){
+  for( int i = 0 ; i < m_node_size ; i++ ){
 
     if( i == m_pred[i] ){
       
@@ -50,47 +53,46 @@ void UnionFindForest<T>::SetRoot( V<uint>& a ) const
 
 }
 
-template <typename T>
-T UnionFindForest<T>::Weight( const uint& num0 , const uint& num1 )
+template <typename U , typename GROUP>
+U AbstractUnionFindForest<U,GROUP>::Potential( const int& num0 , const int& num1 )
 {
 
   assert( num0 < m_node_size && num1 < m_node_size );
-  const uint& root0 = RootOfNode( num0 );
-  const uint& root1 = RootOfNode( num1 );
+  const int& root0 = RootOfNode( num0 );
+  const int& root1 = RootOfNode( num1 );
   assert( root0 == root1 );
-  return m_w[num1] - m_w[num0];
+  return m_M.Sum( m_w[num0] , m_M.Inverse( m_w[num1] ) );
 
 }
 
-template <typename T> inline const uint& UnionFindForest<T>::SizeOfNode() const noexcept { return m_node_size; }
-template <typename T> inline const uint& UnionFindForest<T>::SizeOfRoot() const noexcept { return m_root_size; }
+template <typename U , typename GROUP> inline const int& AbstractUnionFindForest<U,GROUP>::size( const bool& node ) const noexcept { return node ? m_node_size : m_root_size; }
 
-template <typename T>
-bool UnionFindForest<T>::Graft( const uint& num0 , const uint& num1 , const T& w )
+template <typename U , typename GROUP>
+bool AbstractUnionFindForest<U,GROUP>::Graft( const int& num0 , const int& num1 , const U& w )
 {
 
   assert( num0 < m_node_size && num1 < m_node_size );
-  const uint& root0 = RootOfNode( num0 );
-  const uint& root1 = RootOfNode( num1 );
+  const int& root0 = RootOfNode( num0 );
+  const int& root1 = RootOfNode( num1 );
 
   if( root0 == root1 ){
 
-    return Weight( num0 , num1 ) == w;
+    return Potential( num0 , num1 ) == w;
     
   }
 
-  uint& height0 = m_height[root0];
-  const uint& height1 = m_height[root1];
-  const uint* p_removed_root;
-  const uint* p_removed_node;
-  const uint* p_kept_root;
+  int& height0 = m_height[root0];
+  const int& height1 = m_height[root1];
+  const int* p_removed_root;
+  const int* p_removed_node;
+  const int* p_kept_root;
 
   if( height0 < height1 ){
 
     p_removed_root = &root0;
     p_removed_node = &num0;
     p_kept_root = &root1;
-    m_w[*p_removed_root] -= w - m_w[num1] + m_w[num0];
+    m_w[*p_removed_root] = m_M.Sum( m_w[*p_removed_root] , m_M.Sum( m_M.Sum( m_w[num1] , w ) , m_M.Inverse( m_w[num0] ) ) );
 
   } else {
 
@@ -103,13 +105,13 @@ bool UnionFindForest<T>::Graft( const uint& num0 , const uint& num1 , const T& w
     p_removed_root = &root1;
     p_removed_node = &num1;
     p_kept_root = &root0;
-    m_w[*p_removed_root] += w - m_w[num1] + m_w[num0];
+    m_w[*p_removed_root] = m_M.Sum( m_w[*p_removed_root] , m_M.Sum( m_M.Inverse( m_M.Sum( m_w[num1] , w ) ) , m_w[num0] ) );
 
   }
 
   if( *p_removed_node != *p_removed_root ){
 
-    m_w[*p_removed_node] += m_w[*p_removed_root];
+    m_w[*p_removed_node] = m_M.Sum( m_w[*p_removed_node] , m_w[*p_removed_root] );
 
   }
   
