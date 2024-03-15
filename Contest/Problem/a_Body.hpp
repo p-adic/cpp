83c90b4d320b439e7f7bfca538ddb2217d5aa872
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-03-15 10:03:49 +0900

    Add files via upload

--- Contest/Problem/a_Body.hpp
+++ Contest/Problem/a_Body.hpp
@@ -18,11 +18,11 @@ template <typename T> constexpr T PositiveBaseQuotient( const T& a , const T& p 
 template <typename T> constexpr T Quotient( const T& a , const T& p ){ return p < 0 ? PositiveBaseQuotient( -a , -p ) : PositiveBaseQuotient( a , p ); }
 
 #define POWER( ANSWER , ARGUMENT , EXPONENT )				\
-  static_assert( ! is_same<TYPE_OF( ARGUMENT ),int>::value && ! is_same<TYPE_OF( ARGUMENT ),uint>::value ); \
-  TYPE_OF( ARGUMENT ) ANSWER{ 1 };					\
+  static_assert( ! is_same<decldecay_t( ARGUMENT ),int>::value && ! is_same<decldecay_t( ARGUMENT ),uint>::value ); \
+  decldecay_t( ARGUMENT ) ANSWER{ 1 };					\
   {									\
-    TYPE_OF( ARGUMENT ) ARGUMENT_FOR_SQUARE_FOR_POWER = ( ARGUMENT );	\
-    TYPE_OF( EXPONENT ) EXPONENT_FOR_SQUARE_FOR_POWER = ( EXPONENT );	\
+    decldecay_t( ARGUMENT ) ARGUMENT_FOR_SQUARE_FOR_POWER = ( ARGUMENT );	\
+    decldecay_t( EXPONENT ) EXPONENT_FOR_SQUARE_FOR_POWER = ( EXPONENT );	\
     while( EXPONENT_FOR_SQUARE_FOR_POWER != 0 ){			\
       if( EXPONENT_FOR_SQUARE_FOR_POWER % 2 == 1 ){			\
 	ANSWER *= ARGUMENT_FOR_SQUARE_FOR_POWER;			\
@@ -37,7 +37,7 @@ template <typename T> constexpr T Quotient( const T& a , const T& p ){ return p 
   {									\
     ll ARGUMENT_FOR_SQUARE_FOR_POWER = ( ( ARGUMENT ) % ( MODULO ) ) % ( MODULO ); \
     ARGUMENT_FOR_SQUARE_FOR_POWER < 0 ? ARGUMENT_FOR_SQUARE_FOR_POWER += ( MODULO ) : ARGUMENT_FOR_SQUARE_FOR_POWER; \
-    TYPE_OF( EXPONENT ) EXPONENT_FOR_SQUARE_FOR_POWER = ( EXPONENT );	\
+    decldecay_t( EXPONENT ) EXPONENT_FOR_SQUARE_FOR_POWER = ( EXPONENT );	\
     while( EXPONENT_FOR_SQUARE_FOR_POWER != 0 ){			\
       if( EXPONENT_FOR_SQUARE_FOR_POWER % 2 == 1 ){			\
 	ANSWER = ( ANSWER * ARGUMENT_FOR_SQUARE_FOR_POWER ) % ( MODULO ); \
@@ -66,61 +66,53 @@ template <typename T> constexpr T Quotient( const T& a , const T& p ){ return p 
 // 二分探索用
 // EXPRESSIONがANSWERの広義単調関数の時、EXPRESSION >= CONST_TARGETの整数解を格納。
 #define BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , DESIRED_INEQUALITY , CONST_TARGET , INEQUALITY_FOR_CHECK , UPDATE_U , UPDATE_L , UPDATE_ANSWER ) \
-  static_assert( ! is_same<TYPE_OF( CONST_TARGET ),uint>::value && ! is_same<TYPE_OF( CONST_TARGET ),ull>::value ); \
+  static_assert( ! is_same<decldecay_t( CONST_TARGET ),uint>::value && ! is_same<decldecay_t( CONST_TARGET ),ull>::value ); \
   ll ANSWER = MINIMUM;							\
   {									\
-  ll L_BS = MINIMUM;							\
-  ll U_BS = MAXIMUM;							\
-  ANSWER = UPDATE_ANSWER;						\
-  ll EXPRESSION_BS;							\
-  const ll CONST_TARGET_BS = ( CONST_TARGET );				\
-  ll DIFFERENCE_BS;							\
-  while( L_BS < U_BS ){							\
-    DIFFERENCE_BS = ( EXPRESSION_BS = ( EXPRESSION ) ) - CONST_TARGET_BS; \
-    CERR( "二分探索中：" , "L_BS =" , L_BS , "<=" , ANSWER , "<=" , U_BS , "= U_BS :" , #EXPRESSION , "-" , #CONST_TARGET , "=" , EXPRESSION_BS , "-" , CONST_TARGET_BS , "=" , DIFFERENCE_BS ); \
-    if( DIFFERENCE_BS INEQUALITY_FOR_CHECK 0 ){				\
-      U_BS = UPDATE_U;							\
-    } else {								\
-      L_BS = UPDATE_L;							\
-    }									\
+    ll L_BS = MINIMUM;							\
+    ll U_BS = MAXIMUM;							\
     ANSWER = UPDATE_ANSWER;						\
-  }									\
-  if( L_BS > U_BS ){							\
-    CERR( "二分探索失敗：" , "L_BS =" , L_BS , ">" , U_BS , "= U_BS :" , #ANSWER , ":=" , #MAXIMUM , "+ 1 =" , MAXIMUM + 1  ); \
-    CERR( "二分探索マクロにミスがある可能性があります。変更前の版に戻してください。" );	\
-    ANSWER = MAXIMUM + 1;						\
-  } else {								\
-    CERR( "二分探索終了：" , "L_BS =" , L_BS , "<=" , ANSWER , "<=" , U_BS , "= U_BS" ); \
-    CERR( "二分探索が成功したかを確認するために" , #EXPRESSION , "を計算します。" ); \
-    CERR( "成功判定が不要な場合はこの計算を削除しても構いません。" );	\
-    EXPRESSION_BS = ( EXPRESSION );					\
-    CERR( "二分探索結果：" , #EXPRESSION , "=" , EXPRESSION_BS , ( EXPRESSION_BS > CONST_TARGET_BS ? ">" : EXPRESSION_BS < CONST_TARGET_BS ? "<" : "=" ) , CONST_TARGET_BS ); \
-    if( EXPRESSION_BS DESIRED_INEQUALITY CONST_TARGET_BS ){		\
-      CERR( "二分探索成功：" , #ANSWER , ":=" , ANSWER );		\
-    } else {								\
-      CERR( "二分探索失敗：" , #ANSWER , ":=" , #MAXIMUM , "+ 1 =" , MAXIMUM + 1 ); \
-      CERR( "単調でないか、単調増加性と単調減少性を逆にしてしまったか、探索範囲内に解が存在しません。" ); \
-      ANSWER = MAXIMUM + 1;						\
+    ll EXPRESSION_BS;							\
+    const ll CONST_TARGET_BS = ( CONST_TARGET );			\
+    ll DIFFERENCE_BS;							\
+    while( L_BS < U_BS ){						\
+      DIFFERENCE_BS = ( EXPRESSION_BS = ( EXPRESSION ) ) - CONST_TARGET_BS; \
+      CERR( "二分探索中：" , "L_BS =" , L_BS , "<=" , #ANSWER , "=" , ANSWER , "<=" , U_BS , "= U_BS :" , #EXPRESSION , "=" , EXPRESSION_BS , DIFFERENCE_BS > 0 ? ">" : DIFFERENCE_BS < 0 ? "<" : "=" , CONST_TARGET_BS , "=" , #CONST_TARGET ); \
+      if( DIFFERENCE_BS INEQUALITY_FOR_CHECK 0 ){			\
+	U_BS = UPDATE_U;						\
+      } else {								\
+	L_BS = UPDATE_L;						\
+      }									\
+      ANSWER = UPDATE_ANSWER;						\
     }									\
-  }									\
+    if( L_BS > U_BS ){							\
+      CERR( "二分探索失敗：" , "L_BS =" , L_BS , ">" , U_BS , "= U_BS :" , #ANSWER , ":=" , #MAXIMUM , "+ 1 =" , MAXIMUM + 1  ); \
+      CERR( "二分探索マクロにミスがある可能性があります。変更前の版に戻してください。" ); \
+      ANSWER = MAXIMUM + 1;						\
+    } else {								\
+      CERR( "二分探索終了：" , "L_BS =" , L_BS , "<=" , #ANSWER , "=" , ANSWER , "<=" , U_BS , "= U_BS" ); \
+      CERR( "二分探索が成功したかを確認するために" , #EXPRESSION , "を計算します。" ); \
+      CERR( "成功判定が不要な場合はこの計算を削除しても構いません。" );	\
+      EXPRESSION_BS = ( EXPRESSION );					\
+      CERR( "二分探索結果：" , #EXPRESSION , "=" , EXPRESSION_BS , ( EXPRESSION_BS > CONST_TARGET_BS ? ">" : EXPRESSION_BS < CONST_TARGET_BS ? "<" : "=" ) , CONST_TARGET_BS ); \
+      if( EXPRESSION_BS DESIRED_INEQUALITY CONST_TARGET_BS ){		\
+	CERR( "二分探索成功：" , #ANSWER , ":=" , ANSWER );		\
+      } else {								\
+	CERR( "二分探索失敗：" , #ANSWER , ":=" , #MAXIMUM , "+ 1 =" , MAXIMUM + 1 ); \
+	CERR( "単調でないか、単調増加性と単調減少性を逆にしてしまったか、探索範囲内に解が存在しません。" ); \
+	ANSWER = MAXIMUM + 1;						\
+      }									\
+    }									\
   }									\
 
 // 単調増加の時にEXPRESSION >= CONST_TARGETの最小解を格納。
-#define BS1( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET )	\
-  BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , >= , CONST_TARGET , >= , ANSWER , ANSWER + 1 , ( L_BS + U_BS ) / 2 ) \
-
+#define BS1( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET ) BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , >= , CONST_TARGET , >= , ANSWER , ANSWER + 1 , ( L_BS + U_BS ) / 2 )
 // 単調増加の時にEXPRESSION <= CONST_TARGETの最大解を格納。
-#define BS2( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET )	\
-  BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , <= , CONST_TARGET , > , ANSWER - 1 , ANSWER , ( L_BS + 1 + U_BS ) / 2 ) \
-
+#define BS2( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET ) BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , <= , CONST_TARGET , > , ANSWER - 1 , ANSWER , ( L_BS + 1 + U_BS ) / 2 )
 // 単調減少の時にEXPRESSION >= CONST_TARGETの最大解を格納。
-#define BS3( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET )	\
-  BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , >= , CONST_TARGET , < , ANSWER - 1 , ANSWER , ( L_BS + 1 + U_BS ) / 2 ) \
-
+#define BS3( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET ) BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , >= , CONST_TARGET , < , ANSWER - 1 , ANSWER , ( L_BS + 1 + U_BS ) / 2 )
 // 単調減少の時にEXPRESSION <= CONST_TARGETの最小解を格納。
-#define BS4( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET )	\
-  BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , <= , CONST_TARGET , <= , ANSWER , ANSWER + 1 , ( L_BS + U_BS ) / 2 ) \
-
+#define BS4( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET ) BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , <= , CONST_TARGET , <= , ANSWER , ANSWER + 1 , ( L_BS + U_BS ) / 2 )
 // t以下の値が存在すればその最大値のiterator、存在しなければend()を返す。
 template <typename T> inline typename set<T>::iterator MaximumLeq( set<T>& S , const T& t ) { const auto end = S.end(); if( S.empty() ){ return end; } auto itr = S.upper_bound( t ); return itr == end ? S.find( *( S.rbegin() ) ) : itr == S.begin() ? end : --itr; }
 // t未満の値が存在すればその最大値のiterator、存在しなければend()を返す。
@@ -130,53 +122,67 @@ template <typename T> inline typename set<T>::iterator MinimumGeq( set<T>& S , c
 // tより大きい値が存在すればその最小値のiterator、存在しなければend()を返す。
 template <typename T> inline typename set<T>::iterator MinimumGt( set<T>& S , const T& t ) { return S.upper_bound( t ); }
 
+// 尺取り法用
+// VAR_TPAがINITからUPDATEを繰り返しCONTINUE_CONDITIONを満たす限り、ON_CONDITIONを判定して
+// trueならON、falseならOFFとなる。直近のONの区間を[VAR_TPA_L,VAR_TPA_R)で管理する。
+#define TPA( VAR_TPA , INIT , UPDATE , CONTINUE_CONDITION , ON_CONDITION , ONON , ONOFF , OFFON , OFFOFF , FINISH ) \
+  {									\
+    auto VAR_TPA = INIT;						\
+    auto VAR_TPA ## _L = VAR_TPA;					\
+    auto VAR_TPA ## _R = VAR_TPA;					\
+    bool on_TPA = false;						\
+    int state_TPA = 3;							\
+    while( CONTINUE_CONDITION ){					\
+      bool on_TPA_next = ON_CONDITION;					\
+      state_TPA = ( ( on_TPA ? 1 : 0 ) << 1 ) | ( on_TPA_next ? 1 : 0 ); \
+      CERR( "尺取り中： [L,R) = [" , VAR_TPA ## _L , "," , VAR_TPA ## _R , ") ," , #VAR_TPA , "=" , VAR_TPA , "," , ( ( state_TPA >> 1 ) & 1 ) == 1 ? "on" : "off" , " ->" , ( state_TPA & 1 ) == 1 ? "on" : "off" ); \
+      if( state_TPA == 0 ){						\
+	OFFOFF; VAR_TPA ## _L = VAR_TPA ## _R = VAR_TPA; UPDATE;	\
+      } else if( state_TPA == 1 ){					\
+	OFFON; VAR_TPA ## _L = VAR_TPA; UPDATE; VAR_TPA ## _R = VAR_TPA; \
+      } else if( state_TPA == 2 ){					\
+	ONOFF; VAR_TPA ## _L = VAR_TPA ## _R = VAR_TPA; UPDATE;		\
+      } else {								\
+	ONON; UPDATE; VAR_TPA ## _R = VAR_TPA;				\
+      }									\
+      on_TPA = on_TPA_next;						\
+    }									\
+    CERR( "尺取り終了： [L,R) = [" , VAR_TPA ## _L , "," , VAR_TPA ## _R , ") ," , #VAR_TPA , "=" , VAR_TPA ); \
+    FINISH;								\
+  }									\
+
 // データ構造用
-template <typename T , template <typename...> typename V> inline T operator+( const T& t0 , const T& t1 ) { if( t0.empty() ){ return t1; } if( t1.empty() ){ return t0; } assert( t0.size() == t1.size() ); V<T> answer{}; for( auto itr0 = t0.begin() , itr1 = t1.begin() , end0 = t0.end(); itr0 != end0 ; itr0++ , itr1++ ){ answer.push_back( *itr0 + *itr1 ); } return answer; }
-template <typename T> inline T operator+( const T2<T>& t0 , const T2<T>& t1 ) { return { t0.first + t1.first , t0.second + t1.second }; }
-template <typename T> inline T operator+( const T3<T>& t0 , const T3<T>& t1 ) { return { get<0>( t0 ) + get<0>( t1 ) , get<1>( t0 ) + get<1>( t1 ) , get<2>( t0 ) + get<2>( t1 ) }; }
-template <typename T> inline T operator+( const T4<T>& t0 , const T4<T>& t1 ) { return { get<0>( t0 ) + get<0>( t1 ) , get<1>( t0 ) + get<1>( t1 ) , get<2>( t0 ) + get<2>( t1 ) , get<3>( t0 ) + get<3>( t1 ) }; }
-template <typename T> inline T Add( const T& t0 , const T& t1 ) { return t0 + t1; }
-template <typename T> inline T XorAdd( const T& t0 , const T& t1 ){ return t0 ^ t1; }
-template <typename T> inline T Multiply( const T& t0 , const T& t1 ) { return t0 * t1; }
-template <typename T> inline const T& Zero() { static const T z = 0; return z; }
+template <typename T , template <typename...> typename V> inline auto operator+( const V<T>& a0 , const V<T>& a1 ) -> decldecay_t( ( declval<V<T>>().push_back( declval<T>() ) , a0 ) ) { if( a0.empty() ){ return a1; } if( a1.empty() ){ return a0; } assert( a0.size() == a1.size() ); V<T> answer{}; for( auto itr0 = a0.begin() , itr1 = a1.begin() , end0 = a0.end(); itr0 != end0 ; itr0++ , itr1++ ){ answer.push_back( *itr0 + *itr1 ); } return answer; }
+template <typename T , typename U> inline pair<T,U> operator+( const pair<T,U>& t0 , const pair<T,U>& t1 ) { return { t0.first + t1.first , t0.second + t1.second }; }
+template <typename T , typename U , typename V> inline tuple<T,U,V> operator+( const tuple<T,U,V>& t0 , const tuple<T,U,V>& t1 ) { return { get<0>( t0 ) + get<0>( t1 ) , get<1>( t0 ) + get<1>( t1 ) , get<2>( t0 ) + get<2>( t1 ) }; }
+template <typename T , typename U , typename V , typename W> inline tuple<T,U,V,W> operator+( const tuple<T,U,V,W>& t0 , const tuple<T,U,V,W>& t1 ) { return { get<0>( t0 ) + get<0>( t1 ) , get<1>( t0 ) + get<1>( t1 ) , get<2>( t0 ) + get<2>( t1 ) , get<3>( t0 ) + get<3>( t1 ) }; }
+template <typename T> inline T Addition( const T& t0 , const T& t1 ) { return t0 + t1; }
+template <typename T> inline T Xor( const T& t0 , const T& t1 ){ return t0 ^ t1; }
+template <typename T> inline T Multiplication( const T& t0 , const T& t1 ) { return t0 * t1; }
+template <typename T> inline const T& Zero() { static const T z{}; return z; }
 template <typename T> inline const T& One() { static const T o = 1; return o; }\
-template <typename T> inline T AddInv( const T& t ) { return -t; }
+template <typename T> inline T AdditionInv( const T& t ) { return -t; }
 template <typename T> inline T Id( const T& v ) { return v; }
 template <typename T> inline T Min( const T& a , const T& b ){ return a < b ? a : b; }
 template <typename T> inline T Max( const T& a , const T& b ){ return a < b ? b : a; }
 
+// グラフ用
+template <typename T , template <typename...> typename V> inline auto Get( const V<T>& a ) { return [&]( const int& i = 0 ){ return a[i]; }; }
+
 // グリッド問題用
 int H , W , H_minus , W_minus , HW;
-vector<vector<bool> > non_wall;
+vector<string> wall_str; vector<vector<bool> > non_wall;
+char walkable = '.' , unwalkable = '#';
 inline T2<int> EnumHW( const int& v ) { return { v / W , v % W }; }
-inline int EnumHW_inv( const int& h , const int& w ) { return h * W + w; }
+inline int EnumHW_inv( const T2<int>& ij ) { auto& [i,j] = ij; return i * W + j; }
 const string direction[4] = {"U","R","D","L"};
-// (i,j)->(k,h)の方向番号を取得
 inline int DirectionNumberOnGrid( const int& i , const int& j , const int& k , const int& h ){return i<k?2:i>k?0:j<h?1:j>h?3:(assert(false),-1);}
-// v->wの方向番号を取得
 inline int DirectionNumberOnGrid( const int& v , const int& w ){auto [i,j]=EnumHW(v);auto [k,h]=EnumHW(w);return DirectionNumberOnGrid(i,j,k,h);}
-// 方向番号の反転U<->D、R<->L
 inline int ReverseDirectionNumberOnGrid( const int& n ){assert(0<=n&&n<4);return(n+2)%4;}
-inline void SetEdgeOnGrid( const string& Si , const int& i , list<int> ( &e )[] , const char& walkable = '.' ){FOR(j,0,W){if(Si[j]==walkable){int v = EnumHW_inv(i,j);if(i>0){e[EnumHW_inv(i-1,j)].push_back(v);}if(i+1<H){e[EnumHW_inv(i+1,j)].push_back(v);}if(j>0){e[EnumHW_inv(i,j-1)].push_back(v);}if(j+1<W){e[EnumHW_inv(i,j+1)].push_back(v);}}}}
-inline void SetEdgeOnGrid( const string& Si , const int& i , list<path> ( &e )[] , const char& walkable = '.' ){FOR(j,0,W){if(Si[j]==walkable){const int v=EnumHW_inv(i,j);if(i>0){e[EnumHW_inv(i-1,j)].push_back({v,1});}if(i+1<H){e[EnumHW_inv(i+1,j)].push_back({v,1});}if(j>0){e[EnumHW_inv(i,j-1)].push_back({v,1});}if(j+1<W){e[EnumHW_inv(i,j+1)].push_back({v,1});}}}}
-inline void SetWallOnGrid( const string& Si , const int& i , vector<vector<bool> >& non_wall , const char& walkable = '.'  , const char& unwalkable = '#' ){non_wall.push_back(vector<bool>(W));auto& non_wall_i=non_wall[i];FOR(j,0,W){non_wall_i[j]=Si[j]==walkable?true:(assert(Si[j]==unwalkable),false);}}
-
-// グラフ用
-template <typename PATH> vector<list<PATH> > gE;
-template <typename T> map<T,T> gF;
-template <typename T> vector<T> gA;
-
-template <typename PATH> list<PATH> GetgE( const int& i )
-{
-  list<PATH> answer = gE<PATH>[i];
-  // VVV 入力によらない処理は以下に挿入する。
-
-  // AAA 入力によらない処理は以上に挿入する。
-  return answer;
-}
-
-template <typename T> inline T GetgF( const T& t ){ return gF<T>[t]; }
-template <typename T> inline T GetgA( const int& i ){ return gA<T>[i]; }
+inline vector<int> EdgeOnGrid( const int& v ){vector<int>answer{};auto[i,j]=EnumHW(v);if(i>0&&wall_str[i-1][j]==walkable){answer.push_back(EnumHW_inv({i-1,j}));}if(i+1<H&&wall_str[i+1][j]==walkable){answer.push_back(EnumHW_inv({i+1,j}));}if(j>0&&wall_str[i][j-1]==walkable){answer.push_back(EnumHW_inv({i,j-1}));}if(j+1<W&&wall_str[i][j+1]==walkable){answer.push_back(EnumHW_inv({i,j+1}));}return answer;}
+inline vector<path> WeightedEdgeOnGrid( const int& v ){vector<path>answer{};auto[i,j]=EnumHW(v);if(i>0&&wall_str[i-1][j]==walkable){answer.push_back({EnumHW_inv({i-1,j}),1});}if(i+1<H&&wall_str[i+1][j]==walkable){answer.push_back({EnumHW_inv({i+1,j}),1});}if(j>0&&wall_str[i][j-1]==walkable){answer.push_back({EnumHW_inv({i,j-1}),1});}if(j+1<W&&wall_str[i][j+1]==walkable){answer.push_back({EnumHW_inv({i,j+1}),1});}return answer;}
+inline void SetWallStringOnGrid( const int& i , vector<string>& S ){if(S.empty()){S.resize(H);}cin>>S[i];assert(int(S[i].size())==W);}
+inline void SetWallOnGrid( const int& i , vector<vector<bool>>& b ){if(b.empty()){b.resize(H,vector<bool>(W));}auto&S_i=wall_str[i];auto&b_i=b[i];FOR(j,0,W){b_i[j]=S_i[j]==walkable?false:(assert(S_i[j]==unwalkable),true);}}
 
 // デバッグ用
 #ifdef DEBUG
@@ -187,6 +193,7 @@ template <typename T> inline T GetgA( const int& i ){ return gA<T>[i]; }
   inline void SmallTest();
   inline void RandomTest();
   ll GetRand( const ll& Rand_min , const ll& Rand_max );
+  inline void BreakPoint( const int& LINE ) {}
   int exec_mode;
   CEXPR( int , solve_mode , 0 );
   CEXPR( int , sample_debug_mode , 1 );
--- Contest/Problem/a_Macro.hpp
+++ Contest/Problem/a_Macro.hpp
@@ -1,21 +1,23 @@
 // c:/Users/user/Documents/Programming/Contest/Problem/a_Macro.hpp
 
 // 想定解用マクロのうち
-// ifdef DEBUG, ifdef REACTIVE, ifndef USE_GETLINE
+// ifdef REACTIVE, ifndef USE_GETLINE
+// ifdef DEBUGのうち_GLIBCXX_DEBUGからDEXPRまでと
+// ifndef DEBUGのうちASSERTからCOUR_ITRまで
 // を残し
 // REPEAT_MAIN
 // を変更する。
 #pragma once
 #define _GLIBCXX_DEBUG
 #define SIGNAL signal( SIGABRT , &AlertAbort );
-#define DEXPR( LL , BOUND , VALUE , DEBUG_VALUE ) CEXPR( LL , BOUND , DEBUG_VALUE )
-#define ASSERT( A , MIN , MAX ) CERR( "ASSERTチェック： " , ( MIN ) , ( ( MIN ) <= A ? "<=" : ">" ) , A , ( A <= ( MAX ) ? "<=" : ">" ) , ( MAX ) ); assert( ( MIN ) <= A && A <= ( MAX ) )
-#define CERR( ... ) VariadicCout( cerr , __VA_ARGS__ ) << endl
-#define COUT( ... ) VariadicCout( cout << "出力： " , __VA_ARGS__ ) << endl
-#define CERR_A( A , N ) OUTPUT_ARRAY( cerr , A , N ) << endl
-#define COUT_A( A , N ) cout << "出力： "; OUTPUT_ARRAY( cout , A , N ) << endl
-#define CERR_ITR( A ) OUTPUT_ITR( cerr , A ) << endl
-#define COUT_ITR( A ) cout << "出力： "; OUTPUT_ITR( cout , A ) << endl
+#define DEXPR( LL , BOUND , VALUE1 , VALUE2 ) CEXPR( LL , BOUND , VALUE2 )
+#define ASSERT( A , MIN , MAX ) assert( ( MIN ) <= A && A <= ( MAX ) )
+#define CERR( ... ) 
+#define COUT( ... ) VariadicCout( cout , __VA_ARGS__ ) << ENDL
+#define CERR_A( A , N ) 
+#define COUT_A( A , N ) OUTPUT_ARRAY( cout , A , N ) << ENDL
+#define CERR_ITR( A ) 
+#define COUT_ITR( A ) OUTPUT_ITR( cout , A ) << ENDL
 #define ENDL endl
 #define SET_LL( A ) cin >> A
 #define CIN( LL , ... ) LL __VA_ARGS__; VariadicCin( cin , __VA_ARGS__ )
@@ -23,24 +25,16 @@
 #define CIN_A( LL , A , N ) vector<LL> A( N ); SET_A( A , N );
 #include <bits/stdc++.h>
 using namespace std;
-using uint = unsigned int;
-using ll = long long;
-using ull = unsigned long long;
-using ld = long double;
-using lld = __float128;
-using lld = __float128;
-template <typename INT> using T2 = pair<INT,INT>;
-template <typename INT> using T3 = tuple<INT,INT,INT>;
-template <typename INT> using T4 = tuple<INT,INT,INT,INT>;
-using path = pair<int,ll>;
-#define REPEAT_MAIN( BOUND ) void ExecuteSolution_Body(){ signal( SIGABRT , &AlertAbort ); CEXPR( int , bound_test_case_num , BOUND ); int test_case_num = 1; if constexpr( bound_test_case_num > 1 ){ SET_ASSERT( test_case_num , 1 , bound_test_case_num ); } REPEAT( test_case_num ){ if constexpr( bound_test_case_num > 1 ){ CERR( "testcase " , VARIABLE_FOR_REPEAT_test_case_num , ":" ); } Solve(); } }
+#define REPEAT_MAIN( BOUND ) void ExecuteSolution_Body(){ signal( SIGABRT , &AlertAbort ); CEXPR( int , bound_test_case_num , BOUND ); int test_case_num = 1; if constexpr( bound_test_case_num > 1 ){ CERR( "テストケースの個数を入力してください。" ); SET_ASSERT( test_case_num , 1 , bound_test_case_num ); } REPEAT( test_case_num ){ if constexpr( bound_test_case_num > 1 ){ CERR( "testcase " , VARIABLE_FOR_REPEAT_test_case_num , ":" ); } Solve(); } }
 #define START_WATCH chrono::system_clock::time_point watch = chrono::system_clock::now()
 #define CURRENT_TIME static_cast<double>( chrono::duration_cast<chrono::microseconds>( chrono::system_clock::now() - watch ).count() / 1000.0 )
 #define CHECK_WATCH( TL_MS ) ( CURRENT_TIME < TL_MS - 100.0 )
 #define TYPE_OF( VAR ) decay_t<decltype( VAR )>
 #define CEXPR( LL , BOUND , VALUE ) constexpr LL BOUND = VALUE
 #define SET_ASSERT( A , MIN , MAX ) SET_LL( A ); ASSERT( A , MIN , MAX )
-#define CIN_ASSERT( A , MIN , MAX ) TYPE_OF( MAX ) A; SET_ASSERT( A , MIN , MAX )
+#define SET_A_ASSERT( A , N , MIN , MAX ) FOR( VARIABLE_FOR_SET_A , 0 , N ){ SET_ASSERT( A[VARIABLE_FOR_SET_A] , MIN , MAX ); }
+#define CIN_ASSERT( A , MIN , MAX ) decldecay_t( MAX ) A; SET_ASSERT( A , MIN , MAX )
+#define CIN_A_ASSERT( A , N , MIN , MAX ) vector<decldecay_t( MAX )> A( N ); SET_A_ASSERT( A , N , MIN , MAX )
 #define FOR( VAR , INITIAL , FINAL_PLUS_ONE ) for( TYPE_OF( FINAL_PLUS_ONE ) VAR = INITIAL ; VAR < FINAL_PLUS_ONE ; VAR ++ )
 #define FOREQ( VAR , INITIAL , FINAL ) for( TYPE_OF( FINAL ) VAR = INITIAL ; VAR <= FINAL ; VAR ++ )
 #define FOREQINV( VAR , INITIAL , FINAL ) for( TYPE_OF( INITIAL ) VAR = INITIAL ; VAR + 1 > FINAL ; VAR -- )
@@ -52,11 +46,26 @@ using path = pair<int,ll>;
 #define OUTPUT_ITR( OS , A ) { auto ITERATOR_FOR_OUTPUT_ITR = A.begin() , END_FOR_OUTPUT_ITR = A.end(); bool VARIABLE_FOR_OUTPUT_ITR = ITERATOR_FOR_COUT_ITR != END_FOR_COUT_ITR; while( VARIABLE_FOR_OUTPUT_ITR ){ OS << *ITERATOR_FOR_COUT_ITR; ( VARIABLE_FOR_OUTPUT_ITR = ++ITERATOR_FOR_COUT_ITR != END_FOR_COUT_ITR ) ? OS : OS << " "; } } OS
 #define RETURN( ... ) COUT( __VA_ARGS__ ); return
 
+// 型のエイリアス
+#define decldecay_t( VAR ) decay_t<decltype( VAR )>
+template <typename F , typename...Args> using ret_t = decltype( declval<F>()( declval<Args>()... ) );
+template <typename T> using inner_t = typename T::type;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using lld = __float128;
+template <typename INT> using T2 = pair<INT,INT>;
+template <typename INT> using T3 = tuple<INT,INT,INT>;
+template <typename INT> using T4 = tuple<INT,INT,INT,INT>;
+using path = pair<int,ll>;
+
 // 圧縮用
 #define TE template
 #define TY typename
 #define US using
 #define ST static
+#define AS assert
 #define IN inline
 #define CL class
 #define PU public
@@ -72,11 +81,19 @@ using path = pair<int,ll>;
 #define BE begin
 #define EN end
 #define SZ size
+#define LE length
+#define PW Power
 #define MO move
 #define TH this
 #define CRI CO int&
 #define CRUI CO uint&
 #define CRL CO ll&
+#define VI virtual 
+#define ST_AS static_assert
+#define reMO_CO remove_const
+#define is_COructible_v is_constructible_v
+#define rBE rbegin
+#define reSZ resize
 
 #define OFS( ... ) VariadicCout( ofs_input_list , __VA_ARGS__ ) << endl
 #define OFS_A( A , N ) OUTPUT_ARRAY( ofs_input_list , A , N ) << endl
