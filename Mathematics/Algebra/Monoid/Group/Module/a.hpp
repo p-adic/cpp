2a13d66974f01275a52949069ccd5e676aa04570
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-03-18 10:53:27 +0900

    Add files via upload

--- Mathematics/Algebra/Monoid/Group/Module/a.hpp
+++ Mathematics/Algebra/Monoid/Group/Module/a.hpp
@@ -4,10 +4,12 @@
 #include "../a.hpp"
 
 // verify:
+// https://yukicoder.me/submissions/961537（AbstractRSet）
 // https://yukicoder.me/submissions/950182（Module）
 
 template <typename R , typename U>
-class VirtualModule
+class VirtualModule :
+  virtual public UnderlyingSet<U>
 {
 
 public:
@@ -17,19 +19,30 @@ public:
 
 };
 
-template <typename R , typename U , typename O_U , typename GROUP>
-class AbstractModule :
-  virtual public VirtualModule<R,U> ,
-  public GROUP
+template <typename R , typename U , typename O_U>
+class AbstractRSet :
+  virtual public VirtualModule<R,U>
 {
 
 private:
   O_U m_o_U;
   
 public:
+  // 型推論のために余計な引数dummy0,dummy1が必要
+  inline AbstractRSet( const R& dummy0 , const U& dummy1 , O_U o_U );
+  inline U Action( const R& r , const U& u );
+
+};
+
+template <typename R , typename U , typename O_U , typename GROUP>
+class AbstractModule :
+  public AbstractRSet<R,U,O_U> ,
+  public GROUP
+{
+
+public:
   // 型推論のために余計な引数dummyが必要
   inline AbstractModule( const R& dummy , O_U o_U , GROUP M );
-  inline U Action( const R& r , const U& u );
 
 };
 template <typename R , typename O_U , typename GROUP> AbstractModule( const R& dummy , O_U o_U , GROUP M ) -> AbstractModule<R,inner_t<GROUP>,O_U,GROUP>;
--- Mathematics/Algebra/Monoid/Group/Module/a_Body.hpp
+++ Mathematics/Algebra/Monoid/Group/Module/a_Body.hpp
@@ -5,9 +5,10 @@
 
 #include "../a_Body.hpp"
 
-template <typename R , typename U , typename O_U , typename GROUP> inline AbstractModule<R,U,O_U,GROUP>::AbstractModule( const R& dummy , O_U o_U , GROUP M ) : GROUP( move( M ) ) , m_o_U( move( o_U ) ) { static_assert( is_same_v<U,inner_t<GROUP>> && is_invocable_r_v<U,O_U,R,U> ); }
+template <typename R , typename U , typename O_U> inline AbstractRSet<R,U,O_U>::AbstractRSet( const R& dummy0 , const U& dummy1 , O_U o_U ) : m_o_U( move( o_U ) ) { static_assert( is_invocable_r_v<U,O_U,R,U> ); }
+template <typename R , typename U , typename O_U , typename GROUP> inline AbstractModule<R,U,O_U,GROUP>::AbstractModule( const R& dummy , O_U o_U , GROUP M ) : AbstractRSet<R,U,O_U>( dummy , M.One() , move( o_U ) ) , GROUP( move( M ) ) { static_assert( is_same_v<U,inner_t<GROUP>> ); }
 
-template <typename R , typename U , typename O_U , typename GROUP> inline U AbstractModule<R,U,O_U,GROUP>::Action( const R& r , const U& u ) { return m_o_U( r , u ); }
+template <typename R , typename U , typename O_U> inline U AbstractRSet<R,U,O_U>::Action( const R& r , const U& u ) { return m_o_U( r , u ); }
 template <typename R , typename U> inline U Module<R,U>::Action( const R& r , const U& u ) { return r * u; }
 
 template <typename R , typename U> inline U VirtualModule<R,U>::Power( const U& u , const R& r ) { return Action( r , u ); }
