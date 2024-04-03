6d00aa80829adc073050c2659f3f0efd5f32958e
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-04-03 18:03:21 +0900

    Delete Mathematics/Combinatorial/KnapsackProblem/UnboundedChoice/UnboundedValueSum/NegativeValue directory

--- Mathematics/Combinatorial/KnapsackProblem/UnboundedChoice/UnboundedValueSum/NegativeValue/Unstable/a_Macro.hpp
+++
@@ -1,8 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Combinatorial/KnapsackProblem/UnboundedChoice/UnboundedValueSum/NegativeValue/Unstable/a_Macro.hpp
-
-#pragma once
-
-#ifndef RET_TYPE
-  #define RET_TYPE
-  template <typename F , typename...Args> using ret_t = decltype( declval<F>()( declval<Args>()... ) );
-#endif
