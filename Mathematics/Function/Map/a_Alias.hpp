39208bacf6715ec3f67371768894743fe48a3c23
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-04-05 20:55:19 +0900

    Delete Mathematics/Function/Map directory

--- Mathematics/Function/Map/a_Alias.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Function/Map/a_Alias.hpp
-
-#pragma once
-
-template <typename T , typename U>
-using Map = conditional_t<is_constructible_v<unordered_map<T,int>>,unordered_map<T,U>,conditional_t<is_ordered::value<T>,map<T,U>,void>>;
--- Mathematics/Function/Map/a_Body.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Function/Map/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-template <typename INT , template <typename...> typename T> inline size_t hash<T<INT>>::operator()( const T<INT>& n ) const { return hash<INT>::operator()( n.Represent() ); }
