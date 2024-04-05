dd8ea107cbbae605d0fd0b42a15bbbeb951f5013
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-04-05 12:34:20 +0900

    Add files via upload

--- Mathematics/Geometry/Graph/BellmanFord/a_Macro.hpp
+++ Mathematics/Geometry/Graph/BellmanFord/a_Macro.hpp
@@ -2,77 +2,127 @@
 
 #pragma once
 
-#define BELLMAN_FORD_BODY( INITIALISE_PREV , SET_PREV )			\
-  const U& one = m_M.One();						\
-  const U& infty = this->Infty();					\
-  assert( one < infty );						\
-  const int& size = m_G.size();						\
-  auto&& i_start = m_G.Enumeration_inv( t_start );			\
-  assert( 0 <= i_start && i_start < size );				\
-  vector<bool> found( size );						\
-  vector<U> weight( size , infty );					\
-  found[i_start] = true;						\
-  weight[i_start] = 0;							\
-  INITIALISE_PREV;							\
-									\
-  for( int length = 0 ; length < size ; length++ ){			\
-  									\
-    for( int i = 0 ; i < size ; i++ ){					\
-									\
-      if( found[i] ){							\
-									\
-	const U& weight_i = weight[i];					\
-	assert( weight_i != infty );					\
-	auto&& edge_i = m_G.Edge( m_G.Enumeration( i ) );		\
-									\
-	for( auto itr = edge_i.begin() , end = edge_i.end() ; itr != end ; itr++ ){ \
-									\
-	  auto&& j = m_G.Enumeration_inv( itr->first );			\
-	  const U& edge_ij = itr->second;				\
-	  U temp = m_M.Product( weight_i , edge_ij );			\
-	  U& weight_j = weight[j];					\
-									\
-	  if( weight_j > temp ){					\
-									\
-	    found[j] = true;						\
-	    weight_j = move( temp );					\
-	    SET_PREV;							\
-									\
-	  }								\
-									\
-	}								\
-									\
-      }									\
-									\
-    }									\
-									\
-  }									\
-									\
-  bool valid = true;							\
-									\
-  for( int i = 0 ; i < size && valid ; i++ ){				\
-									\
-    if( found[i] ){							\
-									\
-      const U& weight_i = weight[i];					\
-      auto&& edge_i = m_G.Edge( m_G.Enumeration( i ) );			\
-									\
-      for( auto itr = edge_i.begin() , end = edge_i.end() ; itr != end ; itr++ ){ \
-									\
-	auto&& j = m_G.Enumeration_inv( itr->first );			\
-	const U& edge_ij = itr->second;					\
-	U& weight_j = weight[j];					\
-	const U temp = m_M.Product( weight_i , edge_ij );		\
-									\
-	if( weight_j > temp ){						\
-									\
-	  valid = false;						\
-	  break;							\
-									\
-	}								\
-									\
-      }									\
-									\
-    }									\
-									\
-  }									\
+#define BELLMAN_FORD_BODY( INITIALISE_PREV , SET_PREV )		\
+  const U& one = m_M.One();					\
+  const U& infty = this->Infty();				\
+  assert( one < infty );					\
+  const int& size = m_G.size();					\
+  auto&& i_start = m_G.Enumeration_inv( t_start );		\
+  assert( 0 <= i_start && i_start < size );			\
+  vector<U> weight( size , infty ) , weight_copy;		\
+  weight[i_start] = one;					\
+  vector<bool> found( size );					\
+  found[i_start] = true;					\
+  INITIALISE_PREV;						\
+								\
+  if( path_length == -1 ){					\
+								\
+    path_length = size - 1;					\
+								\
+  } else {							\
+								\
+    assert( many_edges );					\
+								\
+  }								\
+								\
+  for( int num = 0 ; num < path_length ; num++ ){		\
+								\
+    for( int i = 0 ; i < size ; i++ ){				\
+								\
+      const U& weight_i = weight[i];				\
+								\
+      if( found[i] ){						\
+								\
+	auto&& edge_i = m_G.Edge( m_G.Enumeration( i ) );	\
+								\
+	for( auto& edge_ij : edge_i ){				\
+								\
+	  auto&& j = m_G.Enumeration_inv( edge_ij.first );	\
+	  const U& weight_ij = edge_ij.second;			\
+	  U temp = m_M.Product( weight_i , weight_ij );		\
+	  U& weight_j = weight[j];				\
+								\
+	  if( weight_j > temp ){				\
+								\
+	    weight_j = move( temp );				\
+	    found[j] = true;					\
+	    SET_PREV;						\
+								\
+	  }							\
+								\
+	}							\
+								\
+      }								\
+								\
+    }								\
+								\
+  }								\
+								\
+  vector<bool> invalid( size );					\
+								\
+  for( int i = 0 ; i < size ; i++ ){				\
+								\
+    if( found[i] ){						\
+								\
+      const U& weight_i = weight[i];				\
+      auto&& edge_i = m_G.Edge( m_G.Enumeration( i ) );		\
+								\
+      for( auto& edge_ij : edge_i ){				\
+								\
+	auto&& j = m_G.Enumeration_inv( edge_ij.first );	\
+	const U& weight_ij = edge_ij.second;			\
+	U& weight_j = weight[j];				\
+	const U temp = m_M.Product( weight_i , weight_ij );	\
+								\
+	if( weight_j > temp ){					\
+								\
+	  invalid[i] = true;					\
+								\
+	}							\
+								\
+      }								\
+								\
+    }								\
+								\
+  }								\
+								\
+  vector<int> dfs{};						\
+								\
+  for( int i = 0 ; i < size ; i++ ){				\
+								\
+    if( invalid[i] ){						\
+								\
+      dfs.push_back( i );					\
+								\
+    }								\
+								\
+  }								\
+								\
+  while( !dfs.empty() ){					\
+								\
+    const int i = dfs.back();					\
+    dfs.pop_back();						\
+    auto&& edge_i = m_G.Edge( m_G.Enumeration( i ) );		\
+								\
+    for( auto& edge_ij : edge_i ){				\
+								\
+      auto&& j = m_G.Enumeration_inv( edge_ij.first );		\
+								\
+      if( !invalid[j] ){					\
+								\
+	invalid[j] = true;					\
+	dfs.push_back( j );					\
+								\
+      }								\
+								\
+    }								\
+								\
+  }								\
+								\
+  for( int i = 0 ; i < size ; i++ ){				\
+								\
+    found[i] = found[i] && !invalid[i];				\
+								\
+  }								\
+
+
