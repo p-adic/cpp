cd4724874118aa19dd3753a7c8a63f4245ecb79c
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-04-29 22:41:34 +0900

    Add files via upload

--- Utility/StdStream/a.hpp
+++ Utility/StdStream/a.hpp
@@ -4,9 +4,7 @@
 #include "a_Macro.hpp"
 
 #include <bits/stdc++.h>
-#include "../String/a.hpp"
 
-template <class Traits , typename Arg1 , typename Arg2> inline basic_istream<char,Traits>& operator>>( basic_istream<char,Traits>& is , pair<Arg1,Arg2>& arg );
 template <class Traits> inline basic_istream<char,Traits>& VariadicCin( basic_istream<char,Traits>& is );
 template <class Traits , typename Arg , typename... ARGS> inline basic_istream<char,Traits>& VariadicCin( basic_istream<char,Traits>& is , Arg& arg , ARGS&... args );
 template <class Traits> inline basic_istream<char,Traits>& VariadicSet( basic_istream<char,Traits>& is , const int& i );
@@ -15,15 +13,5 @@ template <class Traits , typename Arg , typename... ARGS> inline basic_istream<c
 template <class Traits> inline basic_istream<char,Traits>& VariadicGetline( basic_istream<char,Traits>& is , const char& separator );
 template <class Traits , typename Arg , typename... ARGS> inline basic_istream<char,Traits>& VariadicGetline( basic_istream<char,Traits>& is , const char& separator , Arg& arg , ARGS&... args );
 
-// SFINAEÇæÇ∆stringÇ∆ã£çáÇ∑ÇÈÅB
-DECLARATION_OF_COUT_FOR_VECTOR( vector );
-DECLARATION_OF_COUT_FOR_VECTOR( list );
-DECLARATION_OF_COUT_FOR_VECTOR( set );
-DECLARATION_OF_COUT_FOR_VECTOR( unordered_set );
-template <class Traits , typename Arg1 , typename Arg2> inline basic_ostream<char,Traits>& operator<<( basic_ostream<char,Traits>& os , const pair<Arg1,Arg2>& arg );
-
 template <class Traits , typename Arg> inline basic_ostream<char,Traits>& VariadicCout( basic_ostream<char,Traits>& os , const Arg& arg );
 template <class Traits , typename Arg1 , typename Arg2 , typename... ARGS> inline basic_ostream<char,Traits>& VariadicCout( basic_ostream<char,Traits>& os , const Arg1& arg1 , const Arg2& arg2 , const ARGS&... args );
-
-
-template <class Traits> inline basic_ostream<char,Traits>& operator<<( basic_ostream<char,Traits>& os , const type_info& type ) noexcept;
--- Utility/StdStream/a_Macro.hpp
+++ Utility/StdStream/a_Macro.hpp
@@ -20,5 +20,3 @@
   #endif
 #endif
 
-#define DECLARATION_OF_COUT_FOR_VECTOR( V ) template <class Traits , typename Arg> inline basic_ostream<char,Traits>& operator<<( basic_ostream<char,Traits>& os , const V<Arg>& arg )
-#define DEFINITION_OF_COUT_FOR_VECTOR( V ) template <class Traits , typename Arg> inline basic_ostream<char,Traits>& operator<<( basic_ostream<char,Traits>& os , const V<Arg>& arg ) { auto begin = arg.begin() , end = arg.end(); auto itr = begin; while( itr != end ){ ( itr == begin ? os : os << " " ) << *itr; itr++; } return os; }
