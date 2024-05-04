a4ac558561a083c6d7b517b1bfa06c86eba563d3
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-05-04 22:55:45 +0900

    Delete Utility/Sort directory

--- Utility/Sort/a.hpp
+++
@@ -1,5 +0,0 @@
-// c:/Users/user/Documents/Programming/Utility/Sort/a.hpp
-
-#pragma once
-
-template <typename T> void sort( vector<T>& a , const bool& reversed = false );
--- Utility/Sort/a_Body.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Utility/Sort/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-template <typename T> void sort( vector<T>& a , const bool& reversed ) { if( reversed ){ static auto comp = []( const T& t0 , const T& t1 ) { return t1 < t0; }; sort( a.begin() , a.end() , comp ); } else { sort( a.begin() , a.end() ); } }
