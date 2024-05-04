2a845431397d58577f8dec49eb64c85e5e40f0fa
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-05-04 22:57:45 +0900

    Add files via upload

--- Utility/Tuple/a.hpp
+++ Utility/Tuple/a.hpp
@@ -19,3 +19,10 @@ DECLARATION_OF_OPERATOR_FOR_TUPLE( % );
 
 DECLARATION_OF_INCREMENT_FOR_TUPLE( ++ );
 DECLARATION_OF_INCREMENT_FOR_TUPLE( -- );
+
+#include "../Set/a.hpp"
+template <typename T , typename U> DECLARATION_OF_HASH( pair<T,U> );
+template <typename T , typename U> DECLARATION_OF_HASH( tuple<T,U> );
+template <typename T , typename U , typename V> DECLARATION_OF_HASH( tuple<T,U,V> );
+template <typename T , typename U , typename V , typename W> DECLARATION_OF_HASH( tuple<T,U,V,W> );
+
--- Utility/Tuple/a_Body.hpp
+++ Utility/Tuple/a_Body.hpp
@@ -3,6 +3,8 @@
 #pragma once
 #include "a.hpp"
 
+#include "../Random/a_Body.hpp"
+
 template <class Traits , typename T , typename U , template <typename...> typename V> inline auto operator>>( basic_istream<char,Traits>& is , V<T,U>& arg ) -> decltype((get<0>(arg),is))& { return is >> get<0>( arg ) >> get<1>( arg ); }
 template <class Traits , typename T , typename U , typename V> inline basic_istream<char,Traits>& operator>>( basic_istream<char,Traits>& is , tuple<T,U,V>& arg ) { return is >> get<0>( arg ) >> get<1>( arg ) >> get<2>( arg ); }
 template <class Traits , typename T , typename U , typename V , typename W> inline basic_istream<char,Traits>& operator>>( basic_istream<char,Traits>& is , tuple<T,U,V,W>& arg ) { return is >> get<0>( arg ) >> get<1>( arg ) >> get<2>( arg ) >> get<3>( arg ); }
@@ -19,3 +21,9 @@ DEFINITION_OF_OPERATOR_FOR_TUPLE( % );
 
 DEFINITION_OF_INCREMENT_FOR_TUPLE( ++ );
 DEFINITION_OF_INCREMENT_FOR_TUPLE( -- );
+
+#include "../Set/a_Body.hpp"
+DEFINITION_OF_HASH_FOR_TUPLE( pair );
+DEFINITION_OF_HASH_FOR_TUPLE( tuple );
+template <typename T , typename U , typename V> inline size_t hash<tuple<T,U,V>>::operator()( const tuple<T,U,V>& n ) const { static const size_t seed = ( GetRand( 1e3 , 1e8 ) << 1 ) | 1; static const hash<pair<T,U>> h01; static const hash<V> h2; return ( h01( { get<0>( n ) , get<1>( n ) } ) * seed ) ^ h2( get<2>( n ) ); }
+template <typename T , typename U , typename V , typename W> inline size_t hash<tuple<T,U,V,W>>::operator()( const tuple<T,U,V,W>& n ) const { static const size_t seed = ( GetRand( 1e3 , 1e8 ) << 1 ) | 1; static const hash<pair<T,U>> h01; static const hash<pair<V,W>> h23; return ( h01( { get<0>( n ) , get<1>( n ) } ) * seed ) ^ h23( { get<2>( n ) , get<3>( n ) } ); }
