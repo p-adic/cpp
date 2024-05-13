d052ff45fd6b058d606a072f7fa9395ef296d1cb
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-05-13 09:19:14 +0900

    Add files via upload

--- Mathematics/Arithmetic/Mod/Hash/a_Macro.hpp
+++ Mathematics/Arithmetic/Mod/Hash/a_Macro.hpp
@@ -3,6 +3,10 @@
 #pragma once
 #include "../../../../Utility/Set/a_Macro.hpp"
 
+#ifndef decldecay_t
+  #define decldecay_t( VAR ) decay_t<decltype( VAR )>
+#endif
+
 #define DEFINITION_OF_HASH_FOR_MOD( MOD )				\
-  inline size_t hash<MOD>::operator()( const MOD& n ) const { static const hash<decltype( n.Represent() )> h; return h( n.Represent() ); } \
+  inline size_t hash<MOD>::operator()( const MOD& n ) const { static const hash<decldecay_t( n.Represent() )> h; return h( n.Represent() ); } \
 
