33d20870964ef0b7ec5328c462480b794e8f1348
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-05-16 08:09:39 +0900

    Add files via upload

---
+++ Utility/Reverse/a.hpp
@@ -0,0 +1,44 @@
+// c:/Users/user/Documents/Programming/Utility/Reverse/a.hpp
+
+#pragma once
+#include "a_Macro.hpp"
+
+// 順序を反転させる。
+// Reversed<int>では+1が後続でなくなるので通常の二分探索ができなくなることに注意。
+template <typename T>
+class Reversed
+{
+
+private:
+  // T = intの場合も扱うため、継承にできない。
+  T m_t;
+
+public:
+  inline Reversed( const Reversed<T>& t );
+  inline Reversed( Reversed<T>& t );
+  inline Reversed( Reversed<T>&& t );
+  template <typename...Args> inline Reversed( Args&&... args );
+
+  inline Reversed<T>& operator=( Reversed<T> t );
+  
+  DECLARATION_OF_REVERSED_RELATION( == );
+  DECLARATION_OF_REVERSED_RELATION( != );
+  DECLARATION_OF_REVERSED_RELATION( < );
+  DECLARATION_OF_REVERSED_RELATION( > );
+  DECLARATION_OF_REVERSED_RELATION( <= );
+  DECLARATION_OF_REVERSED_RELATION( >= );
+
+  DECLARATION_OF_REVERSED_ARITHMETIC( + , Reversed<T> );
+  inline Reversed<T> operator-() const;
+  DECLARATION_OF_REVERSED_ARITHMETIC( - , Reversed<T> );
+  DECLARATION_OF_REVERSED_ARITHMETIC( * , Reversed<T> );
+  DECLARATION_OF_REVERSED_ARITHMETIC( / , Reversed<T> );
+  DECLARATION_OF_REVERSED_ARITHMETIC( % , Reversed<T> );
+  DECLARATION_OF_REVERSED_ARITHMETIC( << , int );
+  DECLARATION_OF_REVERSED_ARITHMETIC( >> , int );
+  
+  inline T& Ref() noexcept;
+  inline const T& Get() const noexcept;
+  inline T&& move() noexcept;
+  
+};
