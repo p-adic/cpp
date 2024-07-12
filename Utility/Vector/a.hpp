096b4f002b47cb65489057a998088fd34bb38513
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-07-13 08:47:48 +0900

    Add files via upload

--- Utility/Vector/a.hpp
+++ Utility/Vector/a.hpp
@@ -12,8 +12,14 @@ DECLARATION_OF_COUT_FOR_VECTOR( set );
 DECLARATION_OF_COUT_FOR_VECTOR( unordered_set );
 
 DECLARATION_OF_ARITHMETICS_FOR_VECTOR( vector );
+template <typename T> inline operator*( const T& scalar , vector<T> v );
 
 inline void VariadicResize( const int& size );
 template <typename Arg , typename... ARGS> inline void VariadicResize( const int& size , Arg& arg , ARGS&... args );
 
-template <typename T> void sort( vector<T>& a , const bool& reversed = false );
+// template <typename V> inline auto Get( V& a );
+
+template <typename T = int> inline vector<T> id( const int& size );
+
+template <typename T> inline void Sort( vector<T>& a , const bool& reversed = false );
+template <typename T> inline vector<int> IndexSort( const vector<T>& a , const bool& reversed = false );
--- Utility/Vector/a_Body.hpp
+++ Utility/Vector/a_Body.hpp
@@ -11,8 +11,16 @@ DEFINITION_OF_COUT_FOR_VECTOR( unordered_set );
 DEFINITION_OF_ARITHMETICS_FOR_VECTOR( vector );
 DEFINITION_OF_ARITHMETICS_FOR_VECTOR( list );
 
+template <typename T> inline operator*( const T& scalar , vector<T> v ) { for( auto& t : v ){ v *= t; } return move( v ); }
+
 inline void VariadicResize( const int& size ) {}
 template <typename Arg , typename... ARGS> inline void VariadicResize( const int& size , Arg& arg , ARGS&... args ) { arg.resize( size ); VariadicResize( size , args... ); }
 
-template <typename T> void sort( vector<T>& a , const bool& reversed ) { if( reversed ){ static auto comp = []( const T& t0 , const T& t1 ) { return t1 < t0; }; sort( a.begin() , a.end() , comp ); } else { sort( a.begin() , a.end() ); } }
+template <typename V> inline auto Get( V& a ) { return [&]( const int& i = 0 ) -> const decldecay_t( a[0] )& { return a[i]; }; }
+template <typename T> inline vector<T> id( const int& size ) { vector<T> answer( size ); FOR( i , 0 , size ){ answer[i] = i; } return answer; }
 
+template <typename T> inline void Sort( vector<T>& a , const bool& reversed ) { if( reversed ){ static auto comp = []( const T& t0 , const T& t1 ) { return t1 < t0; }; sort( a.begin() , a.end() , comp ); } else { sort( a.begin() , a.end() ); } }
+
+template <typename T> inline vector<int> IndexSort( const vector<T>& a , const bool& reversed ) { auto index = id<int>( a.size() ); if( reversed ){ sort( index.begin() , index.end() , [&]( const int& i , const int& j ) { return a[j] < a[i]; } ); } else { sort( index.begin() , index.end() , [&]( const int& i , const int& j ) { return a[i] < a[j]; } ); } return index; }
+
+    
