97f2977ef950284108c1a45d43b9e4c8d6d7c607
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-07-20 17:53:36 +0900

    Delete Mathematics/Arithmetic/Prime/Divisor directory

--- Mathematics/Arithmetic/Prime/Divisor/GCD/a.hpp
+++
@@ -1,9 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/Prime/Divisor/GCD/a.hpp
-
-#pragma once
-#include "a_Macro.hpp"
-
-template <typename INT>
-INT GCD( const INT& b_0 , const INT& b_1 );
-
-template <typename INT> inline INT LCM( const INT& b_0 , const INT& b_1 );
