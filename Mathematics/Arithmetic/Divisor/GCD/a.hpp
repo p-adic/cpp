9ffbea39aa3d3e1031df32e3a072b226c14072ab
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-07-20 18:06:27 +0900

    Add files via upload

--- Mathematics/Arithmetic/Divisor/GCD/a.hpp
+++ Mathematics/Arithmetic/Divisor/GCD/a.hpp
@@ -2,12 +2,15 @@
 
 #pragma once
 
+// verify:
+// https://yukicoder.me/submissions/996609（GCD）
+
+// 絶対値A以下の整数N個のgcdをO(N log A)で求める。
 inline int GCD();
 template <typename INT1 , typename...INT2> INT1 GCD( INT1 b_0 , INT2... args );
 template <typename INT , template <typename...> typename V> INT GCD( V<INT> a );
 
-template <typename INT1 , typename INT2> inline INT1 LCM( const INT1& b_0 , const INT2& b_1 );
-
+// 絶対値A以下の整数N個のlcmをO(N log A)で求める。（オーバーフローに注意）
 inline int LCM();
 template <typename INT1 , typename...INT2> INT1 LCM( INT1 b_0 , INT2... args );
 template <typename INT , template <typename...> typename V> INT LCM( V<INT> a );
