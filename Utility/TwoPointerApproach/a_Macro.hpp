7fc16ab5fb5222146e7a8c90781d53be827b1ec2
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-07-24 22:07:43 +0900

    Add files via upload

--- Utility/TwoPointerApproach/a_Macro.hpp
+++ Utility/TwoPointerApproach/a_Macro.hpp
@@ -1,6 +1,7 @@
 // c:/Users/user/Documents/Programming/Utility/TwoPointerApproach/a_Macro.hpp
 
 #pragma once
+#include "../StdStream/a_Macro.hpp"
 
 #ifndef decldecay_t
   #define decldecay_t( VAR ) decay_t<decltype( VAR )>
@@ -8,27 +9,29 @@
 
 #ifndef TPA
 // 尺取り法用
-// VAR_TPA_LとVAR_TPA_RをINITで初期化し、VAR_TPA_RがCONTINUE_CONDITIONを満たす限り、
-// 閉区間[VAR_TPA_L,VAR_TPA_R]が条件ON_CONDITIONを満たすか否かを判定し、
-// trueになるかVAR_TAR_LがVAR_TAR_Rに追い付くまでVAR_TPA_Lの更新操作UPDATE_Lを繰り返し、
-// その後VAR_TPA_Rの更新操作UPDATE_Rを行う。
-// ON_CONDITIONがtrueとなる極大閉区間とその時点でのINFOをANSWERに格納する。
-#define TPA( ANSWER , VAR_TPA , INIT , CONTINUE_CONDITION , UPDATE_L , UPDATE_R , ON_CONDITION , INFO ) \
-  vector<tuple<decldecay_t( INIT ),decldecay_t( INIT ),decldecay_t( INFO )>> ANSWER{}; \
+// VAR_TPAは尺取り法用の変数名の接頭辞で、実際の変数名ではなく、_Lと_Rと_infoがつく。
+// ANSWER ## _temp = {VAR_TPA ## _L,VAR_TPA ## _R,,VPA_TPA ## _info}を{INIT,INIT,INFO_init}で初期化する。VPA_TPA ## _infoは区間和など。
+// ANSWER ## _tempがCONTINUE_CONDITIONを満たす限り、ANSWER ## _tempが条件ON_CONDITIONを満たすか否かを判定し、
+// それがtrueになるかVAR_TAR ## _LがVAR_TAR ## _Rに追い付くまでVAR_TPA ## _LとVPA_TPA ## _infoの更新操作UPDATE_Lを繰り返し、
+// その後VAR_TPA ## _RとVPA_TPA ## _infoの更新操作UPDATE_Rを行う。（マクロとコンマの制約上、関数オブジェクトを用いる）
+// ON_CONDITIONがtrueとなる極大閉区間とその時点でのinfoをANSWERに格納する。
+#define TPA( ANSWER , VAR_TPA , INIT , CONTINUE_CONDITION , UPDATE_L , UPDATE_R , ON_CONDITION , INFO_init ) \
+  vector<tuple<decldecay_t( INIT ),decldecay_t( INIT ),decldecay_t( INFO_init )>> ANSWER{}; \
   {									\
     auto init_TPA = INIT;						\
-    decldecay_t( ANSWER.front() ) ANSWER ## _temp = { init_TPA , init_TPA , INFO }; \
+    decldecay_t( ANSWER.front() ) ANSWER ## _temp = { init_TPA , init_TPA , INFO_init }; \
     auto ANSWER ## _prev = ANSWER ## _temp;				\
     auto& VAR_TPA ## _L = get<0>( ANSWER ## _temp );			\
     auto& VAR_TPA ## _R = get<1>( ANSWER ## _temp );			\
     auto& VAR_TPA ## _info = get<2>( ANSWER ## _temp );			\
     bool on_TPA_prev = false;						\
-    while( true ){							\
+    while( true ){								\
       bool continuing = CONTINUE_CONDITION;				\
       bool on_TPA = continuing && ( ON_CONDITION );			\
       CERR( continuing ? "尺取り中" : "尺取り終了" , "： [L,R] = [" , VAR_TPA ## _L , "," , VAR_TPA ## _R , "] ," , on_TPA_prev ? "on" : "off" , "->" , on_TPA ? "on" : "off" , ", info =" , VAR_TPA ## _info );	\
       if( on_TPA_prev && ! on_TPA ){					\
 	ANSWER.push_back( ANSWER ## _prev );				\
+	CERR( #ANSWER , "に" , ANSWER ## _prev , "を格納します。" );	\
       }									\
       if( continuing ){							\
 	if( on_TPA || VAR_TPA ## _L == VAR_TPA ## _R ){			\
