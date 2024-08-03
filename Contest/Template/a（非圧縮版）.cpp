1ebf714f7073c123e576a4c7f3fb3866ab26d57f
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-08-04 08:21:54 +0900

    Add files via upload

--- Contest/Template/a（非圧縮版）.cpp
+++ Contest/Template/a（非圧縮版）.cpp
@@ -298,36 +298,36 @@ using path = pair<int,ll>;
   static_assert( ! is_same<decldecay_t( CONST_TARGET ),uint>::value && ! is_same<decldecay_t( CONST_TARGET ),ull>::value ); \
   ll ANSWER = MINIMUM;							\
   {									\
-    ll L_BS = MINIMUM;							\
-    ll U_BS = MAXIMUM;							\
+    ll ANSWER ## _L = MINIMUM;							\
+    ll ANSWER ## _R = MAXIMUM;							\
     ANSWER = UPDATE_ANSWER;						\
     ll EXPRESSION_BS;							\
     const ll CONST_TARGET_BS = ( CONST_TARGET );			\
     ll DIFFERENCE_BS;							\
-    while( L_BS < U_BS ){						\
+    while( ANSWER ## _L < ANSWER ## _R ){						\
       DIFFERENCE_BS = ( EXPRESSION_BS = ( EXPRESSION ) ) - CONST_TARGET_BS; \
-      CERR( "二分探索中：" , "L_BS =" , L_BS , "<=" , #ANSWER , "=" , ANSWER , "<=" , U_BS , "= U_BS : (" , #EXPRESSION , ") =" , EXPRESSION_BS , DIFFERENCE_BS > 0 ? ">" : DIFFERENCE_BS < 0 ? "<" : "=" , CONST_TARGET_BS , "= (" , #CONST_TARGET , ")" ); \
+      CERR( "二分探索中：" , string{ #ANSWER } + "_L" , "=" , ANSWER ## _L , "<=" , #ANSWER , "=" , ANSWER , "<=" , ANSWER ## _R , "=" , string{ #ANSWER } + "_R" , ":" , #EXPRESSION , "=" , EXPRESSION_BS , DIFFERENCE_BS > 0 ? ">" : DIFFERENCE_BS < 0 ? "<" : "=" , CONST_TARGET_BS , "=" , #CONST_TARGET ); \
       if( DIFFERENCE_BS INEQUALITY_FOR_CHECK 0 ){			\
-	U_BS = UPDATE_U;						\
+	ANSWER ## _R = UPDATE_U;						\
       } else {								\
-	L_BS = UPDATE_L;						\
+	ANSWER ## _L = UPDATE_L;						\
       }									\
       ANSWER = UPDATE_ANSWER;						\
     }									\
-    if( L_BS > U_BS ){							\
-      CERR( "二分探索失敗：" , "L_BS =" , L_BS , ">" , U_BS , "= U_BS :" , #ANSWER , ":= (" , #MAXIMUM , ") + 1 =" , MAXIMUM + 1  ); \
+    if( ANSWER ## _L > ANSWER ## _R ){							\
+      CERR( "二分探索失敗：" , string{ #ANSWER } + "_L" , "=" , ANSWER ## _L , ">" , ANSWER ## _R , "=" , string{ #ANSWER } + "_R" , ":" , #ANSWER , ":=" , #MAXIMUM , "+ 1 =" , MAXIMUM + 1  ); \
       CERR( "二分探索マクロにミスがある可能性があります。変更前の版に戻してください。" ); \
       ANSWER = MAXIMUM + 1;						\
     } else {								\
-      CERR( "二分探索終了：" , "L_BS =" , L_BS , "<=" , #ANSWER , "=" , ANSWER , "<=" , U_BS , "= U_BS" ); \
+      CERR( "二分探索終了：" , string{ #ANSWER } + "_L" , "=" , ANSWER ## _L , "<=" , #ANSWER , "=" , ANSWER , "<=" , ANSWER ## _R , "=" , string{ #ANSWER } + "_R" ); \
       CERR( "二分探索が成功したかを確認するために" , #EXPRESSION , "を計算します。" ); \
       CERR( "成功判定が不要な場合はこの計算を削除しても構いません。" );	\
       EXPRESSION_BS = ( EXPRESSION );					\
-      CERR( "二分探索結果： (" , #EXPRESSION , ") =" , EXPRESSION_BS , ( EXPRESSION_BS > CONST_TARGET_BS ? ">" : EXPRESSION_BS < CONST_TARGET_BS ? "<" : "=" ) , CONST_TARGET_BS ); \
+      CERR( "二分探索結果：" , #EXPRESSION , "=" , EXPRESSION_BS , ( EXPRESSION_BS > CONST_TARGET_BS ? ">" : EXPRESSION_BS < CONST_TARGET_BS ? "<" : "=" ) , CONST_TARGET_BS ); \
       if( EXPRESSION_BS DESIRED_INEQUALITY CONST_TARGET_BS ){		\
 	CERR( "二分探索成功：" , #ANSWER , ":=" , ANSWER );		\
       } else {								\
-	CERR( "二分探索失敗：" , #ANSWER , ":= (" , #MAXIMUM , ") + 1 =" , MAXIMUM + 1 ); \
+	CERR( "二分探索失敗：" , #ANSWER , ":=" , #MAXIMUM , "+ 1 =" , MAXIMUM + 1 ); \
 	CERR( "単調でないか、単調増加性と単調減少性を逆にしてしまったか、探索範囲内に解が存在しません。" ); \
 	ANSWER = MAXIMUM + 1;						\
       }									\
@@ -335,13 +335,13 @@ using path = pair<int,ll>;
   }									\
 
 // 単調増加の時にEXPRESSION >= CONST_TARGETの最小解を格納。
-#define BS1( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET ) BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , >= , CONST_TARGET , >= , ANSWER , ANSWER + 1 , ( L_BS + U_BS ) / 2 )
+#define BS1( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET ) BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , >= , CONST_TARGET , >= , ANSWER , ANSWER + 1 , ( ANSWER ## _L + ANSWER ## _R ) / 2 )
 // 単調増加の時にEXPRESSION <= CONST_TARGETの最大解を格納。
-#define BS2( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET ) BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , <= , CONST_TARGET , > , ANSWER - 1 , ANSWER , ( L_BS + 1 + U_BS ) / 2 )
+#define BS2( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET ) BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , <= , CONST_TARGET , > , ANSWER - 1 , ANSWER , ( ANSWER ## _L + 1 + ANSWER ## _R ) / 2 )
 // 単調減少の時にEXPRESSION >= CONST_TARGETの最大解を格納。
-#define BS3( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET ) BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , >= , CONST_TARGET , < , ANSWER - 1 , ANSWER , ( L_BS + 1 + U_BS ) / 2 )
+#define BS3( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET ) BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , >= , CONST_TARGET , < , ANSWER - 1 , ANSWER , ( ANSWER ## _L + 1 + ANSWER ## _R ) / 2 )
 // 単調減少の時にEXPRESSION <= CONST_TARGETの最小解を格納。
-#define BS4( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET ) BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , <= , CONST_TARGET , <= , ANSWER , ANSWER + 1 , ( L_BS + U_BS ) / 2 )
+#define BS4( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , CONST_TARGET ) BS( ANSWER , MINIMUM , MAXIMUM , EXPRESSION , <= , CONST_TARGET , <= , ANSWER , ANSWER + 1 , ( ANSWER ## _L + ANSWER ## _R ) / 2 )
 
 // 尺取り法用
 // VAR_TPAは尺取り法用の変数名の接頭辞で、実際の変数名ではなく、_Lと_Rと_infoがつく。
