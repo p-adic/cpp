d31c9a75a9a14c9fc1a16ceeba616e25243d6b40
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-08-17 14:28:56 +0900

    Add files via upload

--- Mathematics/Polynomial/LagrangeInterpolation/a.hpp
+++ Mathematics/Polynomial/LagrangeInterpolation/a.hpp
@@ -3,5 +3,7 @@
 #pragma once
 #include "../a.hpp"
 
+// Tが体である場合のみサポート。
+// argとvalのサイズをMとして、O(M^2 log M)でラグランジュ補間を計算する。
 template <typename T> Polynomial<T> LagrangeInterpolation( const vector<T>& arg , const vector<T>& val );
 
--- Mathematics/Polynomial/LagrangeInterpolation/a_Body.hpp
+++ Mathematics/Polynomial/LagrangeInterpolation/a_Body.hpp
@@ -14,7 +14,7 @@ Polynomial<T> LagrangeInterpolation( const vector<T>& arg , const vector<T>& val
   const int size = arg.size();
   assert( size > 0 && int( val.size() ) == size );
 
-  // SeDifferenceProductそのものだが、top.front()を流用するために実装をコピーする。
+  // SetDifferenceProductそのものだが、top.front()を流用するために実装をコピーする。
   list<vector<Polynomial<T> > > pt{};
   SetPointTree( arg , pt );
   const vector<Polynomial<T> >& top = pt.front();
@@ -22,16 +22,19 @@ Polynomial<T> LagrangeInterpolation( const vector<T>& arg , const vector<T>& val
   Polynomial<T> g{ Differential( 1 , f ) };
   vector<T> coef{};
   SetPointTreeEvaluation( g , pt , coef );
-  auto itr = pt.back().begin();
 
   Polynomial<T> answer{};
+  auto itr = pt.back().begin();
   
   for( int i = 0 ; i < size ; i++ ){
 
+    // 1次式による商なので愚直の方が速いがバグを検出しやすいように
+    // 多項式除算の一般のアルゴリズムを用いる。
     answer += f / *( itr++ ) * ( val[i] / coef[i] );
 
   }
 
+  answer.RemoveRedundantZero();
   return answer;
 
 }
