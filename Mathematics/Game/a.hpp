744ae43a06cecbf549f1ddf8fbf4d5d99a358975
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-08-19 09:44:53 +0900

    Add files via upload

--- Mathematics/Game/a.hpp
+++ Mathematics/Game/a.hpp
@@ -2,9 +2,15 @@
 
 #pragma once
 
+// 着手が存在しない場合に負けとなるゲームの勝敗を計算する。
 // Edgeはゲームの状態遷移を表す写像edge:T->T^{<\infty}に相当する型。
 template <typename Edge , typename T> const bool& IsWinningState( Edge& edge , const T& t , const bool& reset = false );
 
+// 着手が存在しない場合に負けとなるゲームのグランディ数を計算する。
 // AEdgeはゲームの状態遷移の和への分解を表す写像aedge:T->(T^{<\infty})^{<\infty}に相当する型。
 // 各a in aedge(t)はそのゲーム和がtの遷移先に対応する配列。
 template <typename AEdge , typename T> const int& GrundyNumber( AEdge& aedge , const T& t , const bool& reset = false );
+
+// 着手が存在しない局面の{0,1}値不変量が最初に与えられる定数Cと一致する場合に
+// 勝ちとなるゲームで必勝となるC全体の集合を2進法で計算する。
+template <typename Edge , typename T , typename INVARIANT> const int& WinningConstantsOf( Edge& edge , const T& t , const INVARIANT& invariant , const bool& reset = false );
--- Mathematics/Game/a_Body.hpp
+++ Mathematics/Game/a_Body.hpp
@@ -76,3 +76,42 @@ const int& GrundyNumber( AEdge& aedge , const T& t , const bool& reset )
   return g[t] = mex.Get();
 
 }
+
+template <typename Edge , typename T , typename INVARIANT>
+const int& WinningConstantsOf( Edge& edge , const T& t , const INVARIANT& invariant , const bool& reset )
+{
+
+
+  static_assert( is_invocable_v<Edge,const T&> );
+  static Map<T,int> g{};
+
+  if( reset ){
+
+    g.clear();
+
+  }
+
+  if( g.count( t ) == 1 ){
+
+    return g[t];
+
+  }
+
+  auto&& e = edge( t );
+  int c = e.empty() ? ( 1 << invariant( t ) ) : 0;
+
+  for( auto&& u : e ){
+
+    int temp = WinningConstantsOf( edge , u , invariant );
+
+    for( int d = 0 ; d <= 1 ; d++ ){
+
+      c |= ( ( temp >> ( 1 ^ d ) ) & 1 ) << d;
+
+    }
+
+  }
+
+  return ( g[t] = c );
+
+}
