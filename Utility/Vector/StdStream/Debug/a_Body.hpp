a79a94e455eb36272f22533caa7f1721b982c9f3
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-09-08 20:12:47 +0900

    Add files via upload

--- Utility/Vector/StdStream/Debug/a_Body.hpp
+++ Utility/Vector/StdStream/Debug/a_Body.hpp
@@ -12,6 +12,9 @@ DEFINITION_OF_COUT_FOR_DOUBLE_VECTOR( list , "[" , "," , "]" );
 DEFINITION_OF_COUT_FOR_DOUBLE_VECTOR( set , "{" , " <" , "}" );
 DEFINITION_OF_COUT_FOR_DOUBLE_VECTOR( unordered_set , "{" , "," , "}" );
 
+DEFINITION_OF_COUT_FOR_VECTOR( multiset , "{" , " <=" , "}" );
+DEFINITION_OF_COUT_FOR_DOUBLE_VECTOR( multiset , "{" , " <=" , "}" );
+
 inline void VariadicResize( const int& size ) {}
 template <typename Arg , typename... ARGS> inline void VariadicResize( const int& size , Arg& arg , ARGS&... args ) { arg.resize( size ); VariadicResize( size , args... ); }
 
