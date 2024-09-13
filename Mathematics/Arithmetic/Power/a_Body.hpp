a1e141d38bde503d3b6b2e3a2551b19b48f8e3bf
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-09-14 01:14:35 +0900

    Add files via upload

--- Mathematics/Arithmetic/Power/a_Body.hpp
+++ Mathematics/Arithmetic/Power/a_Body.hpp
@@ -3,37 +3,47 @@
 #pragma once
 #include "a.hpp"
 
-#include "../Padic/a_Body.hpp"
-
-// PowerBinaryMetodの呼び出しは部分特殊化ではなくオーバーロードで処理できるようにするためにPowerBinaryMethod<T,UINT>とはしない。
-template <typename T , typename UINT>
-inline T Power( const T& t , const UINT& num , const T& init , const bool& for_right_multiplication , const string& method ) { return method == "binary" ? PowerBinaryMethod( t , num , init , for_right_multiplication ) : PowerNormalMethod( t , num , init , for_right_multiplication ); }
-
-template <typename T , typename UINT> inline T PowerNormalMethod( const T& t , const UINT& num , const T& init , const bool& for_right_multiplication ) { return num == 0 ? init : ( for_right_multiplication ? PowerNormalMethod( t , num - 1 , init ) * t : t * PowerNormalMethod( t , num - 1 , init ) ); }
+template <typename T> inline T Square( const T& t ) { return t * t; }
 
 template <typename T , typename UINT>
-T PowerBinaryMethod( const T& t , const UINT& num , const T& init , const bool& for_right_multiplication )
+T Power( T t , UINT exponent )
 {
 
-  const VLArray<UINT>& num_binary = AdicInt<2>::Expand( num );
-  T answer = init;
-  T power = t;
+  T answer = ( exponent & 1 ) == 1 ? t : 1;
+  exponent >>= 1;
 
-  for( auto itr = num_binary.begin() , end = num_binary.end() ; itr != end ; itr++ ){
+  while( exponent > 0 ){
 
-    if( *itr == 1 ){
-
-      answer = for_right_multiplication ? answer * power : power * answer;
-
-    }
-
+    // オーバーフロー対策で先に（必要な時だけ）2乗する。
     // 部分特殊化ではなくオーバーロードで処理できるようにするためにSquare<T>としない。
-    power = Square( power );
-
+    t = Square( t );
+    ( exponent & 1 ) == 1 ? answer *= t : answer;
+    exponent >>= 1;
+    
   }
 
   return answer;
 
 }
 
-template <typename T> inline T Square( const T& t ) { return t * t; }
+template <typename T , typename UINT>
+T Power( T t , UINT exponent , T init )
+{
+
+  ( exponent & 1 ) == 1 ? init *= t : init;
+  exponent >>= 1;
+
+  while( exponent > 0 ){
+
+    // オーバーフロー対策で先に（必要な時だけ）2乗する。
+    // 部分特殊化ではなくオーバーロードで処理できるようにするためにSquare<T>としない。
+    t = Square( t );
+    ( exponent & 1 ) == 1 ? init *= t : init;
+    exponent >>= 1;
+    
+  }
+
+  return move( init );
+
+}
+
---
+++ Mathematics/Arithmetic/Power/a_Macro.hpp
@@ -0,0 +1,23 @@
+// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/Power/a_Macro.hpp
+
+#pragma once
+
+#ifndef decldecay_t
+  #define decldecay_t( VAR ) decay_t<decltype( VAR )>
+#endif
+
+#define POWER_MOD( ANSWER , ARGUMENT , EXPONENT , MODULO )		\
+  ll ANSWER{ 1 };							\
+  {									\
+    ll ARGUMENT_FOR_SQUARE_FOR_POWER = ( ( ARGUMENT ) % ( MODULO ) ) % ( MODULO ); \
+    ARGUMENT_FOR_SQUARE_FOR_POWER < 0 ? ARGUMENT_FOR_SQUARE_FOR_POWER += ( MODULO ) : ARGUMENT_FOR_SQUARE_FOR_POWER; \
+    decldecay_t( EXPONENT ) EXPONENT_FOR_SQUARE_FOR_POWER = ( EXPONENT );	\
+    while( EXPONENT_FOR_SQUARE_FOR_POWER != 0 ){			\
+      if( EXPONENT_FOR_SQUARE_FOR_POWER % 2 == 1 ){			\
+	ANSWER = ( ANSWER * ARGUMENT_FOR_SQUARE_FOR_POWER ) % ( MODULO ); \
+      }									\
+      ARGUMENT_FOR_SQUARE_FOR_POWER = ( ARGUMENT_FOR_SQUARE_FOR_POWER * ARGUMENT_FOR_SQUARE_FOR_POWER ) % ( MODULO ); \
+      EXPONENT_FOR_SQUARE_FOR_POWER /= 2;				\
+    }									\
+  }									\
+
