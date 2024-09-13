d486b5125e760cdc47c4a2fc53678c5877757d82
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-09-13 09:16:09 +0900

    Add files via upload

--- Contest/AutoCheck/a_Macro.hpp
+++ Contest/AutoCheck/a_Macro.hpp
@@ -3,43 +3,45 @@
 #pragma once
 
 #define ASK_NUMBER( ... )						\
-  CERR( "" );								\
-  CERR( "何番を選択しますか？" );					\
-  problems = { __VA_ARGS__ };						\
-  problems_size = problems.size();					\
-  for( int i = 0 ; i < problems_size ; i++ ){				\
-    CERR( i , ": " , problems[i] );					\
-  }									\
-  if( use_getline ){							\
-    getline( cin , reply );						\
-    num = stoi( reply );						\
-  } else {								\
-    cin >> num;								\
-  }									\
-  CERR( "" );								\
-  if( num < 0 || num >= problems_size ){				\
-    CERR( "返答は" , problems_size - 1 , "以下の非負整数にしてください。" ); \
-    CERR( "終了します。" );						\
+  while( true ){                                                        \
     CERR( "" );								\
-    return;								\
-  }									\
-  num_temp = 0;								\
+    CERR( "何番を選択しますか？" );					\
+    problems = { __VA_ARGS__ };						\
+    problems_size = problems.size();					\
+    for( int i = 0 ; i < problems_size ; i++ ){				\
+      CERR( i , ": " , problems[i] );					\
+    }									\
+    if( use_getline ){							\
+      getline( cin , reply );						\
+      num = stoi( reply );						\
+    } else {								\
+      cin >> num;                                                       \
+    }									\
+    CERR( "" );								\
+    if( num < 0 || num >= problems_size ){				\
+      CERR( "返答は" , problems_size - 1 , "以下の非負整数にしてください。" ); \
+    } else {                                                            \
+      break;                                                            \
+    }                                                                   \
+  }                                                                     \
+  num_temp = 0;                                                         \
   
-#define ASK_YES_NO( QUESTION )			\
-  CERR( "" );					\
-  CERR( QUESTION , "[y/n]" );			\
-  if( use_getline ){				\
-    getline( cin , reply );			\
-  } else {					\
-    cin >> reply;				\
-  }						\
-  if( reply != "y" && reply != "n" ){		\
-    CERR( "y/nのいずれかで答えてください。" );	\
-    CERR( "終了します。" );			\
-    CERR( "" );					\
-    return;					\
-  }						\
-  CERR( "" );					\
+#define ASK_YES_NO( QUESTION )                          \
+  while( true ){                                        \
+    CERR( "" );                                         \
+    CERR( QUESTION , "[y/n]" );                         \
+    if( use_getline ){                                  \
+      getline( cin , reply );                           \
+    } else {                                            \
+      cin >> reply;                                     \
+    }                                                   \
+    CERR( "" );                                         \
+    if( reply != "y" && reply != "n" ){                 \
+      CERR( "y/nのいずれかで答えてください。" );	\
+    } else {                                            \
+      break;                                            \
+    }                                                   \
+  }                                                     \
 
 #define AC( BRANCH ) void BRANCH ## AutoCheck( int& num , int& num_temp , string& reply , vector<string>& problems , int& problems_size , const bool& use_getline )
 #define CALL_AC( BRANCH ) BRANCH ## AutoCheck( num , num_temp , reply , problems , problems_size , use_getline )
