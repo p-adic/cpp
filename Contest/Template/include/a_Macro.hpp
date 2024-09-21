36f62d045e11f57bf538713a01635c0ebfff6502
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-09-21 19:58:07 +0900

    Add files via upload

--- Contest/Template/include/a_Macro.hpp
+++ Contest/Template/include/a_Macro.hpp
@@ -2,19 +2,22 @@
 
 #pragma once
 
-
 #ifdef SAMPLE_CHECK
   #define INPUT ifs
   #define DEBUG_OUTPUT ofs_err
   #define COUT( ... ) VariadicCout( ofs , __VA_ARGS__ ) << endl
   #define COUTNS( ... ) VariadicCoutNonSep( ofs , __VA_ARGS__ ) << flush
   #define COUT_A( I , N , A ) CoutArray( ofs , I , N , A ) << endl
+  #define DERR( ... ) VariadicCout( ofs_err , __VA_ARGS__ ) << endl
+  #define DERRNS( ... ) VariadicCoutNonSep( ofs_err , __VA_ARGS__ ) << flush
 #else
   #define INPUT cin
   #define DEBUG_OUTPUT cerr
   #define COUT( ... ) VariadicCout( cout << "出力" << __LINE__ << "：" , __VA_ARGS__ ) << endl
   #define COUTNS( ... ) VariadicCoutNonSep( cout << "出力" << __LINE__ << "： " , __VA_ARGS__ ) << flush
   #define COUT_A( I , N , A ) CoutArray( cout << "出力： " , I , N , A ) << endl
+  #define DERR( ... ) CERR( __VA_ARGS__ )
+  #define DERRNS( ... ) CERRNS( __VA_ARGS__ )
 #endif
 
 #define REPEAT_MAIN( BOUND )                                    \
@@ -35,7 +38,7 @@
         SmallTest();                                              \
       } else if( exec_mode == random_test_mode ){                 \
         CERR( "ランダムテストを行う回数を指定してください。" );          \
-        SET_LL( test_case_num );                                  \
+        cin >> test_case_num;                                         \
         RandomTest( test_case_num );                              \
       }                                                           \
       RE 0;                                                       \
@@ -48,21 +51,21 @@
 
   #define FINISH_MAIN                                                     \
       int sample_repetition_num; sample_count >> sample_repetition_num;       \
-      REPEAT( sample_repetition_num ){                                           \
-        sample_count >> sample_num;                                    \
-        ofs_err << "サンプル" << sample_num << "を実行します。" << endl; \
-        ofs_err << endl;                                                \
-        ifs = ifstream{ input_path + sample_num + ".txt" };            \
+      REPEAT( sample_repetition_num ){                                  \
+        sample_count >> sample_num;                                     \
+        DERRNS( "サンプル" , stoi( sample_num ) , "を実行します。\n" ); \
+        DERR( "" );                                                     \
+        ifs = ifstream{ input_path + sample_num + ".txt" };             \
         ofs = ofstream{ output_path + sample_num + ".txt" , ios::trunc }; \
-        if constexpr( bound_test_case_num > 1 ){             \
-          SET_ASSERT( test_case_num , 1 , bound_test_case_num );          \
+        if constexpr( bound_test_case_num > 1 ){                        \
+          SET_ASSERT( test_case_num , 1 , bound_test_case_num );      \
         }                                                             \
-        REPEAT( test_case_num ){                                           \
-          Solve();                                                         \
+        REPEAT( test_case_num ){                                        \
+          Solve();                                                      \
         }                                                               \
-        CompareAnswer( sample_path , problem_order , sample_num );    \
-        ofs_err << "サンプル" << sample_num << "の実行を終了しました。" << endl; \
-        ofs_err << endl;                                                \
+        CompareAnswer( sample_path , problem_order , sample_num );      \
+        DERRNS( "サンプル" , stoi( sample_num ) , "の実行を終了しました。" ); \
+        DERR( "" );                                                     \
       }                                                                 \
     }                                                                 \
 
@@ -71,25 +74,25 @@
   #define FINISH_MAIN                                                    \
       if constexpr( bound_test_case_num > 1 ){             \
         CERR( "テストケースの個数を入力してください。" );               \
-        SET_ASSERT( test_case_num , 1 , bound_test_case_num );    \
-        REPEAT( test_case_num ){                                                \
-          CERR( "testcase " , VARIABLE_FOR_REPEAT_test_case_num , ":" ); \
-          Solve();                                                         \
-          CERR( "" );                                                      \
-        }                                                                  \
+        SET_ASSERT( test_case_num , 1 , bound_test_case_num );          \
+        REPEAT( test_case_num ){                                        \
+          CERR( "testcase" , VARIABLE_FOR_REPEAT_test_case_num , ":" ); \
+          Solve();                                                      \
+          CERR( "" );                                                   \
+        }                                                               \
       } else {                                              \
-        Solve();                                                         \
-        CERR( "" );                                                      \
-      }                                                                  \
-    }                                                                    \
+        Solve();                                                        \
+        CERR( "" );                                                     \
+      }                                                                 \
+    }                                                                 \
 
 #endif
 
 #define DEXPR( LL , BOUND , VALUE1 , VALUE2 ) CEXPR( LL , BOUND , VALUE2 )
 
 #define ASSERT( A , MIN , MAX )                                         \
-  CERR( "ASSERTチェック： " , ( MIN ) , ( ( MIN ) <= A ? "<=" : ">" ) , A , ( A <= ( MAX ) ? "<=" : ">" ) , ( MAX ) ); \
-  AS( ( MIN ) <= A && A <= ( MAX ) )                                    \
+  DERR( "ASSERTチェック:" , ( MIN ) , ( ( MIN ) <= A ? "<=" : ">" ) , A , ( A <= ( MAX ) ? "<=" : ">" ) , ( MAX ) ); \
+  assert( ( MIN ) <= A && A <= ( MAX ) )                                    \
 
 #ifdef USE_GETLINE
   #define GETLINE_SEPARATE( SEPARATOR , ... ) string __VA_ARGS__; VariadicGetline( INPUT , SEPARATOR , __VA_ARGS__ )
@@ -112,7 +115,7 @@
     SET_LL( A );                                        \
     ASSERT( A , MIN , MAX );                            \
   } else if( exec_mode == random_test_mode ){           \
-    CERR( #A , " = " , ( A = GetRand( MIN , MAX ) ) );  \
+    DERR( #A , " = " , ( A = GetRand( MIN , MAX ) ) );  \
   } else { AS( false ); }                               \
 
-#define SOLVE_ONLY ST_AS( __FUNCTION__[0] == 'S' )
+#define SOLVE_ONLY static_assert( __FUNCTION__[0] == 'S' )
