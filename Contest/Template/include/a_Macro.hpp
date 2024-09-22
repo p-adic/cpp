8e7d82dec9390b02021d00f48a5c108dd8165f70
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-09-22 15:13:42 +0900

    Add files via upload

--- Contest/Template/include/a_Macro.hpp
+++ Contest/Template/include/a_Macro.hpp
@@ -2,20 +2,38 @@
 
 #pragma once
 
+// 提出時: INPUT_SOURCE -> 不使用, COUT -> cout, CERR -> 出力なし, DERR -> 不使用
+// サンプル確認時: INPUT_SOURCE -> ifs, COUT -> ofs, CERR -> cerr, DERR -> ofs_err
+// デバッグ出力時: INPUT_SOURCE -> cin, COUT -> cout, CERR -> cerr, DERR -> cerr
+// 非デバッグ出力時: INPUT_SOURCE -> cin, COUT -> cout, CERR -> cerr, DERR -> 出力なし
 #ifdef SAMPLE_CHECK
-  #define INPUT ifs
-  #define COUT( ... ) VariadicCout( ofs , __VA_ARGS__ ) << endl
-  #define COUTNS( ... ) VariadicCoutNonSep( ofs , __VA_ARGS__ ) << flush
-  #define COUT_A( I , N , A ) CoutArray( ofs , I , N , A ) << endl
-  #define DERR( ... ) VariadicCout( ofs_err , __VA_ARGS__ ) << endl
-  #define DERRNS( ... ) VariadicCoutNonSep( ofs_err , __VA_ARGS__ ) << flush
+  #define INPUT_SOURCE ifs
+  #define COUT_TARGET ofs
+  #define DERR_TARGET ofs_err
+#elif defined( DEBUG_OUTPUT )
+  #define INPUT_SOURCE cin
+  #define COUT_TARGET cout
+  #define DERR_TARGET cerr
 #else
-  #define INPUT cin
-  #define COUT( ... ) VariadicCout( cout << "出力" << __LINE__ << "：" , __VA_ARGS__ ) << endl
-  #define COUTNS( ... ) VariadicCoutNonSep( cout << "出力" << __LINE__ << "： " , __VA_ARGS__ ) << flush
-  #define COUT_A( I , N , A ) CoutArray( cout << "出力： " , I , N , A ) << endl
-  #define DERR( ... ) CERR( __VA_ARGS__ )
-  #define DERRNS( ... ) CERRNS( __VA_ARGS__ )
+  #define INPUT_SOURCE cin
+  #define COUT_TARGET cout
+#endif
+
+#define COUT( ... ) VariadicCout( COUT_TARGET , __VA_ARGS__ ) << endl
+#define COUTNS( ... ) VariadicCoutNonSep( COUT_TARGET , __VA_ARGS__ ) << flush
+#define COUT_A( I , N , A ) CoutArray( COUT_TARGET , I , N , A ) << endl
+#define CERR( ... ) VariadicCout( cerr , __VA_ARGS__ ) << endl
+#define CERRNS( ... ) VariadicCoutNonSep( cerr , __VA_ARGS__ ) << flush
+#define CERR_A( I , N , A ) CoutArray( cerr , I , N , A ) << endl
+
+#ifdef DERR_TARGET
+  #define DERR( ... ) VariadicCout( DERR_TARGET , __VA_ARGS__ ) << endl
+  #define DERRNS( ... ) VariadicCoutNonSep( DERR_TARGET , __VA_ARGS__ ) << flush
+  #define DERR_A( I , N , A ) CoutArray( DERR_TARGET , I , N , A ) << flush
+#else
+  #define DERR( ... ) 
+  #define DERRNS( ... ) 
+  #define DERR_A( I , N , A ) 
 #endif
 
 #define REPEAT_MAIN( BOUND )                                    \
@@ -51,8 +69,8 @@
       int sample_repetition_num; sample_count >> sample_repetition_num;       \
       REPEAT( sample_repetition_num ){                                  \
         sample_count >> sample_num;                                     \
-        DERRNS( "サンプル" , stoi( sample_num ) , "を実行します。\n" ); \
-        DERR( "" );                                                     \
+        CERRNS( "サンプル" , stoi( sample_num ) , "を実行します。\n" ); \
+        CERR( "" );                                                     \
         ifs = ifstream{ input_path + sample_num + ".txt" };             \
         ofs = ofstream{ output_path + sample_num + ".txt" , ios::trunc }; \
         if constexpr( bound_test_case_num > 1 ){                        \
@@ -62,8 +80,8 @@
           Solve();                                                      \
         }                                                               \
         CompareAnswer( sample_path , problem_order , sample_num );      \
-        DERRNS( "サンプル" , stoi( sample_num ) , "の実行を終了しました。" ); \
-        DERR( "" );                                                     \
+        CERRNS( "サンプル" , stoi( sample_num ) , "の実行を終了しました。\n" ); \
+        CERR( "" );                                                     \
       }                                                                 \
     }                                                                 \
 
@@ -93,25 +111,15 @@
   assert( ( MIN ) <= A && A <= ( MAX ) )                                    \
 
 #ifdef USE_GETLINE
-  #define GETLINE_SEPARATE( SEPARATOR , ... ) string __VA_ARGS__; VariadicGetline( INPUT , SEPARATOR , __VA_ARGS__ )
+  #define GETLINE_SEPARATE( SEPARATOR , ... ) string __VA_ARGS__; VariadicGetline( INPUT_SOURCE , SEPARATOR , __VA_ARGS__ )
   #define GETLINE( ... ) GETLINE_SEPARATE( '\n' , __VA_ARGS__ )
   #define SET_LL( A ) { GETLINE( A ## _str ); A = stoll( A ## _str ); }
 #else
-  #define CIN( LL , ... ) LL __VA_ARGS__; VariadicCin( INPUT , __VA_ARGS__ )
-  #define SET_A( I , N , ... ) VariadicResize( N + I , __VA_ARGS__ ); FOR( VARIABLE_FOR_SET_A , 0 , N ){ VariadicSet( INPUT , VARIABLE_FOR_SET_A + I , __VA_ARGS__ ); }
+  #define CIN( LL , ... ) LL __VA_ARGS__; VariadicCin( INPUT_SOURCE , __VA_ARGS__ )
+  #define SET_A( I , N , ... ) VariadicResize( N + I , __VA_ARGS__ ); FOR( VARIABLE_FOR_SET_A , 0 , N ){ VariadicSet( INPUT_SOURCE , VARIABLE_FOR_SET_A + I , __VA_ARGS__ ); }
   #define CIN_A( LL , I , N , ... ) VE<LL> __VA_ARGS__; SET_A( I , N , __VA_ARGS__ )
   #define CIN_AA( LL , I0 , N0 , I1 , N1 , VAR ) VE<VE<LL>> VAR( N0 + I0 ); FOR( VARIABLE_FOR_CIN_AA , 0 , N0 ){ SET_A( I1 , N1 , VAR[VARIABLE_FOR_CIN_AA + I0] ); }
-  #define SET_LL( A ) INPUT >> A
-#endif
-
-#ifdef DEBUG_OUTPUT
-  #define CERR( ... ) VariadicCout( cerr , __VA_ARGS__ ) << endl
-  #define CERRNS( ... ) VariadicCoutNonSep( cerr , __VA_ARGS__ ) << flush
-  #define CERR_A( I , N , A ) CoutArray( cerr , I , N , A ) << endl
-#else
-  #define CERR( ... ) 
-  #define CERRNS( ... ) 
-  #define CERR_A( I , N , A ) 
+  #define SET_LL( A ) INPUT_SOURCE >> A
 #endif
 
 #define SET_ASSERT( A , MIN , MAX )                     \
@@ -119,7 +127,8 @@
     SET_LL( A );                                        \
     ASSERT( A , MIN , MAX );                            \
   } else if( exec_mode == random_test_mode ){           \
-    DERR( #A , " = " , ( A = GetRand( MIN , MAX ) ) );  \
+    A = GetRand( MIN , MAX );                           \
+    DERR( #A , " = " , A );                             \
   } else { AS( false ); }                               \
 
 #define SOLVE_ONLY static_assert( __FUNCTION__[0] == 'S' )
