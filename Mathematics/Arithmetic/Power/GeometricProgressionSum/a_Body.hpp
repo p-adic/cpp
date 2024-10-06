cb5ef2195da8caebb9abfd5d74a465965cbfa255
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-10-06 17:49:54 +0900

    Add files via upload

--- Mathematics/Arithmetic/Power/GeometricProgressionSum/a_Body.hpp
+++ Mathematics/Arithmetic/Power/GeometricProgressionSum/a_Body.hpp
@@ -5,7 +5,7 @@
 
 #include "../a_Body.hpp"
 
-template <typename T , typename UINT> inline T GeometricProgressionSum( T rate , UINT exponent_max , const T& init ) { const T& one = T::one(); T rate_minus = rate - one;  return ( Power( move( rate ) , move( ++exponent_max ) ) - one ) / move( rate_minus ) * init; }
+template <typename T , typename UINT> inline T GeometricProgressionSum( T rate , UINT exponent_max , const T& init ) { T rate_minus = rate - 1; return rate_minus == 0 ? init * ++exponent_max : ( Power( move( rate ) , move( ++exponent_max ) ) - 1 ) / move( rate_minus ) * init; }
 
 template <typename T , typename UINT>
 T GeometricProgressionLinearCombinationSum( vector<T> rate , vector<UINT> exponent_max , const vector<T>& init )
@@ -13,9 +13,9 @@ T GeometricProgressionLinearCombinationSum( vector<T> rate , vector<UINT> expone
 
   const int size = init.size();
   assert( int( rate.size() ) == size && int( exponent_max.size() ) == size );
-  T answer = T::zero();
+  T answer{};
 
-  for( int i = 0 , i < size ; i++ ){
+  for( int i = 0 ; i < size ; i++ ){
 
     answer += GeometricProgressionSum( move( rate[i] ) , move( exponent_max[i] ) , init[i] );
 
--- Mathematics/Arithmetic/Power/a_Macro.hpp
+++ Mathematics/Arithmetic/Power/a_Macro.hpp
@@ -6,18 +6,21 @@
   #define decldecay_t( VAR ) decay_t<decltype( VAR )>
 #endif
 
-#define POWER_MOD( ANSWER , ARGUMENT , EXPONENT , MODULO )		\
-  ll ANSWER{ 1 };							\
-  {									\
-    ll ARGUMENT_FOR_SQUARE_FOR_POWER = ( ( ARGUMENT ) % ( MODULO ) ) % ( MODULO ); \
-    ARGUMENT_FOR_SQUARE_FOR_POWER < 0 ? ARGUMENT_FOR_SQUARE_FOR_POWER += ( MODULO ) : ARGUMENT_FOR_SQUARE_FOR_POWER; \
-    decldecay_t( EXPONENT ) EXPONENT_FOR_SQUARE_FOR_POWER = ( EXPONENT );	\
-    while( EXPONENT_FOR_SQUARE_FOR_POWER != 0 ){			\
-      if( EXPONENT_FOR_SQUARE_FOR_POWER % 2 == 1 ){			\
-	ANSWER = ( ANSWER * ARGUMENT_FOR_SQUARE_FOR_POWER ) % ( MODULO ); \
+#ifndef POWER_MOD
+  #define POWER_MOD( ANSWER , ARGUMENT , EXPONENT , MODULO )		\
+    ll ANSWER{ 1 };							\
+    {									\
+      ll ARGUMENT_FOR_SQUARE_FOR_POWER = ( ( ARGUMENT ) % ( MODULO ) ) % ( MODULO ); \
+      ARGUMENT_FOR_SQUARE_FOR_POWER < 0 ? ARGUMENT_FOR_SQUARE_FOR_POWER += ( MODULO ) : ARGUMENT_FOR_SQUARE_FOR_POWER; \
+      decldecay_t( EXPONENT ) EXPONENT_FOR_SQUARE_FOR_POWER = ( EXPONENT ); \
+      while( EXPONENT_FOR_SQUARE_FOR_POWER != 0 ){			\
+        if( EXPONENT_FOR_SQUARE_FOR_POWER % 2 == 1 ){			\
+          ANSWER = ( ANSWER * ARGUMENT_FOR_SQUARE_FOR_POWER ) % ( MODULO ); \
+        }                                                               \
+        ARGUMENT_FOR_SQUARE_FOR_POWER = ( ARGUMENT_FOR_SQUARE_FOR_POWER * ARGUMENT_FOR_SQUARE_FOR_POWER ) % ( MODULO ); \
+        EXPONENT_FOR_SQUARE_FOR_POWER /= 2;				\
       }									\
-      ARGUMENT_FOR_SQUARE_FOR_POWER = ( ARGUMENT_FOR_SQUARE_FOR_POWER * ARGUMENT_FOR_SQUARE_FOR_POWER ) % ( MODULO ); \
-      EXPONENT_FOR_SQUARE_FOR_POWER /= 2;				\
     }									\
-  }									\
+
+#endif
 
