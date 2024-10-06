f2c65e35bb678ce39451791ed0a35131d5eb2468
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-10-06 17:49:37 +0900

    Add files via upload

---
+++ Mathematics/Arithmetic/QuadraticExtension/Constant/a_Body.hpp
@@ -0,0 +1,6 @@
+// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/QuadraticExtension/Constant/a_Body.hpp
+
+#pragma once
+#include "a.hpp"
+
+template <typename INT , int NUM> INT ConstantsForQuadraticExtensions<INT,NUM>::g_D = INT{};
--- Mathematics/Arithmetic/QuadraticExtension/a.hpp
+++ Mathematics/Arithmetic/QuadraticExtension/a.hpp
@@ -2,54 +2,86 @@
 
 #pragma once
 
-template <typename INT>
-class QuadraticExtension
+// verify:
+// https://yukicoder.me/submissions/1015838 (INT = Mod<998244353>, D = 5)
+
+#include "Constant/a.hpp"
+
+template <typename INT , int NUM>
+class QuadraticExtensions
 {
 
 private:
   INT m_a;
   INT m_b;
-  const INT* m_p_D;
   
 public:
-  inline QuadraticExtension() noexcept;
-  template <typename T> inline QuadraticExtension( const T& a ) noexcept;
-  inline QuadraticExtension( const INT& a , const INT& b , const INT* const & p_D ) noexcept;
-  inline QuadraticExtension( const QuadraticExtension<INT>& n ) noexcept;
+  inline QuadraticExtensions() noexcept;
+  inline QuadraticExtensions( const INT& a ) noexcept;
+  inline QuadraticExtensions( const INT& a , const INT& b ) noexcept;
+  inline QuadraticExtensions( const QuadraticExtensions<INT,NUM>& n ) noexcept;
+  inline QuadraticExtensions( QuadraticExtensions<INT,NUM>&& n ) noexcept;
 
-  inline const INT& GetA() const noexcept;
-  inline const INT& GetB() const noexcept;
-  inline const INT& GetD() const noexcept;
+  inline QuadraticExtensions<INT,NUM>& operator=( QuadraticExtensions<INT,NUM> n ) noexcept;
+  
+  inline pair<const INT&,const INT&> Get() const noexcept;
 
-  inline QuadraticExtension<INT>& operator+=( const QuadraticExtension<INT>& n ) noexcept;
-  template <typename T> inline QuadraticExtension<INT>& operator+=( const T& a ) noexcept;
-  inline QuadraticExtension<INT>& operator-=( const QuadraticExtension<INT>& n ) noexcept;
-  template <typename T> inline QuadraticExtension<INT>& operator-=( const T& a ) noexcept;
+  inline QuadraticExtensions<INT,NUM>& operator+=( const QuadraticExtensions<INT,NUM>& n ) noexcept;
+  template <typename T> inline auto operator+=( const T& a ) noexcept -> enable_if_t<is_constructible_v<INT,T>,QuadraticExtensions<INT,NUM>&>;
+  template <typename T> inline auto operator+=( const T& n ) noexcept -> enable_if_t<!is_constructible_v<INT,T>,QuadraticExtensions<INT,NUM>&>;
+
+  inline QuadraticExtensions<INT,NUM>& operator-=( const QuadraticExtensions<INT,NUM>& n ) noexcept;
+  template <typename T> inline auto operator-=( const T& a ) noexcept -> enable_if_t<is_constructible_v<INT,T>,QuadraticExtensions<INT,NUM>&>;
+  template <typename T> inline auto operator-=( const T& n ) noexcept -> enable_if_t<!is_constructible_v<INT,T>,QuadraticExtensions<INT,NUM>&>;
 
   // 3数の乗法が入るのでオーバーフローに注意
   // 10^6より大きいmodで計算する時はINTにMod<M>などを代入するか乗法の定義を修正する
-  inline QuadraticExtension<INT>& operator*=( const QuadraticExtension<INT>& n ) noexcept;
-  template <typename T> inline QuadraticExtension<INT>& operator*=( const T& a ) noexcept;
-  template <typename T> inline QuadraticExtension<INT>& operator/=( const T& a ) noexcept;
-  template <typename T> inline QuadraticExtension<INT>& operator%=( const T& a ) noexcept;
+  inline QuadraticExtensions<INT,NUM>& operator*=( const QuadraticExtensions<INT,NUM>& n ) noexcept;
+  template <typename T> inline auto operator*=( const T& a ) noexcept -> enable_if_t<is_constructible_v<INT,T>,QuadraticExtensions<INT,NUM>&>;
+  template <typename T> inline auto operator*=( const T& n ) noexcept -> enable_if_t<!is_constructible_v<INT,T>,QuadraticExtensions<INT,NUM>&>;
 
-  // m_p_Dの一致込みの等号
-  static inline bool Equal( const QuadraticExtension<INT>& n0 , const QuadraticExtension<INT>& n1 ) noexcept;
+  inline QuadraticExtensions<INT,NUM>& operator/=( QuadraticExtensions<INT,NUM> n ) noexcept;
+  template <typename T> inline auto operator/=( const T& a ) noexcept -> enable_if_t<is_constructible_v<INT,T>,QuadraticExtensions<INT,NUM>&>;
+  template <typename T> inline auto operator/=( const T& n ) noexcept -> enable_if_t<!is_constructible_v<INT,T>,QuadraticExtensions<INT,NUM>&>;
+
+  template <typename T> inline auto operator%=( const T& a ) noexcept -> enable_if_t<is_constructible_v<INT,T>,QuadraticExtensions<INT,NUM>&>;
   
-private:
-  static inline const INT& Zero() noexcept;
+  inline bool operator==( const QuadraticExtensions<INT,NUM>& n ) noexcept;
+  template <typename T> inline bool operator==( const T& n ) noexcept;
+  template <typename T> inline bool operator!=( const T& n ) noexcept;
 
+  // exponent>=0である場合にのみサポート。exponent乗する。
+  template <typename SINT> inline QuadraticExtensions<INT,NUM> operator^( SINT exponent ) const;
+
+  inline QuadraticExtensions<INT,NUM> operator-() const noexcept;
+  inline INT Norm() const noexcept;
+  inline QuadraticExtensions<INT,NUM> Adjoint() const noexcept;
+  // exponent>=0である場合にのみサポート。exponent乗する。
+  template <typename SINT> inline QuadraticExtensions<INT,NUM>& Power( SINT exponent );
+  // グローバルスコープでswapを定義するためのもの。
+  inline void swap( QuadraticExtensions<INT,NUM>& n ) noexcept;
+  
+  static inline const QuadraticExtensions<INT,NUM>& zero() noexcept;
+  static inline const QuadraticExtensions<INT,NUM>& one() noexcept;
+
+  static inline const INT& GetD() noexcept;
+  static inline void SetD( const INT& M ) noexcept;  
+
+private:
+  template <typename UINT> inline QuadraticExtensions<INT,NUM>& PositivePower( UINT exponent ) noexcept;
+  template <typename UINT> inline QuadraticExtensions<INT,NUM>& NonNegativePower( UINT exponent ) noexcept;
+
+  using Constants = ConstantsForQuadraticExtensions<INT,NUM>;
+  
 };
 
-template <typename INT> inline bool operator==( const QuadraticExtension<INT>& n0 , const QuadraticExtension<INT>& n1 ) noexcept;
-template <typename INT> inline bool operator!=( const QuadraticExtension<INT>& n0 , const QuadraticExtension<INT>& n1 ) noexcept;
+#include "a_Alias.hpp"
 
-template <typename INT , typename T> inline QuadraticExtension<INT> operator+( const QuadraticExtension<INT>& n , const T& a ) noexcept;
-template <typename INT , typename T> inline QuadraticExtension<INT> operator-( const QuadraticExtension<INT>& n , const T& a ) noexcept;
-template <typename INT , typename T> inline QuadraticExtension<INT> operator*( const QuadraticExtension<INT>& n , const T& a ) noexcept;
-template <typename INT , typename T> inline QuadraticExtension<INT> operator/( const QuadraticExtension<INT>& n , const T& a ) noexcept;
-template <typename INT , typename T> inline QuadraticExtension<INT> operator%( const QuadraticExtension<INT>& n , const T& a ) noexcept;
+template <typename INT , int NUM , typename T> inline QuadraticExtensions<INT,NUM> operator+( QuadraticExtensions<INT,NUM> n , const T& a ) noexcept;
+template <typename INT , int NUM , typename T> inline QuadraticExtensions<INT,NUM> operator-( QuadraticExtensions<INT,NUM> n , const T& a ) noexcept;
+template <typename INT , int NUM , typename T> inline QuadraticExtensions<INT,NUM> operator*( QuadraticExtensions<INT,NUM> n , const T& a ) noexcept;
+template <typename INT , int NUM , typename T> inline QuadraticExtensions<INT,NUM> operator/( QuadraticExtensions<INT,NUM> n , const T& a ) noexcept;
+template <typename INT , int NUM , typename T> inline QuadraticExtensions<INT,NUM> operator%( QuadraticExtensions<INT,NUM> n , const T& a ) noexcept;
 
-// Dは一時変数でない変数
-template <typename INT> inline QuadraticExtension<INT> Sqrt( const INT& D ) noexcept;
+template <typename INT , int NUM , typename SINT> inline QuadraticExtensions<INT,NUM> Power( QuadraticExtensions<INT,NUM> n , SINT exponent );
 
