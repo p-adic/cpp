dee938cc1669469fb110bfa2d13b70679f186d5d
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-10-19 16:17:33 +0900

    Add files via upload

--- Mathematics/Combinatorial/PartitionNumber/a.hpp
+++ Mathematics/Combinatorial/PartitionNumber/a.hpp
@@ -1,8 +1,17 @@
 // c:/Users/user/Documents/Programming/Mathematics/Combinatorial/PartitionNumber/a.hpp
 
 #pragma once
+// verify:
+// PartitionNumber‚Ìpython”Å (https://yukicoder.me/submissions/1021227)
+// UpperBoundedPartitionNumber (https://yukicoder.me/submissions/1021223)
 
-// nˆÈ‰º‚Ì”ñ•‰®”i‚ğ³®”‚Ì¸‡˜a‚É•ªŠ„‚·‚é•û–@‚ğ‘æi¬•ª‚ÉŠi”[‚·‚éBiO(n^2)j
-vector<int> PartitionNumber( const int& n );
-// nˆÈ‰º‚Ì”ñ•‰®”i‚ğ”ñ•‰®”lŒÂ‚Ì¸‡˜a‚É•ªŠ„‚·‚é•û–@‚ğ‘æi¬•ª‚ÉŠi”[‚·‚éBiO(ln^2)j
-vector<int> NonNegativePartitionNumber( const int& n , const int& l );
+
+// nˆÈ‰º‚Ì”ñ•‰®”i‚ğ³®”‚Ì¸‡˜a‚É•ªŠ„‚·‚éŒÂ”‚ğanswer[i]‚ÉŠi”[‚·‚éBiO(n^2)j
+template <typename INT> vector<INT> PartitionNumber( const int& n );
+// nˆÈ‰º‚Ì”ñ•‰®”i‚ğjˆÈã‚Ì³®”‚Ì¸‡˜a‚É•ªŠ„‚·‚éŒÂ”‚ğanswer[i][j]‚ÉŠi”[‚·‚éBiO(n^2)j
+template <typename INT> vector<vector<INT>> LowerBoundedPartitionNumber( const int& n );
+// nˆÈ‰º‚Ì”ñ•‰®”i‚ğjˆÈ‰º‚Ì³®”‚Ì¸‡˜a‚É•ªŠ„‚·‚éŒÂ”‚ğanswer.first[i][j]‚ÉŠi”[‚·‚éBiO(n^2)j
+template <typename INT> vector<vector<INT>> UpperBoundedPartitionNumber( const int& n );
+// nˆÈ‰º‚Ì”ñ•‰®”i‚ğ”ñ•‰®”jŒÂ‚Ì¸‡˜a‚É•ªŠ„‚·‚éŒÂ”‚â
+// nˆÈ‰º‚Ì”ñ•‰®”i‚ğ³®”jŒÂˆÈ‰º‚Ì¸‡˜a‚É•ªŠ„‚·‚éŒÂ”‚Æ‚à“™‚µ‚¢B
+
