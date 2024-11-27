164fc3ffd294a5ffd7616feb2580aade6e5a911b
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-11-27 09:28:59 +0900

    Delete Utility/StdStream directory

--- Utility/StdStream/type_info/a.hpp
+++
@@ -1,5 +0,0 @@
-// c:/Users/user/Documents/Programming/Utility/StdStream/type_info/a.hpp
-
-#pragma once
-
-template <class Traits> inline basic_ostream<char,Traits>& operator<<( basic_ostream<char,Traits>& os , const type_info& type ) noexcept;
--- Utility/StdStream/type_info/a_Body.hpp
+++
@@ -1,8 +0,0 @@
-// c:/Users/user/Documents/Programming/Utility/StdStream/type_info/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-#include "../../String/a_Body.hpp"
-
-template <class Traits> inline basic_ostream<char,Traits>& operator<<( basic_ostream<char,Traits>& os , const type_info& type ) noexcept { return os << to_string( type ); }
