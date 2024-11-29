100e43a15781da1de018759951f2b14b9eea6e92
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-11-29 13:29:01 +0900

    Add files via upload

--- SetTheory/DirectProduct/CoordinateCompress/Debug/a_Body.hpp
+++ SetTheory/DirectProduct/CoordinateCompress/Debug/a_Body.hpp
@@ -10,8 +10,8 @@ template <typename INT> inline CoordinateCompress<INT>::CoordinateCompress() : m
 
   if( init ){
 
-    cerr << "CoordinateCompressをデバッグモードで実行します。" << endl;
-    cerr << "エラー出力以外は変わりません。" << endl;
+    DERR( "CoordinateCompressをデバッグモードで実行します。" );
+    DERR( "エラー出力以外は変わりません。" );
     init = false;
 
   }
@@ -29,19 +29,19 @@ pair<vector<INT>,unordered_map<INT,int>> CoordinateCompress<INT>::GetR()
   answer.first.resize( m_r.size() );
   int i = 0;
 
-  cerr << "CoordinateCompress::GetR()で、登録された右辺値を座標圧縮します。" << endl;
-  cerr << "登録された右辺値全体の集合は { " << m_r << " } です。" << endl;
-  cerr << "小さい順にそれぞれ以下のように変換します：" << endl;
+  DERR( "CoordinateCompress::GetR()で、登録された右辺値を座標圧縮します。" );
+  DERR( "登録された右辺値全体の集合は {" , m_r , "} です。" );
+  DERR( "小さい順にそれぞれ以下のように変換します：" );
 
   for( auto t : m_r ){
 
-    cerr << t << " -> " << i << endl;
+    DERR( t , "->" , i );
     answer.first[i] = t;
     answer.second[t] = i++;
 
   }
 
-  cerr << "座標圧縮を終了します。" << endl;
+  DERR( "座標圧縮を終了します。" );
   return answer;
 
 }
@@ -57,41 +57,41 @@ vector<INT> CoordinateCompress<INT>::GetL()
 
   int i = -1;
   vector<INT> answer{};
-  cerr << "CoordinateCompress::GetR()で、登録された左辺値を座標圧縮します。" << endl;
+  DERR( "CoordinateCompress::GetR()で、登録された左辺値を座標圧縮します。" );
 
   if( m_l.empty() ){
 
-    cerr << "登録された左辺値はありませんでした。";
+    DERR( "登録された左辺値はありませんでした。" );
 
   } else {
 
-    cerr << "登録された左辺値は登録順に ( ";
+    DERRNS( "登録された左辺値は登録順に ( " );
     int size = m_l.size();
   
     for( int j = 0 ; j < size ; j++ ){
 
-      ( j == 0 ? cerr : cerr << " " ) << *( m_l[j] );
+      DERR( j == 0 ? "" : " " , *( m_l[j] ) );
 
     }
   
-    cerr << " ) です。" << endl;
+    DERR( " ) です。" );
     auto comp = []( INT* const& p0 , INT* const& p1 ) { return *p0 < *p1; };
     sort( m_l.begin() , m_l.end() , comp );
     INT temp = *( m_l[0] ) - 1;
-    cerr << "小さい順にそれぞれ以下のように変換します：" << endl;
+    DERR( "小さい順にそれぞれ以下のように変換します：" );
 
     for( auto p : m_l ){
 
-      cerr << *p << " -> ";
+      DERRNS( *p , " -> " );
       *p = temp == *p ? i : ( answer.push_back( temp = *p ) , ++i );
-      cerr << i << endl;		    
+      DERR( i );		    
 
     }
 
   }
 
-  cerr << "変換前の左辺値全体の集合は { " << answer << " } です。" << endl;
-  cerr << "座標圧縮を終了します。" << endl;
+  DERR( "変換前の左辺値全体の集合は { " << answer << " } です。" );
+  DERR( "座標圧縮を終了します。" );
   return answer;
 
 }
