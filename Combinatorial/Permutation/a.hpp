bf9a70c621c07cea7af6f4adefbdb5a1de43a4e7
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-12-10 18:52:39 +0900

    Add files via upload

--- Combinatorial/Permutation/a.hpp
+++ Combinatorial/Permutation/a.hpp
@@ -2,7 +2,19 @@
 
 #pragma once
 
-// ˆê„‚Ì‡ŒvŒvZ—ÊO(N!)B
+// verify: https://yukicoder.me/submissions/942586i“]“|”j
+
+// 7! à 5~10^3"
+// 8! à 4~10^4"
+// 9! à 4~10^5"
+// 10! à 4~10^6"
+// 11! à 4~10^7"
+// 12! à 5~10^8"
+// 18! à 6~10^15
+// 19! à 1~10^17
+// 20! à 2~10^18
+
+// ˆê„‚Ì‡ŒvŒvZ—ÊO(N(N!))B
 // «‘‡‚ÅŒãÒ‚ª‘¶İ‚µ‚È‚¢ê‡‚Í«‘‡‚ÅÅ¬’l‚É–ß‚é‚±‚Æ‚É’ˆÓB
 template <typename INT , int size_max> inline bool NextPermutation( INT ( &P )[size_max] , const int& size );
 template <typename INT> inline bool NextPermutation( vector<INT>& P );
@@ -10,5 +22,12 @@ template <typename INT> inline bool NextPermutation( vector<INT>& P );
 template <typename INT , int size_max> inline bool PreviousPermutation( INT ( &P )[size_max] , const int& size );
 template <typename INT> inline bool PreviousPermutation( vector<INT>& P );
 
-// P‚Í0,...,size-1‚Ì‡—ñ
-template <int size_max> ll InversionNumber( const int ( &P )[size_max] , const int& size );
+// ‡—ñP‚Ì“]“|”ŒvZO(N log N)B
+template <typename INT> ll InversionNumber( const vector<INT>& P );
+template <typename INT> ll InversionNumber( const vector<INT>& P , const vector<INT>& P_inv , const int& size );
+
+// ‡—ñP‚Ì„‰ñ’uŠ·•\¦O(N)B
+template <typename INT> vector<vector<INT>> CyclicPermutationRepresentation( const vector<INT>& P , const bool& zero_indexed = true );
+
+// ‡—ñP‚Ì‹É¬ŒİŠ·•\¦O(N)B
+template <typename INT> vector<pair<INT,INT>> MinimalTranspositionRepresentation( const vector<INT>& P , const bool& zero_indexed = true );
--- Combinatorial/Permutation/a_Body.hpp
+++ Combinatorial/Permutation/a_Body.hpp
@@ -3,21 +3,27 @@
 #pragma once
 #include "a.hpp"
 
-// InversionNumberに使う。
-#include "../../SetTheory/DirectProduct/AffineSpace/BIT/a_Body.hpp"
 // NextPermutationとPreviousPermutationに使う。
 #include <algorithm>
 
-template <typename INT , int size_max> inline bool NextPermutation( INT ( &P )[size_max] , const int& size ) {  return next_permutation( P , P + size ); }
+// InversionNumberに使う。
+#ifdef DEBUG
+  #include "../../SetTheory/DirectProduct/AffineSpace/BIT/Debug/a_Body.hpp"
+#else
+  #include "../../SetTheory/DirectProduct/AffineSpace/BIT/a_Body.hpp"
+#endif
+
+template <typename INT , int size_max> inline bool NextPermutation( INT ( &P )[size_max] , const int& size ) { return next_permutation( P , P + size ); }
 template <typename INT> inline bool NextPermutation( vector<INT>& P ) {  return next_permutation( P.begin() , P.end() ); }
-template <typename INT , int size_max> inline bool PreviousPermutation( INT ( &P )[size_max] , const int& size ) {  return prev_permutation( P , P + size ); }
+template <typename INT , int size_max> inline bool PreviousPermutation( INT ( &P )[size_max] , const int& size ) { return prev_permutation( P , P + size ); }
 template <typename INT> inline bool PreviousPermutation( vector<INT>& P ) {  return prev_permutation( P.begin() , P.end() ); }
 
-template <int size_max>
-ll InversionNumber( const int ( &P )[size_max] , const int& size )
+template <typename INT>
+ll InversionNumber( const vector<INT>& P )
 {
 
-  int P_inv[size_max];
+  const int size = P.size();
+  vector<INT> P_inv( size );
 
   for( int i = 0 ; i < size ; i++ ){
 
@@ -25,14 +31,111 @@ ll InversionNumber( const int ( &P )[size_max] , const int& size )
 
   }
 
-  BIT<int,size_max> count{};
-  long long answer = 0;
+  return InversionNumber( P , P_inv , size );
+
+}
+
+template <typename INT>
+ll InversionNumber( const vector<INT>& P , const vector<INT>& P_inv , const int& size )
+{
+  
+  BIT<ll> count{ size };
+  ll answer = 0;
   
   for( int i = size - 1 ; i >= 0 ; i-- ){
 
-    const int& P_inv_i = P_inv[i];
+    const INT& P_inv_i = P_inv[i];
     count.Add( P_inv_i , 1 );
     answer += count.IntervalSum( 0 , P_inv_i - 1 );
+
+  }
+
+  return answer;
+
+}
+
+template <typename INT>
+vector<vector<INT>> CyclicPermutationRepresentation( const vector<INT>& P , const bool& zero_indexed )
+{
+
+  if( !zero_indexed ){
+
+    auto Q = P;
+
+    for( auto& i : Q ){
+
+      i--;
+
+    }
+
+    auto answer = CyclicPermutationRepresentation( Q );
+
+    for( auto& C : answer ){
+
+      for( auto& i : C ){
+
+        i++;
+
+      }
+
+    }
+
+    return answer;
+
+  }
+  
+  const int size = P.size();
+  vector<vector<INT>> answer{};
+  vector<bool> found( size );
+
+  for( int i = 0 ; i < size ; i++ ){
+
+    if( found[i] ){
+
+      continue;
+
+    }
+
+    vector<INT> temp = { i };
+    found[i] = true;
+    int j = P[i];
+
+    while( j != i ){
+
+      found[j] = true;
+      temp.push_back( j );
+      j = P[j];
+
+    }
+
+    if( temp.size() > 1 ){
+
+      answer.push_back( move( temp ) );
+
+    }
+
+  }
+  
+  return answer;
+
+}
+
+template <typename INT>
+vector<pair<INT,INT>> MinimalTranspositionRepresentation( const vector<INT>& P , const bool& zero_indexed )
+{
+
+  auto crp = CyclicPermutationRepresentation( P , zero_indexed );
+  vector<pair<INT,INT>> answer{};
+
+  for( auto& C : crp ){
+
+    const int size = C.size();
+
+    for( int i = size - 1 ; i > 0 ; i-- ){
+
+      answer.push_back( { C[0] , C[i] } );
+
+    }
 
   }
 
